3c8831a49fc8852f0b7ae7ad67bb2919b681dd00
Michael Marineau <mike@marineau.org>
2014-09-17 17:15:57 -0700

    Add support for retrying writes after transient server errors.

--- index/write.go
+++ index/write.go
@@ -5,11 +5,22 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 
-	storage "code.google.com/p/google-api-go-client/storage/v1"
+	"code.google.com/p/google-api-go-client/googleapi"
+	"code.google.com/p/google-api-go-client/storage/v1"
 )
 
-var indexTemplate *template.Template
+var (
+	// Retry write requests up to 6 times.
+	maxTries int = 6
+	// Wait no less than a second before retrying.
+	minBackoff time.Duration = time.Second
+	// Do not wait more than 8 seconds between tries.
+	maxBackoff time.Duration = time.Second * 8
+
+	indexTemplate *template.Template
+)
 
 func init() {
 	indexText := `<html>
@@ -32,6 +43,24 @@ func init() {
 	indexTemplate = template.Must(template.New("index").Parse(indexText))
 }
 
+func expBackoff(interval time.Duration) time.Duration {
+	interval = interval * 2
+	if interval > maxBackoff {
+		interval = maxBackoff
+	}
+	return interval
+}
+
+func serverError(err error) bool {
+	if apierr, ok := err.(*googleapi.Error); ok {
+		if apierr.Code == 500 || apierr.Code == 503 {
+			return true
+		}
+	}
+
+	return false
+}
+
 func (d *Directory) WriteIndex(client *http.Client) error {
 	service, err := storage.New(client)
 	if err != nil {
@@ -52,6 +81,17 @@ func (d *Directory) WriteIndex(client *http.Client) error {
 	writeReq.Media(&buf)
 
 	fmt.Printf("Writing gs://%s/%s\n", d.Bucket, writeObj.Name)
-	_, err = writeReq.Do()
+
+	// Retry write, sometimes transient 500 errors are reported.
+	retryDelay := minBackoff
+	for try := 1; try <= maxTries; try++ {
+		_, err = writeReq.Do()
+		if err != nil && serverError(err) {
+			time.Sleep(retryDelay)
+			retryDelay = expBackoff(retryDelay)
+		} else {
+			break
+		}
+	}
 	return err
 }
