f8490705d9606af89b5fe1e42232332401467ffb
Patrick Baxter <pb@coreos.com>
2015-09-15 19:14:27 -0400

    cmd/cork: add download-image command

---
+++ cmd/cork/downloadimage.go
@@ -0,0 +1,116 @@
+// Copyright 2015 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/coreos/mantle/Godeps/_workspace/src/github.com/spf13/cobra"
+	"github.com/coreos/mantle/sdk"
+)
+
+var (
+	downloadImageCmd = &cobra.Command{
+		Use:   "download-image",
+		Short: "Download and verify CoreOS images",
+		Long:  "Download and verify current CoreOS images to a local cache.",
+		Run:   runDownloadImage,
+	}
+	downloadImageRoot         string
+	downloadImageCacheDir     string
+	downloadImagePrefix       string
+	downloadImageVerify       bool
+	downloadImagePlatformList platformList
+)
+
+func init() {
+	downloadImageCmd.Flags().StringVar(&downloadImageRoot,
+		"root", "http://storage.core-os.net/coreos/amd64-usr/master/", "base URL of images")
+	downloadImageCmd.Flags().StringVar(&downloadImageCacheDir,
+		"cache-dir", "", "local dir for image cache")
+	downloadImageCmd.Flags().StringVar(&downloadImagePrefix,
+		"image-prefix", "coreos_production", "image filename prefix")
+	downloadImageCmd.Flags().BoolVar(&downloadImageVerify,
+		"verify", true, "verify")
+	downloadImageCmd.Flags().Var(&downloadImagePlatformList,
+		"platform", "Choose qemu, gce, or aws. Multiple platforms can be specified by repeating the flag")
+
+	root.AddCommand(downloadImageCmd)
+}
+
+type platformList []string // satisfies pflag.Value interface
+
+func (platforms *platformList) String() string {
+	return fmt.Sprintf("%v", *platforms)
+}
+
+// not sure what this is for, but won't compile without it
+func (platforms *platformList) Type() string {
+	return "platformList"
+}
+
+// Set will append additional platform for each flag set. Comma
+// separated flags without spaces will also be parsed correctly.
+func (platforms *platformList) Set(value string) error {
+
+	// maps names of platforms to filename prefix
+	platformMap := map[string]string{
+		"qemu": "_image.bin.bz2",
+		"gce":  "_gce.tar.gz",
+		"aws":  "_ami_image.bin.bz2",
+	}
+
+	values := strings.Split(value, ",")
+
+	for _, platform := range values {
+		prefix, ok := platformMap[platform]
+		if !ok {
+			plog.Fatalf("platform not supported: %v", platform)
+		}
+		*platforms = append(*platforms, prefix)
+	}
+	return nil
+}
+
+func runDownloadImage(cmd *cobra.Command, args []string) {
+	if len(args) != 0 {
+		plog.Fatalf("Unrecognized arguements: %v", args)
+	}
+
+	if downloadImageCacheDir == "" {
+		plog.Fatal("Missing --cache-dir=FILEPATH")
+	}
+	if len(downloadImagePlatformList) == 0 {
+		plog.Fatal("Must specify 1 or more platforms to download")
+	}
+	if downloadImageVerify == false {
+		plog.Notice("Warning: image verification turned off")
+	}
+
+	// download signed digest
+	for _, suffix := range downloadImagePlatformList {
+		file := downloadImagePrefix + suffix
+		url := downloadImageRoot + file
+
+		if downloadImageVerify {
+			plog.Noticef("Verifying and updating to latest image %v", file)
+			sdk.DownloadSignedFile(file, url)
+		} else {
+			plog.Noticef("Forcing non-verified download of image %v", file)
+			sdk.DownloadFile(file, downloadImageRoot)
+		}
+	}
+}
