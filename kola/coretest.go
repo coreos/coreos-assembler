d047099ee421bacadecfad6e0881f2357a97d544
Nick Owens <mischief@coreos.com>
2015-09-30 15:27:10 -0700

    kola: write proper fleet test that waits for etcd availability

--- kola/coretest.go
+++ kola/coretest.go
@@ -39,9 +39,8 @@ func init() {
 		Run:         coretest.ClusterTests,
 		ClusterSize: 3,
 		NativeFuncs: map[string]func() error{
-			"EtcdUpdateValue":      coretest.TestEtcdUpdateValue,
-			"FleetctlListMachines": coretest.TestFleetctlListMachines,
-			"FleetctlRunService":   coretest.TestFleetctlRunService,
+			"EtcdUpdateValue":    coretest.TestEtcdUpdateValue,
+			"FleetctlRunService": coretest.TestFleetctlRunService,
 		},
 		CloudConfig: `#cloud-config
 
