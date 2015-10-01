1cd34ad50620c9c8de2e28f847f24f849e70791d
Nick Owens <mischief@coreos.com>
2015-10-01 15:51:21 -0700

    kola: bump fleet request timeout to 2.5s

--- kola/coretest.go
+++ kola/coretest.go
@@ -52,6 +52,8 @@ coreos:
     initial-advertise-peer-urls: http://$private_ipv4:2380
     listen-client-urls: http://0.0.0.0:2379,http://0.0.0.0:4001
     listen-peer-urls: http://$private_ipv4:2380,http://$private_ipv4:7001
+  fleet:
+    etcd-request-timeout: 2.5
   units:
     - name: etcd2.service
       command: start
