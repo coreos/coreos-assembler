46bb38b5100b54917cdc68d7e7786241cae20c33
Patrick Baxter <pb@coreos.com>
2015-12-08 17:55:01 -0500

    kola: allow tests to specify min CoreOS semver
    The minimum semver check is ignored if test glob is an exact match
    meaning a precise single test was specified.

--- kola/harness.go
+++ kola/harness.go
@@ -26,6 +26,7 @@ import (
 	"github.com/coreos/mantle/kola/register"
 	"github.com/coreos/mantle/platform"
 
+	"github.com/coreos/mantle/Godeps/_workspace/src/github.com/coreos/go-semver/semver"
 	"github.com/coreos/mantle/Godeps/_workspace/src/github.com/coreos/pkg/capnslog"
 
 	// Tests imported for registration side effects.
@@ -90,7 +91,7 @@ func testRunner(platform string, done <-chan struct{}, tests chan *register.Test
 	}
 }
 
-func filterTests(tests map[string]*register.Test, pattern, platform string) (map[string]*register.Test, error) {
+func filterTests(tests map[string]*register.Test, pattern, platform string, version *semver.Version) (map[string]*register.Test, error) {
 	r := make(map[string]*register.Test)
 
 	for name, t := range tests {
@@ -104,6 +105,11 @@ func filterTests(tests map[string]*register.Test, pattern, platform string) (map
 
 		// Skip the test if Manual is set and the name doesn't fully match.
 		if t.Manual && t.Name != pattern {
+			continue
+		}
+
+		// Check the test's minVersion when running more then one test
+		if version.LessThan(t.MinVersion) && t.Name != pattern {
 			continue
 		}
 
@@ -134,7 +140,12 @@ func RunTests(pattern, pltfrm string) error {
 	var passed, failed, skipped int
 	var wg sync.WaitGroup
 
-	tests, err := filterTests(register.Tests, pattern, pltfrm)
+	semver, err := getClusterSemver(pltfrm)
+	if err != nil {
+		plog.Fatal(err)
+	}
+
+	tests, err := filterTests(register.Tests, pattern, pltfrm, semver)
 	if err != nil {
 		plog.Fatal(err)
 	}
@@ -192,9 +203,55 @@ func RunTests(pattern, pltfrm string) error {
 	return nil
 }
 
+// getClusterSemVer returns the CoreOS semantic version via starting a
+// machine and checking
+func getClusterSemver(pltfrm string) (*semver.Version, error) {
+	var err error
+	var cluster platform.Cluster
+
+	switch pltfrm {
+	case "qemu":
+		cluster, err = platform.NewQemuCluster(QEMUOptions)
+	case "gce":
+		cluster, err = platform.NewGCECluster(GCEOptions)
+	case "aws":
+		cluster, err = platform.NewAWSCluster(AWSOptions)
+	default:
+		err = fmt.Errorf("invalid platform %q", pltfrm)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("creating cluster for semver check: %v", err)
+	}
+	defer func() {
+		if err := cluster.Destroy(); err != nil {
+			plog.Errorf("cluster.Destroy(): %v", err)
+		}
+	}()
+
+	m, err := cluster.NewMachine("")
+	if err != nil {
+		return nil, fmt.Errorf("creating new machine for semver check: %v", err)
+	}
+
+	out, err := m.SSH("grep ^VERSION_ID= /etc/os-release")
+	if err != nil {
+		return nil, fmt.Errorf("parsing /etc/os-release: %v", err)
+	}
+
+	version, err := semver.NewVersion(strings.Split(string(out), "=")[1])
+	if err != nil {
+		return nil, fmt.Errorf("parsing os-release semver: %v", err)
+	}
+
+	return version, nil
+}
+
 // RunTest is a harness for running a single test. It is used by
 // RunTests but can also be used directly by binaries that aim to run a
-// single test.
+// single test. Using RunTest directly means that TestCluster flags used
+// to filter out tests such as 'Platforms', 'Manual', or 'MinVersion'
+// are not respected.
 func RunTest(t *register.Test, pltfrm string) error {
 	var err error
 	var cluster platform.Cluster
