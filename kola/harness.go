3f50f59ba775877074fd7315ca72340420bf2e5a
Alex Crawford <alex.crawford@coreos.com>
2016-03-29 17:53:09 -0700

    Kola: add tests for Ignition spec v2

--- kola/harness.go
+++ kola/harness.go
@@ -35,6 +35,7 @@ import (
 	_ "github.com/coreos/mantle/kola/tests/flannel"
 	_ "github.com/coreos/mantle/kola/tests/fleet"
 	_ "github.com/coreos/mantle/kola/tests/ignition/v1"
+	_ "github.com/coreos/mantle/kola/tests/ignition/v2"
 	_ "github.com/coreos/mantle/kola/tests/kubernetes"
 	_ "github.com/coreos/mantle/kola/tests/metadata"
 	_ "github.com/coreos/mantle/kola/tests/misc"
---
+++ kola/tests/ignition/v2/sethostname.go
@@ -0,0 +1,73 @@
+// Copyright 2016 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ignition
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/coreos/mantle/kola/register"
+	"github.com/coreos/mantle/platform"
+
+	"github.com/coreos/mantle/Godeps/_workspace/src/github.com/coreos/go-semver/semver"
+	"github.com/coreos/mantle/Godeps/_workspace/src/github.com/coreos/pkg/capnslog"
+)
+
+var (
+	plog = capnslog.NewPackageLogger("github.com/coreos/mantle", "kola/tests/ignition")
+)
+
+func init() {
+	// Set the hostname
+	register.Register(&register.Test{
+		Name:        "coreos.ignition.v2.sethostname",
+		Run:         setHostname,
+		ClusterSize: 1,
+		Platforms:   []string{"aws"},
+		MinVersion:  semver.Version{Major: 1010},
+		UserData: `{
+		               "ignition": {
+		                   "version": "2.0.0"
+		               },
+		               "storage": {
+		                   "files": [
+		                       {
+		                           "filesystem": "root",
+		                           "path": "/etc/hostname",
+		                           "mode": 420,
+		                           "contents": {
+		                               "source": "data:,core1"
+		                           }
+		                       }
+		                   ]
+		               }
+		           }`,
+	})
+}
+
+func setHostname(c platform.TestCluster) error {
+	m := c.Machines()[0]
+
+	out, err := m.SSH("hostnamectl")
+	if err != nil {
+		return fmt.Errorf("failed to run hostnamectl: %s: %v", out, err)
+	}
+
+	if !strings.Contains(string(out), "Static hostname: core1") {
+		return fmt.Errorf("hostname wasn't set correctly:\n%s", out)
+	}
+
+	return nil
+}
