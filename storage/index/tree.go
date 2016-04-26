0b00995f602d14883bdafcaa3dcd75c786a556e2
Michael Marineau <michael.marineau@coreos.com>
2016-04-26 13:33:51 -0700

    storage/index: fix non-recursive index generation

--- storage/index/tree.go
+++ storage/index/tree.go
@@ -39,7 +39,11 @@ func NewIndexTree(bucket *storage.Bucket, includeEmpty bool) *IndexTree {
 	}
 
 	for _, prefix := range bucket.Prefixes() {
-		t.prefixes[prefix] = includeEmpty
+		if includeEmpty {
+			t.addDir(prefix)
+		} else {
+			t.prefixes[prefix] = false // initialize as empty
+		}
 	}
 
 	indexes := NewIndexSet(bucket)
@@ -63,6 +67,10 @@ func NewIndexTree(bucket *storage.Bucket, includeEmpty bool) *IndexTree {
 func (t *IndexTree) addObj(obj *gs.Object) {
 	prefix := storage.NextPrefix(obj.Name)
 	t.objects[prefix] = append(t.objects[prefix], obj)
+	t.addDir(prefix)
+}
+
+func (t *IndexTree) addDir(prefix string) {
 	for !t.prefixes[prefix] {
 		t.prefixes[prefix] = true // mark as not empty
 		if prefix == "" {
