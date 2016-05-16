8131620c986958e88aa82d670058bb7cd23dfd60
Matthew Garrett <mjg59@coreos.com>
2016-05-16 10:48:49 -0700

    kola: Ensure we can't access hosts when using invalid credentials
    Add a test that attempts to ssh into the test host with invalid credentials
    and verify that the attempt fails.

---
+++ kola/tests/misc/auth.go
@@ -0,0 +1,47 @@
+// Copyright 2016 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package misc
+
+import (
+	"fmt"
+
+	"github.com/coreos/mantle/kola/register"
+	"github.com/coreos/mantle/platform"
+)
+
+func init() {
+	register.Register(&register.Test{
+		Run:         AuthVerify,
+		ClusterSize: 1,
+		Name:        "coreos.auth.verify",
+		Platforms:   []string{"qemu", "aws", "gce"},
+		UserData:    `#cloud-config`,
+	})
+}
+
+// Basic authentication tests.
+
+// AuthVerify asserts that invalid passwords do not grant access to the system
+func AuthVerify(c platform.TestCluster) error {
+	m := c.Machines()[0]
+
+	client, err := m.PasswordSSHClient("core", "asdf")
+	if err == nil {
+		client.Close()
+		return fmt.Errorf("Successfully authenticated despite invalid password auth")
+	}
+
+	return nil
+}
