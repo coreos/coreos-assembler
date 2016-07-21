7e68152a05616e08ca195fd2e1af981af9647779
Nick Owens <mischief@coreos.com>
2016-07-20 17:27:44 -0700

    auth: drop unused debug code and godep hack
    godep hack appears to not be needed any more (godep v74)

--- auth/godephack.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-// oauth2 imports google.golang.org/cloud/compute/metadata but godep insists
-// on deleting it without something importing google.golang.org/cloud too.
-import (
-	_ "google.golang.org/cloud"
-)
