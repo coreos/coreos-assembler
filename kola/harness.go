3a073663603d9980f7838c31ec8e3ecde0617f34
Patrick Baxter <pb@coreos.com>
2016-11-17 22:32:33 +0000

    make OS test registration optional from use of package kola

--- kola/harness.go
+++ kola/harness.go
@@ -36,21 +36,6 @@ import (
 	"github.com/coreos/mantle/platform/machine/aws"
 	"github.com/coreos/mantle/platform/machine/gcloud"
 	"github.com/coreos/mantle/platform/machine/qemu"
-
-	// Tests imported for registration side effects.
-	_ "github.com/coreos/mantle/kola/tests/coretest"
-	_ "github.com/coreos/mantle/kola/tests/docker"
-	_ "github.com/coreos/mantle/kola/tests/etcd"
-	_ "github.com/coreos/mantle/kola/tests/flannel"
-	_ "github.com/coreos/mantle/kola/tests/fleet"
-	_ "github.com/coreos/mantle/kola/tests/ignition/v1"
-	_ "github.com/coreos/mantle/kola/tests/ignition/v2"
-	_ "github.com/coreos/mantle/kola/tests/kubernetes"
-	_ "github.com/coreos/mantle/kola/tests/locksmith"
-	_ "github.com/coreos/mantle/kola/tests/metadata"
-	_ "github.com/coreos/mantle/kola/tests/misc"
-	_ "github.com/coreos/mantle/kola/tests/rkt"
-	_ "github.com/coreos/mantle/kola/tests/systemd"
 )
 
 var (
