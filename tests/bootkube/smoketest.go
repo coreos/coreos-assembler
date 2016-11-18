0b96ed733fca790dcf2c9b1df0a86624acb3cbd4
Patrick Baxter <pb@coreos.com>
2016-11-18 06:47:44 +0000

    tests/bootkube: add nginxCheck to smoketest

--- tests/bootkube/smoketest.go
+++ tests/bootkube/smoketest.go
@@ -1,18 +1,63 @@
 package bootkube
 
 import (
+	"bytes"
+	"fmt"
+	"time"
+
 	"github.com/coreos/mantle/kola/cluster"
+	"github.com/coreos/mantle/util"
 )
 
 func bootkubeSmoke(c cluster.TestCluster) error {
-	// This should will not return until cluster is ready
-	_, err := MakeSimpleCluster(c)
+	// This should not return until cluster is ready
+	sc, err := MakeSimpleCluster(c)
 	if err != nil {
 		return err
 	}
-	// TODO add basic smoke tests here. Some examples: schedule an nginx
-	// pod and ping it, test that a pod network is correctly running. The
-	// setup function should be blocking on all the basic components being
-	// running.
+
+	// run an nginx deployment and ping it
+	if err := nginxCheck(sc); err != nil {
+		return fmt.Errorf("nginxCheck: %s", err)
+	}
+	// TODO add more basic or regression tests here
+	return nil
+}
+
+func nginxCheck(sc *SimpleCluster) error {
+	// start nginx deployment
+	_, err := sc.Kubectl("run my-nginx --image=nginx --replicas=2 --port=80")
+	if err != nil {
+		return err
+	}
+
+	// expose nginx
+	_, err = sc.Kubectl("expose deployment my-nginx --port=80 --type=LoadBalancer")
+	if err != nil {
+		return err
+	}
+	serviceIP, err := sc.Kubectl("get service my-nginx --template={{.spec.clusterIP}}")
+	if err != nil {
+		return err
+	}
+
+	// curl for welcome message
+	nginxRunning := func() error {
+		out, err := sc.Master.SSH("curl " + serviceIP + ":80")
+		if err != nil || !bytes.Contains(out, []byte("Welcome to nginx!")) {
+			return fmt.Errorf("unable to reach nginx: %s", out)
+		}
+		return nil
+	}
+	if err := util.Retry(15, 5*time.Second, nginxRunning); err != nil {
+		return err
+	}
+
+	// delete pod
+	_, err = sc.Kubectl("delete deployment my-nginx")
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
