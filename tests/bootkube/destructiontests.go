d9afd13ff30e61d4fdb179df5e10f93be2316ffb
Patrick Baxter <pb@coreos.com>
2017-01-11 22:59:51 +0000

    tests/bootkube: apiserver pod delete test

---
+++ tests/bootkube/destructiontests.go
@@ -0,0 +1,88 @@
+package bootkube
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/coreos-inc/pluton/spawn"
+
+	"github.com/coreos/mantle/kola/cluster"
+	"github.com/coreos/mantle/platform"
+	"github.com/coreos/mantle/util"
+)
+
+// TODO: Test that workloads started before any destruction tests are still
+// working after the destruction tests rather then creating a new workload after
+// the destructive test.
+
+// Restart master node and check that cluster is still functional
+func rebootMaster(tc cluster.TestCluster) error {
+	c, err := spawn.MakeBootkubeCluster(tc, 1)
+	if err != nil {
+		return err
+	}
+
+	// reboot and wait for api to come up 3 times to avoid false positives
+	for i := 0; i < 3; i++ {
+		if err := platform.Reboot(c.Masters[0]); err != nil {
+			return err
+		}
+
+		// TODO(pb) find a way to globally disable selinux in kola
+		_, err := c.Masters[0].SSH("sudo setenforce 0")
+		if err != nil {
+			return err
+		}
+
+		if err := c.NodeCheck(25); err != nil {
+			return fmt.Errorf("nodeCheck: %s", err)
+		}
+	}
+
+	if err := nginxCheck(c); err != nil {
+		return fmt.Errorf("nginxCheck: %s", err)
+	}
+	return nil
+}
+
+// Delete api-server pod and wait for it to recover
+func deleteAPIServer(tc cluster.TestCluster) error {
+	c, err := spawn.MakeBootkubeCluster(tc, 1)
+	if err != nil {
+		return err
+	}
+
+	out, err := c.Kubectl("get pods -l k8s-app=kube-apiserver -o=jsonpath={.items[*].metadata.name} --namespace=kube-system")
+	if err != nil {
+		return err
+	}
+
+	apipods := strings.Split(strings.TrimSpace(out), " ")
+	for _, pod := range apipods {
+		c.Kubectl("delete pod " + pod + " --namespace=kube-system")
+	}
+
+	// wait until kubectl errors out to know that apiserver terminated
+	f := func() error {
+		out, err := c.Kubectl("get pods --all-namespaces")
+		if err == nil {
+			return fmt.Errorf(out)
+		}
+		return nil
+	}
+	if err := util.Retry(25, 2*time.Second, f); err != nil {
+		return fmt.Errorf("apiserver never terminated: %s", err)
+	}
+
+	// wait for apiserver to return
+	if err := c.NodeCheck(25); err != nil {
+		return fmt.Errorf("nodeCheck: %s", err)
+	}
+
+	if err := nginxCheck(c); err != nil {
+		return fmt.Errorf("nginxCheck: %s", err)
+	}
+
+	return nil
+}
--- tests/bootkube/registry.go
+++ tests/bootkube/registry.go
@@ -16,8 +16,14 @@ func init() {
 	})
 
 	register.Register(&register.Test{
-		Name:      "bootkube.destruction",
-		Run:       bootkubeDestruction,
+		Name:      "bootkube.destruct.reboot",
+		Run:       rebootMaster,
+		Platforms: []string{"gce"},
+	})
+
+	register.Register(&register.Test{
+		Name:      "bootkube.destruct.delete",
+		Run:       deleteAPIServer,
 		Platforms: []string{"gce"},
 	})
 
