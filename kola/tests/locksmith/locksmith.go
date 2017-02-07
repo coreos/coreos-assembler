dd72dca96ce24dbd886778500883030a20885b03
David Michael <david.michael@coreos.com>
2017-02-06 16:25:35 -0800

    Revert "kola/tests/locksmith: disable on qemu"
    This reverts commit 4729d9bc7e23e4a856eca54ad31c4a97f65897a3.

--- kola/tests/locksmith/locksmith.go
+++ kola/tests/locksmith/locksmith.go
@@ -32,7 +32,6 @@ func init() {
 		Name:        "coreos.locksmith.cluster",
 		Run:         locksmithCluster,
 		ClusterSize: 3,
-		Platforms:   []string{"aws", "gce"},
 		UserData: `{
   "ignition": { "version": "2.0.0" },
   "systemd": {
