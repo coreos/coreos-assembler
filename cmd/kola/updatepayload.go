coreos/coreos-assembler

commit c4fb9c1542bb3461be009945e769f3f84fe0234c

kola: add support for dumping arbitrary test data
kola will now create a temporary directory `_kola_temp` to dump tests
data to, such as machine configuration, journals, etc. To start off with
only QEMU is implemented and only ignition configurations are written.

For example after `kola run` all ignition configs used can be found at:

    _kola_temp/$test_name/$machine_id/ignition.json

cmd/kola/updatepayload.go
@@ -130,7 +130,7 @@ func runUpdatePayload(cmd *cobra.Command, args []string) {
 }
 
 func runUpdateTest() error {
-	cluster, err := qemu.NewCluster(&kola.QEMUOptions)
+	cluster, err := qemu.NewCluster(&kola.QEMUOptions, outputDir)
 	if err != nil {
 		return fmt.Errorf("new cluster: %v", err)
 	}

cmd/kola/bootchart.go
@@ -56,8 +56,15 @@ func runBootchart(cmd *cobra.Command, args []string) {
 		cluster platform.Cluster
 		err     error
 	)
+
+	outputDir, err = kola.CleanOutputDir(outputDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Setup failed: %v\n", err)
+		os.Exit(1)
+	}
+
 	if kolaPlatform == "qemu" {
-		cluster, err = qemu.NewCluster(&kola.QEMUOptions)
+		cluster, err = qemu.NewCluster(&kola.QEMUOptions, outputDir)
 	} else if kolaPlatform == "gce" {
 		cluster, err = gcloud.NewCluster(&kola.GCEOptions)
 	} else if kolaPlatform == "aws" {

cmd/kola/etcdupgrade.go
@@ -90,7 +90,13 @@ coreos:
 	kola.RegisterTestOption("EtcdUpgradeBin", etcdUpgradeBin)
 	kola.RegisterTestOption("EtcdUpgradeBin2", etcdUpgradeBin2)
 
-	if err := kola.RunTest(t, "gce"); err != nil {
+	outputDir, err := kola.CleanOutputDir(outputDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Setup failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := kola.RunTest(t, "gce", outputDir); err != nil {
 		fmt.Fprintf(os.Stderr, "--- FAIL: %v", err)
 		os.Exit(1)
 	}

cmd/kola/kola.go
@@ -84,7 +84,7 @@ func runRun(cmd *cobra.Command, args []string) {
 		pattern = "*" // run all tests by default
 	}
 
-	err := kola.RunTests(pattern, kolaPlatform)
+	err := kola.RunTests(pattern, kolaPlatform, outputDir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)

cmd/kola/options.go
@@ -23,6 +23,7 @@ import (
 )
 
 var (
+	outputDir          string
 	kolaPlatform       string
 	defaultTargetBoard = sdk.DefaultBoard()
 	kolaDefaultImages  = map[string]string{
@@ -41,6 +42,7 @@ func init() {
 	bv := root.PersistentFlags().BoolVar
 
 	// general options
+	sv(&outputDir, "output-dir", "_kola_temp", "Temporary output directory for test data and logs")
 	sv(&kolaPlatform, "platform", "qemu", "VM platform: qemu, gce, aws")
 	root.PersistentFlags().IntVar(&kola.TestParallelism, "parallel", 1, "number of tests to run in parallel")
 	sv(&kola.TAPFile, "tapfile", "", "file to write TAP results to")

cmd/kola/qemu.go
@@ -47,14 +47,20 @@ func runQemu(cmd *cobra.Command, args []string) {
 		os.Exit(2)
 	}
 
-	cluster, err := qemu.NewCluster(&kola.QEMUOptions)
+	outputDir, err := kola.CleanOutputDir(outputDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Setup failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	cluster, err := qemu.NewCluster(&kola.QEMUOptions, outputDir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Cluster failed: %v\n", err)
 		os.Exit(1)
 	}
 	defer cluster.Destroy()
 
-	m, err := cluster.NewMachine("#cloud-config")
+	m, err := cluster.NewMachine(`{"ignition": { "version": "2.0.0" }}`)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Machine failed: %v\n", err)
 		os.Exit(1)

cmd/kola/spawn.go
@@ -78,9 +78,15 @@ func runSpawn(cmd *cobra.Command, args []string) {
 		userdata = "#cloud-config"
 	}
 
+	outputDir, err = kola.CleanOutputDir(outputDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Setup failed: %v\n", err)
+		os.Exit(1)
+	}
+
 	switch kolaPlatform {
 	case "qemu":
-		cluster, err = qemu.NewCluster(&kola.QEMUOptions)
+		cluster, err = qemu.NewCluster(&kola.QEMUOptions, outputDir)
 	case "gce":
 		cluster, err = gcloud.NewCluster(&kola.GCEOptions)
 	case "aws":

kola/harness.go
@@ -105,11 +105,11 @@ func (res resultSlice) ToTAP(w io.Writer) error {
 	return nil
 }
 
-func testRunner(platform string, done <-chan struct{}, tests chan *register.Test, results chan *result) {
+func testRunner(platform, outputDir string, done <-chan struct{}, tests chan *register.Test, results chan *result) {
 	for test := range tests {
 		plog.Noticef("=== RUN %s on %s", test.Name, platform)
 		start := time.Now()
-		err := RunTest(test, platform)
+		err := RunTest(test, platform, outputDir)
 		duration := time.Since(start)
 
 		select {
@@ -184,10 +184,17 @@ func versionOutsideRange(version, minVersion, endVersion semver.Version) bool {
 // tests based on a glob pattern and by platform. Has access to all
 // tests either registered in this package or by imported packages that
 // register tests in their init() function.
-func RunTests(pattern, pltfrm string) error {
+// outputDir is where various test logs and data will be written for
+// analysis after the test run. If it already exists it will be erased!
+func RunTests(pattern, pltfrm, outputDir string) error {
 	var passed, failed, skipped int
 	var wg sync.WaitGroup
 	var results resultSlice
+
+	outputDir, err := CleanOutputDir(outputDir)
+	if err != nil {
+		return err
+	}
 
 	// Avoid incurring cost of starting machine in getClusterSemver when
 	// either:
@@ -211,7 +218,7 @@ func RunTests(pattern, pltfrm string) error {
 	}
 
 	if !skipGetVersion {
-		version, err := getClusterSemver(pltfrm)
+		version, err := getClusterSemver(pltfrm, outputDir)
 		if err != nil {
 			plog.Fatal(err)
 		}
@@ -232,7 +239,7 @@ func RunTests(pattern, pltfrm string) error {
 
 	for i := 0; i < TestParallelism; i++ {
 		go func() {
-			testRunner(pltfrm, done, testc, resc)
+			testRunner(pltfrm, outputDir, done, testc, resc)
 			wg.Done()
 		}()
 	}
@@ -293,13 +300,18 @@ func RunTests(pattern, pltfrm string) error {
 
 // getClusterSemVer returns the CoreOS semantic version via starting a
 // machine and checking
-func getClusterSemver(pltfrm string) (*semver.Version, error) {
+func getClusterSemver(pltfrm, outputDir string) (*semver.Version, error) {
 	var err error
 	var cluster platform.Cluster
 
+	testDir := filepath.Join(outputDir, "get_cluster_semver")
+	if err := os.MkdirAll(testDir, 0777); err != nil {
+		return nil, err
+	}
+
 	switch pltfrm {
 	case "qemu":
-		cluster, err = qemu.NewCluster(&QEMUOptions)
+		cluster, err = qemu.NewCluster(&QEMUOptions, testDir)
 	case "gce":
 		cluster, err = gcloud.NewCluster(&GCEOptions)
 	case "aws":
@@ -340,12 +352,19 @@ func getClusterSemver(pltfrm string) (*semver.Version, error) {
 // single test. Using RunTest directly means that TestCluster flags used
 // to filter out tests such as 'Platforms', 'Manual', or 'MinVersion'
 // are not respected.
-func RunTest(t *register.Test, pltfrm string) (err error) {
+// outputDir is where various test logs and data will be written for
+// analysis after the test run. It should already exist.
+func RunTest(t *register.Test, pltfrm, outputDir string) (err error) {
 	var c platform.Cluster
+
+	testDir := filepath.Join(outputDir, t.Name)
+	if err := os.MkdirAll(testDir, 0777); err != nil {
+		return err
+	}
 
 	switch pltfrm {
 	case "qemu":
-		c, err = qemu.NewCluster(&QEMUOptions)
+		c, err = qemu.NewCluster(&QEMUOptions, testDir)
 	case "gce":
 		c, err = gcloud.NewCluster(&GCEOptions)
 	case "aws":
@@ -458,4 +477,19 @@ func MakeConfigs(url, cfg string, csize int) []string {
 		cfgs = append(cfgs, strings.Replace(cfg, "$name", "instance"+strconv.Itoa(i), -1))
 	}
 	return cfgs
+}
+
+// CleanOutputDir creates an empty directory, any existing data will be wiped!
+func CleanOutputDir(outputDir string) (string, error) {
+	outputDir = filepath.Clean(outputDir)
+	if outputDir == "." {
+		return "", fmt.Errorf("kola: missing output directory path")
+	}
+	if err := os.RemoveAll(outputDir); err != nil {
+		return "", err
+	}
+	if err := os.MkdirAll(outputDir, 0777); err != nil {
+		return "", err
+	}
+	return outputDir, nil
 }

commit 305f56011e38dcef1750cadd2ebaeef9f6538d51

kola: fix updatepayload command
Broken by c4fb9c1542, only partially updated this command to use the new
output directory.

cmd/kola/updatepayload.go
@@ -19,6 +19,7 @@ import (
 	"bytes"
 	"fmt"
 	"net"
+	"os"
 	"path/filepath"
 	"strings"
 	"text/template"
@@ -130,6 +131,12 @@ func runUpdatePayload(cmd *cobra.Command, args []string) {
 }
 
 func runUpdateTest() error {
+	outputDir, err := kola.CleanOutputDir(outputDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Setup failed: %v\n", err)
+		os.Exit(1)
+	}
+
 	cluster, err := qemu.NewCluster(&kola.QEMUOptions, outputDir)
 	if err != nil {
 		return fmt.Errorf("new cluster: %v", err)