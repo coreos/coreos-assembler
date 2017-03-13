a988b995c89b9a1580669556e51e98d71eca1919
Patrick Baxter <pb@coreos.com>
2017-03-13 23:30:00 +0000

    pluton/tests/bootkube: re-org registry

--- pluton/tests/bootkube/registry.go
+++ pluton/tests/bootkube/registry.go
@@ -23,21 +23,10 @@ import (
 var plog = capnslog.NewPackageLogger("github.com/coreos/mantle", "pluton/tests/bootkube")
 
 func init() {
+	// main test suite run on every PR
 	register.Register(&register.Test{
 		Name:      "bootkube.smoke",
 		Run:       bootkubeSmoke,
-		Platforms: []string{"gce"},
-	})
-
-	register.Register(&register.Test{
-		Name:      "bootkube.selfetcd.smoke",
-		Run:       bootkubeSmokeEtcd,
-		Platforms: []string{"gce"},
-	})
-
-	register.Register(&register.Test{
-		Name:      "bootkube.selfetcd.scale",
-		Run:       etcdScale,
 		Platforms: []string{"gce"},
 	})
 
@@ -53,6 +42,22 @@ func init() {
 		Platforms: []string{"gce"},
 	})
 
+	// main self-hosted test suite run on every PR
+	register.Register(&register.Test{
+		Name:      "bootkube.selfetcd.smoke",
+		Run:       bootkubeSmokeEtcd,
+		Platforms: []string{"gce"},
+	})
+
+	register.Register(&register.Test{
+		Name:      "bootkube.selfetcd.scale",
+		Run:       etcdScale,
+		Platforms: []string{"gce"},
+	})
+
+	// experimental self-hosted test suite run via `rktbot run etcd tests`
+
+	// conformance
 	register.Register(&register.Test{
 		Name:      "conformance.bootkube",
 		Run:       conformanceBootkube,
