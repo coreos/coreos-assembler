3a441c7c685fce432273b4961adae36b03f2f2e5
Benjamin Gilbert <benjamin.gilbert@coreos.com>
2017-04-27 16:36:39 -0700

    platform/api/aws: Try several times to launch instances
    Some instances (particularly PV) never successfully boot. Retry launch
    a few times before giving up.

--- platform/api/aws/ec2.go
+++ platform/api/aws/ec2.go
@@ -43,15 +43,18 @@ func (a *API) DeleteKey(name string) error {
 }
 
 // CheckInstances waits until a set of EC2 instances are accessible by SSH, waiting a maximum of 'd' time.
-func (a *API) CheckInstances(ids []string, d time.Duration) error {
+// Returns lists of the accessible and inaccessible instances.
+func (a *API) CheckInstances(ids []string, d time.Duration) ([]string, []string, error) {
 	after := time.After(d)
-	online := make(map[string]bool)
+	online := make([]string, 0, len(ids))
+	offline := make([]string, len(ids))
+	copy(offline, ids)
 
 	// loop until all machines are online
-	for len(ids) != len(online) {
+	for len(offline) > 0 {
 		select {
 		case <-after:
-			return fmt.Errorf("timed out waiting for instances to run")
+			return online, offline, fmt.Errorf("timed out waiting for instances to run")
 		default:
 		}
 
@@ -59,46 +62,43 @@ func (a *API) CheckInstances(ids []string, d time.Duration) error {
 		time.Sleep(10 * time.Second)
 
 		getinst := &ec2.DescribeInstancesInput{
-			InstanceIds: aws.StringSlice(ids),
+			InstanceIds: aws.StringSlice(offline),
 		}
 
 		insts, err := a.ec2.DescribeInstances(getinst)
 		if err != nil {
-			return err
+			return online, offline, err
 		}
 
 		for _, r := range insts.Reservations {
 			for _, i := range r.Instances {
-				switch *i.State.Name {
-				case ec2.InstanceStateNamePending:
-					// continue
-				case ec2.InstanceStateNameRunning:
-					// skip instances known to be up
-					if online[*i.InstanceId] {
-						continue
-					}
+				if *i.State.Name != ec2.InstanceStateNameRunning {
+					continue
+				}
 
-					if i.PublicIpAddress == nil {
-						continue
-					}
+				if i.PublicIpAddress == nil {
+					continue
+				}
 
-					// XXX: ssh is a terrible way to check this, but it is all we have.
-					c, err := net.DialTimeout("tcp", *i.PublicIpAddress+":22", 3*time.Second)
-					if err != nil {
-						continue
-					}
-					c.Close()
+				// XXX: ssh is a terrible way to check this, but it is all we have.
+				c, err := net.DialTimeout("tcp", *i.PublicIpAddress+":22", 3*time.Second)
+				if err != nil {
+					continue
+				}
+				c.Close()
 
-					online[*i.InstanceId] = true
-				default:
-					// instances should not be stopping, shutting-down, terminated, etc.
-					return fmt.Errorf("instance %v in unexpected state %q", *i.InstanceId, *i.State.Name)
+				online = append(online, *i.InstanceId)
+				for j, v := range offline {
+					if v == *i.InstanceId {
+						offline = append(offline[:j], offline[j+1:]...)
+						break
+					}
 				}
 			}
 		}
 	}
 
-	return nil
+	return online, offline, nil
 }
 
 // CreateInstances creates EC2 instances with a given name tag, ssh key name, user data. The image ID, instance type, and security group set in the API will be used.
@@ -157,20 +157,34 @@ func (a *API) CreateInstancesWithoutWaiting(name, keyname, userdata string, coun
 
 // CreateInstances creates EC2 instances with a given name tag, ssh key name, user data. The image ID, instance type, and security group set in the API will be used. CreateInstances will block until all instances are reachable by SSH.
 func (a *API) CreateInstances(name, keyname, userdata string, count uint64) ([]*ec2.Instance, error) {
-	instances, err := a.CreateInstancesWithoutWaiting(name, keyname, userdata, count)
-	if err != nil {
-		return nil, err
-	}
+	var savedErr error
+	ids := make([]string, 0, count)
 
-	ids := make([]string, len(instances))
-	for i, inst := range instances {
-		ids[i] = *inst.InstanceId
-	}
+	// try 4 times to get a working set of instances
+	for try := 0; try < 4; try++ {
+		instances, err := a.CreateInstancesWithoutWaiting(name, keyname, userdata, count-uint64(len(ids)))
+		if err != nil {
+			a.TerminateInstances(ids)
+			return nil, err
+		}
 
-	// 5 minutes is a pretty reasonable timeframe for AWS instances to work.
-	if err := a.CheckInstances(ids, 10*time.Minute); err != nil {
+		currentIds := make([]string, len(instances))
+		for i, inst := range instances {
+			currentIds[i] = *inst.InstanceId
+		}
+
+		// 5 minutes is a pretty reasonable timeframe for AWS instances to work.
+		online, offline, err := a.CheckInstances(currentIds, 5*time.Minute)
+		ids = append(ids, online...)
+		if err == nil {
+			break
+		}
+		a.TerminateInstances(offline)
+		savedErr = err
+	}
+	if uint64(len(ids)) < count {
 		a.TerminateInstances(ids)
-		return nil, err
+		return nil, savedErr
 	}
 
 	// call DescribeInstances to get machine IP
