63155e2a87ff0b3fbd86d535046494fac727cc33
Benjamin Gilbert <benjamin.gilbert@coreos.com>
2017-06-12 14:53:12 -0700

    kola/tests/ignition/v1: Sync with v2

---
+++ kola/tests/ignition/v1/execution.go
@@ -0,0 +1,66 @@
+// Copyright 2017 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ignition
+
+import (
+	"github.com/coreos/mantle/kola/cluster"
+	"github.com/coreos/mantle/kola/register"
+)
+
+func init() {
+	register.Register(&register.Test{
+		Name:        "coreos.ignition.v1.once",
+		Run:         runsOnce,
+		ClusterSize: 1,
+		UserData: `{
+                             "ignitionVersion": 1,
+                             "storage": {
+                               "filesystems": [
+                                 {
+                                   "device": "/dev/disk/by-partlabel/ROOT",
+                                   "format": "ext4",
+                                   "files": [
+                                     {
+                                       "path": "/etc/ignition-ran",
+                                       "contents": "Ignition ran."
+                                     }
+                                   ]
+                                 }
+                               ]
+                             }
+                           }`,
+	})
+}
+
+func runsOnce(c cluster.TestCluster) {
+	m := c.Machines()[0]
+
+	// remove file created by Ignition; fail if it doesn't exist
+	_, err := m.SSH("sudo rm /etc/ignition-ran")
+	if err != nil {
+		c.Fatalf("Couldn't remove flag file: %v", err)
+	}
+
+	err = m.Reboot()
+	if err != nil {
+		c.Fatalf("Couldn't reboot machine: %v", err)
+	}
+
+	// make sure file hasn't been recreated
+	_, err = m.SSH("test -e /etc/ignition-ran")
+	if err == nil {
+		c.Fatalf("Flag file recreated after reboot")
+	}
+}
--- kola/tests/ignition/v1/root.go
+++ kola/tests/ignition/v1/root.go
@@ -1,4 +1,4 @@
-// Copyright 2015 CoreOS, Inc.
+// Copyright 2016 CoreOS, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
 package ignition
 
 import (
+	"strings"
+
 	"github.com/coreos/mantle/kola/cluster"
 	"github.com/coreos/mantle/kola/register"
 )
@@ -69,6 +71,37 @@ func init() {
 		ClusterSize: 1,
 		UserData:    xfsConfig,
 	})
+
+	// Reformat the root as ext4
+	ext4Config := `{
+		             "ignitionVersion": 1,
+		             "storage": {
+		                 "filesystems": [
+		                     {
+		                         "device": "/dev/disk/by-partlabel/ROOT",
+		                         "format": "ext4",
+		                         "create": {
+		                             "force": true,
+		                             "options": [
+		                                 "-L", "ROOT"
+		                             ]
+		                         }
+		                     }
+		                 ]
+		             }
+		         }`
+	register.Register(&register.Test{
+		Name:        "coreos.ignition.v1.ext4root",
+		Run:         ext4Root,
+		ClusterSize: 1,
+		UserData:    ext4Config,
+	})
+	register.Register(&register.Test{
+		Name:        "coreos.ignition.v1.ext4checkexisting",
+		Run:         ext4CheckExisting,
+		ClusterSize: 1,
+		UserData:    ext4Config,
+	})
 }
 
 func btrfsRoot(c cluster.TestCluster) {
@@ -77,6 +110,14 @@ func btrfsRoot(c cluster.TestCluster) {
 
 func xfsRoot(c cluster.TestCluster) {
 	testRoot(c, "xfs")
+}
+
+func ext4Root(c cluster.TestCluster) {
+	// Since the image's root partition is formatted to ext4 by default,
+	// this test wont be able to differentiate between the original filesystem
+	// and a newly created one. If mkfs.ext4 never ran, it would still pass.
+	// It will ensure that if mkfs.ext4 ran, it ran successfully.
+	testRoot(c, "ext4")
 }
 
 func testRoot(c cluster.TestCluster, fs string) {
@@ -89,5 +130,20 @@ func testRoot(c cluster.TestCluster, fs string) {
 
 	if string(out) != fs {
 		c.Fatalf("root wasn't correctly reformatted:\n%s", out)
+	}
+}
+
+func ext4CheckExisting(c cluster.TestCluster) {
+	m := c.Machines()[0]
+
+	// Redirect /dev/null to stdin so isatty(stdin) fails and the -p flag can be
+	// checked
+	out, err := m.SSH("sudo mkfs.ext4 -p /dev/disk/by-partlabel/ROOT < /dev/null")
+	if err == nil {
+		c.Fatalf("mkfs.ext4 returned sucessfully when it should have failed")
+	}
+
+	if !strings.HasPrefix(string(out), "/dev/disk/by-partlabel/ROOT contains a ext4 file system labelled 'ROOT'") {
+		c.Fatalf("mkfs.ext4 did not check for existing filesystems.\nmkfs.ext4: %s", out)
 	}
 }
