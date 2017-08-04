3b681bdc4440e74eff6721dbae225cc47af153de
Benjamin Gilbert <benjamin.gilbert@coreos.com>
2017-08-03 17:43:49 -0700

    kola/tests/docker: Don't fail if a single ICMP message is dropped
    For each container, send 10 pings in one second, and succeed if we
    receive any responses.

--- kola/tests/docker/docker.go
+++ kola/tests/docker/docker.go
@@ -415,7 +415,7 @@ func dockerNetworksReliably(c cluster.TestCluster) {
 		c.Fatal(err)
 	}
 
-	output, err := m.SSH(`seq 1 100 | xargs -i -n 1 -P 20 docker run ping sh -c 'out=$(ping -c 1 172.17.0.1 -w 1); if [[ "$?" != 0 ]]; then echo "{} FAIL"; echo "$out"; exit 1; else echo "{} PASS"; fi'`)
+	output, err := m.SSH(`seq 1 100 | xargs -i -n 1 -P 20 docker run ping sh -c 'out=$(ping -i 0.1 172.17.0.1 -w 1); if [[ "$?" != 0 ]]; then echo "{} FAIL"; echo "$out"; exit 1; else echo "{} PASS"; fi'`)
 	if err != nil {
 		c.Fatalf("could not run 100 containers pinging the bridge: %v: %q", err, string(output))
 	}
