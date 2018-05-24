2ca0cfc1c38b2aefc0e70f1d015690c37afde5c8
Jonathan Lebon <jonathan@jlebon.com>
2018-05-24 10:58:59 -0400

    Add ore to container
    This builds and installs mantle's `ore` binary as well as some other
    useful utilities like `jq` and `awscli`. Then we should be able to fully
    migrate the cloud pipeline.

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
 set -xeuo pipefail
+
 mkdir -p /usr/app/
 cd /usr/app/
 git clone https://github.com/ostreedev/ostree-releng-scripts
+
 cd /root/src
 ls -al
 cargo build --release
 mv target/release/coreos-assembler /usr/bin
 rm target -rf
+
+cd /root
+git clone https://github.com/coreos/mantle
+# for now just build ore, we can add more components as we use them
+mantle/build ore
+mv mantle/bin/ore /usr/bin
+rm mantle -rf
