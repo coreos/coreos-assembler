98db05446cbb223b8eaaad54e5075c432ede8375
Colin Walters <walters@verbum.org>
2018-05-26 11:00:35 -0400

    build: Squash layers and remove build dependencies
    This takes the image down from 1.4GB to 700MB.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 FROM registry.fedoraproject.org/fedora:28
-# rsync, python2, pygobject3-base are dependencies of ostree-releng-scripts
-# Also add python3 so people can use that too.
-RUN yum -y install rpm-ostree make cargo golang git jq \
-    rsync pygobject3-base python3-gobject-base awscli \
-    && yum clean all
 ADD build.sh /root
 RUN mkdir /root/src
-COPY Cargo.toml /root/src/
+COPY Makefile Cargo.toml /root/src/
 COPY src /root/src/src
 RUN ./root/build.sh && rm -f /root/build.sh # cache20180523
--- build.sh
+++ build.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/bash
 set -xeuo pipefail
 
+# rsync, python2, pygobject3-base are dependencies of ostree-releng-scripts
+# Also add python3 so people can use that too.
+yum -y install rpm-ostree make cargo golang git jq \
+    rsync pygobject3-base python3-gobject-base awscli
+
 mkdir -p /usr/app/
 cd /usr/app/
 git clone https://github.com/ostreedev/ostree-releng-scripts
 
 cd /root/src
 ls -al
+make build PREFIX=/usr/bin
 cargo build --release
 mv target/release/coreos-assembler /usr/bin
-rm target -rf
+cd /
+rm /root/src -rf
 
 cd /root
 git clone https://github.com/coreos/mantle
@@ -17,3 +24,7 @@ git clone https://github.com/coreos/mantle
 mantle/build ore
 mv mantle/bin/ore /usr/bin
 rm mantle -rf
+
+dnf remove -y cargo golang
+rpm -q grubby && dnf remove -y grubby
+yum clean all
