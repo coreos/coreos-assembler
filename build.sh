253ea874bf576bb2fc46eeae1e34fa6eb023e8d6
Colin Walters <walters@verbum.org>
2018-05-29 13:58:19 -0400

    build: Install createrepo_c+yum-utils for rojig
    Part of porting https://pagure.io/fedora-atomic-host-continuous

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ set -xeuo pipefail
 
 # rsync, python2, pygobject3-base are dependencies of ostree-releng-scripts
 # Also add python3 so people can use that too.
+# createrepo_c+yum-utils is used for managing rojig bits.
 yum -y install rpm-ostree make cargo golang git jq \
-    rsync pygobject3-base python3-gobject-base awscli
+    rsync pygobject3-base python3-gobject-base \
+    createrepo_c yum-utils
 
 mkdir -p /usr/app/
 cd /usr/app/
@@ -18,6 +20,8 @@ mv target/release/coreos-assembler /usr/bin
 cd /
 rm /root/src -rf
 
+# Part of general image management
+yum -y install awscli
 cd /root
 git clone https://github.com/coreos/mantle
 # for now just build ore, we can add more components as we use them
