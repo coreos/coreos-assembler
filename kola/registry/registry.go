b664735987e13b73785f61f5481015097dc14660
Yu Qi Zhang <jzehrarnyg@gmail.com>
2018-08-21 19:32:17 +0000

    kola/tests: add basic crio package
    Add Base (info, reliable network) and Network testing for cri-o for
    RHCOS. The tests are based on existing Docker tests, but adapted to
    crio pods/containers. The tests are network dependant due to the
    need of a kubernetes pause image.
    
    Signed-off-by: Steve Milner <smilner@redhat.com>
                   Yu Qi Zhang <jerzhang@redhat.com>

--- kola/registry/registry.go
+++ kola/registry/registry.go
@@ -3,6 +3,7 @@ package registry
 // Tests imported for registration side effects. These make up the OS test suite and is explicitly imported from the main package.
 import (
 	_ "github.com/coreos/mantle/kola/tests/coretest"
+	_ "github.com/coreos/mantle/kola/tests/crio"
 	_ "github.com/coreos/mantle/kola/tests/docker"
 	_ "github.com/coreos/mantle/kola/tests/etcd"
 	_ "github.com/coreos/mantle/kola/tests/flannel"
