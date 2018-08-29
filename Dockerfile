81bd1ae5e7ecdf0948ab613348f282fdaa49409f
Colin Walters <walters@verbum.org>
2018-08-28 21:31:26 -0400

    Introduce new coreos-assembler wrapper script, make ENTRYPOINT
    I regret the naming e.g. `coreos-oemid`; let's make things more
    verbose but also consistent by having `coreos-assembler` be
    an entrypoint with sub-commands.  Also, specifically for `oemid`,
    let's introduce a family of subcommands there prefixed with `gf-`
    for "guestfish".  So for example I'd like to add
    `coreos-assembler gf-mount`, etc.
    
    At some point soon I'll probably use a real programming language,
    but this low tech implementation gets us started.
    
    Having an `ENTRYPOINT` makes things nicer for invoking the container,
    though note it requires a workaround when using with Jenkins
    pipelines: https://github.com/openshift/os/pull/253

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM registry.fedoraproject.org/fedora:28
 WORKDIR /root/src
 COPY . /root/src
 RUN ./build.sh
+ENTRYPOINT ["/usr/bin/coreos-assembler"]
--- coreos-assembler.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This used to be a separate program, but is now
-# merged into rpm-ostree: https://github.com/projectatomic/rpm-ostree/pull/1377
-exec rpm-ostree compose tree "$@"
