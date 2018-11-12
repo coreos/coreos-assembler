82157b4ae6da5e39eca9913ae5206adf9fa6da2c
Colin Walters <walters@verbum.org>
2018-11-12 12:41:24 +0000

    buildextend-openstack: New command to add OpenStack image
    Building on the prior art of `buildextend-ec2`.  We really want
    a config file or arguments for `build` or so, but for now this
    fixes a regression in our switch to coreos-assembler for RHCOS
    in that we need an OpenStack image.

--- src/cmdlib.py
+++ src/cmdlib.py
@@ -1,6 +1,6 @@
 # Python version of cmdlib.sh
 
-import os,json,tempfile,subprocess
+import os,json,tempfile,subprocess,hashlib
 
 def run_verbose(args, **kwargs):
     print("+ {}".format(subprocess.list2cmdline(args)))
@@ -12,3 +12,10 @@ def write_json(path, data):
     json.dump(data, f)
     os.fchmod(f.file.fileno(), 0o644)
     os.rename(f.name, path)
+
+def sha256sum_file(filename):
+    h = hashlib.sha256()
+    with open(filename, 'rb', buffering=0) as f:
+        for b in iter(lambda: f.read(128 * 1024), b''):
+            h.update(b)
+    return h.hexdigest()
