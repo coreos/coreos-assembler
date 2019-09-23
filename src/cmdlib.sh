coreos/coreos-assembler

commit 6eb97016f4dab7d13aa00ae10846f26c1cd1cb02

build: Only prepare overlays when doing build, not fetch
I have `cosa fetch; and cosa build` in my shell history and use
it somewhat often.  We only need to commit the overlay directories
on actual builds, not fetch which is just about RPMs.  Mostly
doing this because:

 - The overlay commit is more visual noise
 - For `overrides/rootfs` it can be a nontrivial cost

src/cmdlib.sh
@@ -264,9 +264,13 @@ commit_overlay() {
         --timestamp "${git_timestamp}"
 }
 
-runcompose() {
-    # Implement support for automatic local overrides:
-    # https://github.com/coreos/coreos-assembler/issues/118
+# Implement support for automatic local overrides:
+# https://github.com/coreos/coreos-assembler/issues/118
+#
+# This function commits the contents of overlay.d/ as well
+# as overrides/{rootfs} to OSTree commits, and also handles
+# overrides/rpm.
+prepare_compose_overlays() {
     local overridesdir=${workdir}/overrides
     local tmp_overridesdir=${TMPDIR}/override
     local override_manifest="${tmp_overridesdir}"/coreos-assembler-override-manifest.yaml
@@ -362,7 +366,13 @@ ostree-override-layers:
   - cosa-bin-overlay
 EOF
     fi
+}
 
+# Wrapper for `rpm-ostree compose tree` which adds some default options
+# such as `--repo` (which is auto-derived from the builddir) and
+# `--unified-core` that we always want.  Also dispatches to supermin if
+# we're running without support for nested containerization.
+runcompose() {
     # shellcheck disable=SC2086
     set - ${COSA_RPMOSTREE_GDB:-} rpm-ostree compose tree --repo="${tmprepo}" \
             --cachedir="${workdir}"/cache --touch-if-changed "${changed_stamp}" \

src/cmd-build
@@ -204,6 +204,7 @@ fi
 composejson=${PWD}/tmp/compose.json
 # Put this under tmprepo so it gets automatically chown'ed if needed
 lockfile_out=${tmprepo}/tmp/manifest-lock.generated.${basearch}.json
+prepare_compose_overlays
 # --cache-only is here since `fetch` is a separate verb.
 # shellcheck disable=SC2086
 runcompose --cache-only ${FORCE} --add-metadata-from-json "${commitmeta_input_json}" \

commit 350829a9a30af44b8392e267f71241072497a98b

cmdlib: Fix RPM overrides handling
Minor regression from #771.

src/cmdlib.sh
@@ -338,9 +338,6 @@ for line in sys.stdin:
     lockfile["packages"][name] = {"evra": f"{evr}.{arch}"}
 json.dump(lockfile, sys.stdout)' > "${local_overrides_lockfile}"
 
-        # we need our overrides to be at the end of the list
-        set - "$@" --ex-lockfile="${local_overrides_lockfile}"
-
         echo "Using RPM overrides from: ${overridesdir}/rpm"
         touch "${overrides_active_stamp}"
         cat >> "${override_manifest}" <<EOF
@@ -373,6 +370,13 @@ EOF
 # `--unified-core` that we always want.  Also dispatches to supermin if
 # we're running without support for nested containerization.
 runcompose() {
+    local tmp_overridesdir=${TMPDIR}/override
+
+    if [ -f "${tmp_overridesdir}/local-overrides.json" ]; then
+        # we need our overrides to be at the end of the list
+        set - "$@" --ex-lockfile="${tmp_overridesdir}/local-overrides.json"
+    fi
+
     # shellcheck disable=SC2086
     set - ${COSA_RPMOSTREE_GDB:-} rpm-ostree compose tree --repo="${tmprepo}" \
             --cachedir="${workdir}"/cache --touch-if-changed "${changed_stamp}" \