1344a07cb611dc0c50928f1f2b18874901a608ab
Colin Walters <walters@verbum.org>
2019-10-04 20:58:57 +0000

    Remove support for "legacy" build schema
    The new build layout includes architecture-awareness.
    It was really nice that we supported both for a while, but
    RHCOS for 4.3 has switched to the new layout.  There's no
    reason to keep around the legacy build layout code, so remove
    it for at least three minutes of warm fuzzy "code deletion high".
    
    Also prep for work around failed builds handling.

--- src/cosalib/builds.py
+++ src/cosalib/builds.py
@@ -32,9 +32,9 @@ class Builds:  # pragma: nocover
         # we understand < 2.0.0 only
         if self._version._major >= 2:
             raise Exception("Builds schema too new; please update cosa")
-        # for now, since we essentially just support "1.0.0" and "0.0.1",
-        # just dillute to a bool
-        self._legacy = (self._version._major < 1)
+        if self._version._major < 1:
+            err = f"Unsupported build metadata version {self._version}"
+            raise SystemExit(err)
 
     def _path(self, path):
         if not self._workdir:
@@ -42,8 +42,6 @@ class Builds:  # pragma: nocover
         return os.path.join(self._workdir, path)
 
     def has(self, build_id):
-        if self._legacy:
-            return build_id in self._data['builds']
         return any([b['id'] == build_id for b in self._data['builds']])
 
     def is_empty(self):
@@ -51,12 +49,9 @@ class Builds:  # pragma: nocover
 
     def get_latest(self):
         # just let throw if there are none
-        if self._legacy:
-            return self._data['builds'][0]
         return self._data['builds'][0]['id']
 
     def get_build_arches(self, build_id):
-        assert not self._legacy
         for build in self._data['builds']:
             if build['id'] == build_id:
                 return build['arches']
@@ -65,41 +60,33 @@ class Builds:  # pragma: nocover
     def get_build_dir(self, build_id, basearch=None):
         if build_id == 'latest':
             build_id = self.get_latest()
-        if self._legacy:
-            return self._path(f"builds/{build_id}")
         if not basearch:
             # just assume caller wants build dir for current arch
             basearch = get_basearch()
         return self._path(f"builds/{build_id}/{basearch}")
 
     def insert_build(self, build_id, basearch=None):
-        if self._legacy:
-            self._data['builds'].insert(0, build_id)
+        if not basearch:
+            basearch = get_basearch()
+        # for future tooling: allow inserting in an existing build for a
+        # separate arch
+        for build in self._data['builds']:
+            if build['id'] == build_id:
+                if basearch in build['arches']:
+                    raise "Build {build_id} for {basearch} already exists"
+                build['arches'] += [basearch]
+                break
         else:
-            if not basearch:
-                basearch = get_basearch()
-            # for future tooling: allow inserting in an existing build for a
-            # separate arch
-            for build in self._data['builds']:
-                if build['id'] == build_id:
-                    if basearch in build['arches']:
-                        raise "Build {build_id} for {basearch} already exists"
-                    build['arches'] += [basearch]
-                    break
-            else:
-                self._data['builds'].insert(0, {
-                    'id': build_id,
-                    'arches': [
-                        basearch
-                    ]
-                })
+            self._data['builds'].insert(0, {
+                'id': build_id,
+                'arches': [
+                    basearch
+                ]
+            })
 
     def bump_timestamp(self):
         self._data['timestamp'] = rfc3339_time()
         self.flush()
-
-    def is_legacy(self):
-        return self._legacy
 
     def raw(self):
         return self._data
