c1327a77b4dc048070652bc3c65e562562da6736
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-10-10 16:27:08 +0200

    cosalib: create libs for s3 and ec2 operations
    These libraries contain code to work on s3 buckets (head, list, delete)
    and ec2 (deregister images and snapshots)

---
+++ src/cosalib/aws.py
@@ -0,0 +1,19 @@
+import boto3
+from cosalib.cmdlib import (
+    retry_stop,
+    retry_boto_exception,
+    retry_callback
+)
+from tenacity import retry
+
+
+@retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
+def deregister_ami(ami_id, region):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.deregister_image(ImageId=ami_id)
+
+
+@retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
+def delete_snapshot(snap_id, region):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.delete_snapshot(SnapshotId=snap_id)
