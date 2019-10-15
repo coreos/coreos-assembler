3d2bf40ffce54c02253f57b1a0d3442ba03b02d5
Benjamin Gilbert <bgilbert@redhat.com>
2019-10-15 09:54:32 -0400

    *: drop legacy names for Packet plans
    Drop bogus "baremetal_3a" plan for s390x.

--- cmd/ore/packet/create-device.go
+++ cmd/ore/packet/create-device.go
@@ -40,7 +40,7 @@ var (
 func init() {
 	Packet.AddCommand(cmdCreateDevice)
 	cmdCreateDevice.Flags().StringVar(&options.Facility, "facility", "sjc1", "facility code")
-	cmdCreateDevice.Flags().StringVar(&options.Plan, "plan", "", "plan slug (default board-dependent, e.g. \"baremetal_0\")")
+	cmdCreateDevice.Flags().StringVar(&options.Plan, "plan", "", "plan slug (default board-dependent, e.g. \"t1.small.x86\")")
 	cmdCreateDevice.Flags().StringVar(&options.Board, "board", "amd64-usr", "Container Linux board")
 	cmdCreateDevice.Flags().StringVar(&options.InstallerImageBaseURL, "installer-image-base-url", "", "installer image base URL, non-https (default board-dependent, e.g. \"http://stable.release.core-os.net/amd64-usr/current\")")
 	cmdCreateDevice.Flags().StringVar(&options.ImageURL, "image-url", "", "image base URL (default board-dependent, e.g. \"https://alpha.release.core-os.net/amd64-usr/current/coreos_production_packet_image.bin.bz2\")")
