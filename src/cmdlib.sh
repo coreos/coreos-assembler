dce29f6fb2315dfddc98d52b041e957bd82ffb4f
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-10-16 15:22:58 +0200

    cmdlib: set size to "unknown" if stat in prepare_git_artifacts cannot be performed

--- src/cmdlib.sh
+++ src/cmdlib.sh
@@ -624,7 +624,7 @@ prepare_git_artifacts() {
         "checksum_type": "sha256",
         "format": "tar.gz",
         "name": "$(basename ${tarball})",
-        "size": $(stat --format=%s ${tarball})
+        "size": $(stat --format=%s ${tarball} || echo unknown)
     }
 }
 EOC
