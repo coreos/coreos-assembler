f2650312d79298e4879890434335bebbc48d4afd
Jonathan Lebon <jonathan@jlebon.com>
2020-01-17 17:23:49 -0500

    cosa/build: fix AMIs spec
    The `amis` element is an array of objects, not an object. While we're
    here, add the region field.

--- cosa/build.go
+++ cosa/build.go
@@ -19,8 +19,9 @@ package cosa
 // https://github.com/openshift/installer/blob/a0350404997b0493d7bb16aa2875e5c42879b069/pkg/rhcos/builds.go
 // For now, copy-paste updates there.  Later, maybe consider making this a public API?
 type Build struct {
-	AMIs map[string]struct {
-		HVM string `json:"hvm"`
+	AMIs          []struct {
+		Region string `json:"name"`
+		HVM    string `json:"hvm"`
 	} `json:"amis"`
 	Azure struct {
 		Image string `json:"image"`
