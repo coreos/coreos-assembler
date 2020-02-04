18ebd410270a852614d299c4df19cc8018df8d80
Luca BRUNO <luca.bruno@coreos.com>
2020-02-04 09:36:50 +0000

    qemuvariants: add ibmcloud
    This adds a new qcow2-based image for the `ibmcloud` platform,
    which can be booted on IBM Cloud Gen 2.

--- src/cosalib/qemuvariants.py
+++ src/cosalib/qemuvariants.py
@@ -56,10 +56,6 @@ VARIANTS = {
         "image_suffix": "vhd",
         "platform": "azure",
     },
-    "openstack": {
-        "image_format": "qcow2",
-        "platform": "openstack",
-    },
     "gcp": {
         # See https://cloud.google.com/compute/docs/import/import-existing-image#requirements_for_the_image_file
         "image_format": "raw",
@@ -76,6 +72,14 @@ VARIANTS = {
             "-z",
             "--format=oldgnu"
         ]
+    },
+    "ibmcloud": {
+        "image_format": "qcow2",
+        "platform": "ibmcloud",
+    },
+    "openstack": {
+        "image_format": "qcow2",
+        "platform": "openstack",
     },
     "vmware_vmdk": {
         "image_format": "vmdk",
