f677b2c010a8e5c7792868001e24b410a3797a50
Jonathan Lebon <jonathan@jlebon.com>
2020-04-17 16:40:12 -0400

    cmd-build: add --strict for building in strict mode
    This makes use of rpm-ostree's new `--ex-lockfile-strict`. We really
    want this for FCOS CI and release builds. For more background, see:
    
    https://github.com/coreos/fedora-coreos-tracker/issues/454
    
    This also drops the printing of lockfiles and the `sleep 1` bit,
    since... I think by now people should be pretty familiar with lockfiles
    and rpm-ostree prints them already (and it's easily viewable while
    libsolv is loading its cache).

--- tests/test_pruning.sh
+++ tests/test_pruning.sh
@@ -9,9 +9,9 @@ set -xeuo pipefail
 # don't actually ask for a image rebuild since we use `ostree`.
 # FIXME: Add env COSA_BUILD_DUMMY=true or something instead of using this subtle
 # hack. Or better add `cosa build --shortcut=overrides` or so.
-cosa build ostree --force-image
-cosa build ostree --force-image
-cosa build ostree --force-image
+cosa build ostree --force-image --strict
+cosa build ostree --force-image --strict
+cosa build ostree --force-image --strict
 jq -e '.builds|length == 3' builds/builds.json
 jq -e '.builds[2].id | endswith("0-1")' builds/builds.json
 
@@ -21,10 +21,10 @@ cosa prune --build="${latest}"
 # And validate it
 cosa meta --get ostree-version>/dev/null
 # Another build to get back to previous state
-cosa build ostree --force-image
+cosa build ostree --force-image --strict
 
 # Test --skip-prune
-cosa build ostree --force-image --skip-prune
+cosa build ostree --force-image --skip-prune --strict
 jq -e '.builds|length == 4' builds/builds.json
 jq -e '.builds[3].id | endswith("0-1")' builds/builds.json
 
