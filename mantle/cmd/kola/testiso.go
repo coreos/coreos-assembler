7a2822b40539e03d38160bbaaa1877d17c5be461
Colin Walters <walters@verbum.org>
2020-04-23 23:58:42 +0000

    testiso: Accept zero args
    I added `--pxe-kargs` and then accidentally edited my command
    history to omit it but passed a value, and was confused
    why it wasn't working.

--- mantle/cmd/kola/testiso.go
+++ mantle/cmd/kola/testiso.go
@@ -104,6 +104,7 @@ func init() {
 	cmdTestIso.Flags().StringSliceVar(&pxeKernelArgs, "pxe-kargs", nil, "Additional kernel arguments for PXE")
 	// FIXME move scenarioISOLiveLogin into the defaults once https://github.com/coreos/fedora-coreos-config/pull/339#issuecomment-613000050 is fixed
 	cmdTestIso.Flags().StringSliceVar(&scenarios, "scenarios", []string{scenarioPXEInstall, scenarioISOInstall}, fmt.Sprintf("Test scenarios (also available: %v)", []string{scenarioLegacyInstall, scenarioISOLiveLogin}))
+	cmdTestIso.Args = cobra.ExactArgs(0)
 
 	root.AddCommand(cmdTestIso)
 }
