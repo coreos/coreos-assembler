coreos/coreos-assembler

commit e1d5c2cc4fd104a71feb3d10c5384724fcb01f54

kola: Support distros and tags in external test metadata
We should have symmetrical support for metadata in internal/external
tests.

mantle/kola/harness.go
@@ -471,6 +471,8 @@ func RunUpgradeTests(patterns []string, pltfrm, outputDir string, propagateTestE
 type externalTestMeta struct {
 	Architectures string `json:",architectures,omitempty"`
 	Platforms     string `json:",platforms,omitempty"`
+	Distros       string `json:",distros,omitempty"`
+	Tags          string `json:",tags,omitempty"`
 }
 
 func registerExternalTest(testname, executable, dependencydir, ignition string, meta externalTestMeta) error {
@@ -578,6 +580,12 @@ RequiredBy=multi-user.target
 	} else {
 		t.Platforms = strings.Fields(meta.Platforms)
 	}
+	if strings.HasPrefix(meta.Distros, "!") {
+		t.ExcludeDistros = strings.Fields(meta.Distros[1:])
+	} else {
+		t.Distros = strings.Fields(meta.Distros)
+	}
+	t.Tags = strings.Fields(meta.Tags)
 
 	register.RegisterTest(t)
 

commit f420cbbf4a49ac70ff1e27439eb8d5a841995025

kola: Avoid losing "external" tag
This is a regression from https://github.com/coreos/coreos-assembler/pull/1440
which is breaking the Ignition CI here
https://jenkins-coreos-ci.apps.ci.centos.org/job/github-ci/job/coreos/job/ignition/job/PR-968/35/console

mantle/kola/harness.go
@@ -585,7 +585,7 @@ RequiredBy=multi-user.target
 	} else {
 		t.Distros = strings.Fields(meta.Distros)
 	}
-	t.Tags = strings.Fields(meta.Tags)
+	t.Tags = append(t.Tags, strings.Fields(meta.Tags)...)
 
 	register.RegisterTest(t)
 