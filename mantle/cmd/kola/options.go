coreos/coreos-assembler

commit 51d94a39a1c97dd3249e5738d3a14e896bc45f02

kola: Make --qemu-iso automatically imply -p qemu-iso
`cosa run -p qemu-iso --qemu-iso fedora-coreos-32.20200715.2.2-live.x86_64.iso`
has at least one too many `.iso`.

mantle/cmd/kola/options.go
@@ -48,7 +48,7 @@ func init() {
 
 	// general options
 	sv(&outputDir, "output-dir", "", "Temporary output directory for test data and logs")
-	root.PersistentFlags().StringVarP(&kolaPlatform, "platform", "p", "qemu-unpriv", "VM platform: "+strings.Join(kolaPlatforms, ", "))
+	root.PersistentFlags().StringVarP(&kolaPlatform, "platform", "p", "", "VM platform: "+strings.Join(kolaPlatforms, ", "))
 	root.PersistentFlags().StringVarP(&kola.Options.Distribution, "distro", "b", "", "Distribution: "+strings.Join(kolaDistros, ", "))
 	root.PersistentFlags().IntVarP(&kola.TestParallelism, "parallel", "j", 1, "number of tests to run in parallel")
 	sv(&kola.TAPFile, "tapfile", "", "file to write TAP results to")
@@ -157,6 +157,13 @@ func syncOptionsImpl(useCosa bool) error {
 			}
 		}
 		return fmt.Errorf("unsupported %v %q", name, item)
+	}
+
+	// TODO: Could also auto-synchronize if e.g. --aws-ami is passed
+	if kolaPlatform == "" && kola.QEMUIsoOptions.IsoPath != "" {
+		kolaPlatform = "qemu-iso"
+	} else {
+		kolaPlatform = "qemu-unpriv"
 	}
 
 	// There used to be a "privileged" qemu path, it is no longer supported.

commit 7d91fcfe18778e23c1d4bf28d50c4a9ec2ae46d4

mantle: kola: stop ignoring user provided --platform option
This fixes a regression from 51d94a3 where the kolaPlatform would
get set to `qemu-unpriv` even if the user had passed --platform gce
or any other valid value. I noticed that the GCE and AWS tests started
failing with a weird error like:

```
--- FAIL: podman.network-single (0.01s)
        harness.go:900: Cluster failed starting machines: lstat
        /home/jenkins/workspace/fedora-coreos/fedora-coreos-fedora-coreos-pipeline-kola-aws/builds/32.20200725.20.0/x86_64/fedora-coreos-32.20200725.20.0-qemu.x86_64.qcow2.xz:
        no such file or directory
```

It's because they were inappropriately trying to execute qemu tests. The
error message we got is because the qemu artifact didn't exist in the local
test environment because we don't sync them down (they're not necessary).

mantle/cmd/kola/options.go
@@ -160,10 +160,12 @@ func syncOptionsImpl(useCosa bool) error {
 	}
 
 	// TODO: Could also auto-synchronize if e.g. --aws-ami is passed
-	if kolaPlatform == "" && kola.QEMUIsoOptions.IsoPath != "" {
-		kolaPlatform = "qemu-iso"
-	} else {
-		kolaPlatform = "qemu-unpriv"
+	if kolaPlatform == "" {
+		if kola.QEMUIsoOptions.IsoPath != "" {
+			kolaPlatform = "qemu-iso"
+		} else {
+			kolaPlatform = "qemu-unpriv"
+		}
 	}
 
 	// There used to be a "privileged" qemu path, it is no longer supported.