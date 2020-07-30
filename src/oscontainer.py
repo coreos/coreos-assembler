coreos/coreos-assembler

commit 237b6d7e1a23fc98e89aa708b0dd7ba8f5c178f6

Add cosa commit and rhcos commit as labels in oscontainer
Tackle this issue https://issues.redhat.com/browse/GRPA-2086.

src/oscontainer.py
@@ -21,6 +21,7 @@ import shutil
 import subprocess
 from functools import wraps
 from time import sleep
+from cosalib import cmdlib
 
 OSCONTAINER_COMMIT_LABEL = 'com.coreos.ostree-commit'
 
@@ -195,6 +196,19 @@ def oscontainer_build(containers_storage, tmpdir, src, ref, image_name_and_tag,
                   '-l', OSCONTAINER_COMMIT_LABEL + '=' + rev]
         if ostree_version is not None:
             config += ['-l', 'version=' + ostree_version]
+
+        with open('builds/builds.json') as fb:
+            builds = json.load(fb)['builds']
+        latest_build = builds[0]['id']
+        arch = cmdlib.get_basearch()
+        metapath = f"builds/{latest_build}/{arch}/meta.json"
+        with open(metapath) as f:
+            meta = json.load(f)
+        rhcos_commit = meta['coreos-assembler.container-config-git']['commit']
+        cosa_commit = meta['coreos-assembler.container-image-git']['commit']
+        config += ['-l', f"com.coreos.coreos-assembler-commit={cosa_commit}"]
+        config += ['-l', f"com.coreos.redhat-coreos-commit={rhcos_commit}"]
+
         if display_name is not None:
             config += ['-l', 'io.openshift.build.version-display-names=machine-os=' + display_name,
                        '-l', 'io.openshift.build.versions=machine-os=' + ostree_version]

commit b2ca61ecb2824b8fbbbf6c6b2f969a6557389966

oscontainer: Skip cosa commit if not present
I regularly develop on coreos-assembler outside of a container,
this key needs to be optional.

Regression from 237b6d7e1a23fc98e89aa708b0dd7ba8f5c178f6

See also e.g. ded997b8634b0026899306d86d7ce688d0934bee

src/oscontainer.py
@@ -205,8 +205,10 @@ def oscontainer_build(containers_storage, tmpdir, src, ref, image_name_and_tag,
         with open(metapath) as f:
             meta = json.load(f)
         rhcos_commit = meta['coreos-assembler.container-config-git']['commit']
-        cosa_commit = meta['coreos-assembler.container-image-git']['commit']
-        config += ['-l', f"com.coreos.coreos-assembler-commit={cosa_commit}"]
+        imagegit = meta.get('coreos-assembler.container-image-git')
+        if imagegit is not None:
+            cosa_commit = imagegit['commit']
+            config += ['-l', f"com.coreos.coreos-assembler-commit={cosa_commit}"]
         config += ['-l', f"com.coreos.redhat-coreos-commit={rhcos_commit}"]
 
         if display_name is not None: