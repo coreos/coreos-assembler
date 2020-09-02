coreos/coreos-assembler

commit f03fcd8c76f2530df89a1353c16eb9d368862483

mantle/platform/conf: Convenience function to add console auto login

mantle/platform/conf/conf.go
@@ -978,6 +978,19 @@ func (c *Conf) IsEmpty() bool {
 	return !c.IsIgnition()
 }
 
+func getAutologinUnit(name, args string) string {
+	return fmt.Sprintf(`[Service]
+	ExecStart=
+	ExecStart=-/sbin/agetty --autologin core -o '-p -f core' %s %%I $TERM
+	`, args)
+}
+
+// AddAutoLogin adds an Ignition config for automatic login on consoles
+func (c *Conf) AddAutoLogin() {
+	c.AddSystemdUnit("getty@.service", getAutologinUnit("getty@.service", "--noclear"), Enable)
+	c.AddSystemdUnit("serial-getty@.service", getAutologinUnit("serial-getty@.service", "--keep-baud 115200,38400,9600"), Enable)
+}
+
 func getAutologinFragment(name, args string) v3types.Unit {
 	contents := fmt.Sprintf(`[Service]
 ExecStart=

commit 97dca701d6c579326a410fe6bb81eae257d6dec8

mantle/platform/conf: Use droppins in 'AddAutoLogin' wrapper
The conversion to a spec generic version incorrectly used system unit
directly instead of creating config droppins.

Fixes: f03fcd8c mantle/platform/conf: Convenience function to add console auto login

mantle/platform/conf/conf.go
@@ -1118,8 +1118,8 @@ func getAutologinUnit(name, args string) string {
 
 // AddAutoLogin adds an Ignition config for automatic login on consoles
 func (c *Conf) AddAutoLogin() {
-	c.AddSystemdUnit("getty@.service", getAutologinUnit("getty@.service", "--noclear"), Enable)
-	c.AddSystemdUnit("serial-getty@.service", getAutologinUnit("serial-getty@.service", "--keep-baud 115200,38400,9600"), Enable)
+	c.AddSystemdUnitDropin("getty@.service", "10-autologin.conf", getAutologinUnit("getty@.service", "--noclear"))
+	c.AddSystemdUnitDropin("serial-getty@.service", "10-autologin.conf", getAutologinUnit("serial-getty@.service", "--keep-baud 115200,38400,9600"))
 }
 
 func getAutologinFragment(name, args string) v3types.Unit {