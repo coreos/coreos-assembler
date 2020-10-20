cfcddd36c922b1160ed9b81d6162ae78786aae88
Dusty Mabe <dusty@dustymabe.com>
2020-10-20 14:30:50 -0400

    mantle: stop excluding openstack from fcos.ignition.v3.noop test
    On OpenStack we should be picking up the keys from afterburn now
    that https://github.com/coreos/fedora-coreos-tracker/issues/633
    is fixed.

--- mantle/kola/tests/ignition/empty.go
+++ mantle/kola/tests/ignition/empty.go
@@ -37,7 +37,7 @@ func init() {
 		Name:             "fcos.ignition.v3.noop",
 		Run:              empty,
 		ClusterSize:      1,
-		ExcludePlatforms: []string{"qemu", "esx", "openstack"},
+		ExcludePlatforms: []string{"qemu", "esx"},
 		Distros:          []string{"fcos"},
 		Flags:            []register.Flag{register.NoSSHKeyInUserData},
 		UserData:         conf.Ignition(`{"ignition":{"version":"3.0.0"}}`),
