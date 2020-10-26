c899ca285c7af37154b5776f8a2f5244b767d46c
Dusty Mabe <dusty@dustymabe.com>
2020-10-26 15:49:41 -0400

    mantle: switch to v1 for gcloud compute API
    The update image API call that we need (see de57130, 728e0d5,
    6df34a2, f0427bd) is now in the stable v1 API so let's update the
    library so we know how to talk to the v1 API for that functionality.
    
    Note: ran `go mod vendor` to pick up the v1 API files.

--- mantle/cmd/plume/release.go
+++ mantle/cmd/plume/release.go
@@ -29,7 +29,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/spf13/cobra"
 	"golang.org/x/net/context"
-	"google.golang.org/api/compute/v0.beta"
+	"google.golang.org/api/compute/v1"
 	gs "google.golang.org/api/storage/v1"
 
 	"github.com/coreos/mantle/fcos"
