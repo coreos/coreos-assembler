coreos/coreos-assembler

commit c7920933a2d80a20af5c8e5642a9de34d7c032ed

mantle: clean up cmd/ore/gcloud/upload
This cleans up cmd/ore/gcloud/upload.go.

Fixes part of https://github.com/coreos/coreos-assembler/issues/1813

mantle/cmd/ore/gcloud/upload.go
@@ -15,6 +15,7 @@
 package gcloud
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/url"
@@ -50,11 +51,17 @@ var (
 
 func init() {
 	cmdUpload.Flags().StringVar(&uploadBucket, "bucket", "", "gs://bucket/prefix/")
-	cmdUpload.MarkFlagRequired("bucket")
+	if err := cmdUpload.MarkFlagRequired("bucket"); err != nil {
+		panic(err)
+	}
 	cmdUpload.Flags().StringVar(&uploadImageName, "name", "", "name for uploaded image")
-	cmdUpload.MarkFlagRequired("name")
+	if err := cmdUpload.MarkFlagRequired("name"); err != nil {
+		panic(err)
+	}
 	cmdUpload.Flags().StringVar(&uploadFile, "file", "", "path to image .tar.gz file to upload")
-	cmdUpload.MarkFlagRequired("file")
+	if err := cmdUpload.MarkFlagRequired("file"); err != nil {
+		panic(err)
+	}
 	cmdUpload.Flags().BoolVar(&uploadForce, "force", false, "overwrite existing GS and GCE images without prompt")
 	cmdUpload.Flags().StringVar(&uploadWriteUrl, "write-url", "", "output the uploaded URL to the named file")
 	cmdUpload.Flags().StringVar(&uploadImageFamily, "family", "", "GCP image family to attach image to")
@@ -97,7 +104,8 @@ func runUpload(cmd *cobra.Command, args []string) {
 	// Sanitize the image name for GCE
 	imageNameGCE := gceSanitize(uploadImageName)
 
-	storageAPI, err := storage.New(api.Client())
+	ctx := context.Background()
+	storageAPI, err := storage.NewService(ctx)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Storage client failed: %v\n", err)
 		os.Exit(1)

commit cb06d3d95f4dfe2f077522b19227e38ff19b1f8d

ore/gcloud/upload: fix GCP authentication
Minor regression from #1864. We lost the bit which passes the
authentication info to GCP API. Restore it using new not-deprecated API.

mantle/cmd/ore/gcloud/upload.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"google.golang.org/api/googleapi"
+	"google.golang.org/api/option"
 	"google.golang.org/api/storage/v1"
 
 	"github.com/coreos/mantle/platform/api/gcloud"
@@ -105,7 +106,7 @@ func runUpload(cmd *cobra.Command, args []string) {
 	imageNameGCE := gceSanitize(uploadImageName)
 
 	ctx := context.Background()
-	storageAPI, err := storage.NewService(ctx)
+	storageAPI, err := storage.NewService(ctx, option.WithHTTPClient(api.Client()))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Storage client failed: %v\n", err)
 		os.Exit(1)