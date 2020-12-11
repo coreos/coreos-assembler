185ad3ab8bc9ba0ead52a92e3b893478a5b17a71
bh7cw <jingzhan@redhat.com>
2020-12-11 09:31:17 -0500

    mantle: clean up cmd/ore/gcloud/promote-image.go
    This cleans up cmd/ore/gcloud/promote-image.go.
    
    Fixes part of https://github.com/coreos/coreos-assembler/issues/1813

--- mantle/cmd/ore/gcloud/promote-image.go
+++ mantle/cmd/ore/gcloud/promote-image.go
@@ -92,6 +92,9 @@ func runPromoteImage(cmd *cobra.Command, args []string) {
 			continue
 		}
 		// Some debug messages which are useful when needed.
+		//nolint // Add nolint to pass golang-ci lint,
+		// drop it when the error is gone, it's already fixed
+		// see https://github.com/googleapis/google-api-go-client/issues/767
 		if image.Deprecated != nil {
 			plog.Debugf("Deprecation state for %v is %v",
 				image.Name, image.Deprecated.State)
