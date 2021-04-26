coreos/coreos-assembler

commit 06fe78adbc16e1a7a737b0e4d7cb5d355c822b67

Gangplank: add podless mode
Instead of having a useless "runCmd" target, this change enables
"podless" more for executing Gangplank directly. The use cases are:
- developers testing inside a pet-container for JobSpec testing
- Prow (while we tease out permissions)

Example invocation:
  gangplank podless --init -A base -A metal4k

When running in podless mode Gangplank only parse the jobspec and
executes the task.

Signed-off-by: Ben Howard <ben.howard@redhat.com>

gangplank/cmd/generate.go
@@ -63,8 +63,24 @@ func init() {
 	cmdGenerateSingle.Flags().StringSliceVar(&generateSingleRequires, "req", []string{}, "artifacts to require")
 }
 
-// generateJobSpec creates a jobspec from the commandline.
-func generateJobSpec() {
+// setCliSpec reads or generates a jobspec based on CLI arguments.
+func setCliSpec() {
+	if specFile != "" {
+		log.WithFields(log.Fields{
+			"jobspec":          specFile,
+			"ingored cli args": "-A|--artifact|--singleReq|--singleCmd",
+		}).Info("Using jobspec from file, some cli arguments will be ignored")
+		if spec.Recipe.Repos == nil {
+			spec.AddRepos()
+		}
+		return
+	}
+
+	log.Info("Generating jobspec from CLI arguments")
+	if generateCommands != nil || generateSingleRequires != nil {
+		log.Info("--cmd and --req forces single stage mode, only one stage will be run")
+		generateSingleStage = true
+	}
 	if specFile != "" {
 		js, err := jobspec.JobSpecFromFile(specFile)
 		if err != nil {
@@ -104,7 +120,7 @@ func generateCLICommand(*cobra.Command, []string) {
 		defer f.Close()
 		out = f
 	}
-	generateJobSpec()
+	setCliSpec()
 	defer out.Sync() //nolint
 
 	now := time.Now().Format(time.RFC3339)

gangplank/cmd/commonflags.go
@@ -1,0 +1,14 @@
+package main
+
+import (
+	flag "github.com/spf13/pflag"
+)
+
+// Flags has the configuration flags.
+var specCommonFlags = flag.NewFlagSet("", flag.ContinueOnError)
+
+func init() {
+	specCommonFlags.StringSliceVar(&generateCommands, "singleCmd", []string{}, "commands to run in stage")
+	specCommonFlags.StringSliceVar(&generateSingleRequires, "singleReq", []string{}, "artifacts to require")
+	specCommonFlags.StringVarP(&cosaSrvDir, "srvDir", "S", "", "directory for /srv; in pod mount this will be bind mounted")
+}

gangplank/cmd/main.go
@@ -60,13 +60,16 @@ Wrapper for COSA commands and templates`,
 		Run:   runSingle,
 	}
 
-	cmdSteps = &cobra.Command{
-		Use:          "run-scripts",
-		Short:        "Run Steps from [file]",
-		Args:         cobra.MinimumNArgs(1),
-		RunE:         runScripts,
-		SilenceUsage: true,
+	cmdPodless = &cobra.Command{
+		Use:   "podless",
+		Short: "Run outisde of pod (via prow/ci)",
+		RunE:  runSpecLocally,
 	}
+)
+
+var (
+	// cosaInit indicates that cosa init should be run
+	cosaInit bool
 )
 
 func init() {
@@ -90,12 +93,13 @@ func init() {
 	cmdRoot.PersistentFlags().StringVarP(&specFile, "spec", "s", "", "location of the spec")
 	cmdRoot.AddCommand(cmdVersion)
 	cmdRoot.AddCommand(cmdSingle)
+	cmdRoot.Flags().StringVarP(&specFile, "spec", "s", "", "location of the spec")
 	spec.AddCliFlags(cmdRoot.PersistentFlags())
 
-	// cmdStep options
-	cmdRoot.AddCommand(cmdSteps)
-	cmdSteps.Flags().StringVarP(&specFile, "spec", "s", "", "location of the spec")
-	cmdSteps.Flags().StringArrayVarP(&shellCmd, "shell", "S", shellCmd, "shellcommand to execute")
+	// cmdPodless options
+	cmdRoot.AddCommand(cmdPodless)
+	cmdPodless.Flags().AddFlagSet(specCommonFlags)
+	cmdPodless.Flags().BoolVar(&cosaInit, "init", false, "force initialize srv dir")
 }
 
 func main() {
@@ -106,14 +110,39 @@ func main() {
 	os.Exit(0)
 }
 
-// runScripts reads ARGs as files and executes the rendered templates.
-func runScripts(c *cobra.Command, args []string) error {
+// runSpecLocally executes a jobspec locally.
+func runSpecLocally(c *cobra.Command, args []string) error {
+	myDir, _ := os.Getwd()
+	defer func() {
+		ctx.Done()
+		_ = os.Chdir(myDir)
+	}()
+
+	if _, err := os.Stat("src"); err != nil || cosaInit {
+		log.WithField("dir", cosaSrvDir).Info("Initalizing Build Tree")
+		spec.Stages = append(spec.Stages, jobspec.Stage{
+			ID:             "Initialization",
+			ExecutionOrder: 0,
+			Commands: []string{
+				"cosa init --force --branch {{ .JobSpec.Recipe.GitRef }} --force {{ .JobSpec.Recipe.GitURL}} ",
+			},
+		})
+	}
+
+	setCliSpec()
 	rd := &jobspec.RenderData{
 		JobSpec: &spec,
 	}
-	if err := rd.RendererExecuter(ctx, envVars, args...); err != nil {
-		log.Fatalf("Failed to execute scripts: %v", err)
+
+	if err := os.Chdir(cosaSrvDir); err != nil {
+		log.WithError(err).Fatal("failed to change to srv dir")
 	}
+	for _, stage := range spec.Stages {
+		if err := stage.Execute(ctx, rd, envVars); err != nil {
+			log.WithError(err).Fatal("failed to execute job")
+		}
+	}
+
 	log.Infof("Execution complete")
 	return nil
 }
@@ -138,10 +167,6 @@ func runSingle(c *cobra.Command, args []string) {
 
 // preRun processes the spec file.
 func preRun(c *cobra.Command, args []string) {
-	if specFile == "" {
-		return
-	}
-
 	// Terminal "keep alive" helper. When following logs via the `oc` commands,
 	// cloud-deployed instances will send an EOF. To get around the EOF, the func sends a
 	// null character that is not printed to the screen or reflected in the logs.
@@ -156,11 +181,4 @@ func preRun(c *cobra.Command, args []string) {
 			}
 		}
 	}()
-
-	ns, err := jobspec.JobSpecFromFile(specFile)
-	if err != nil {
-		log.WithFields(log.Fields{"input file": specFile, "error": err}).Fatal(
-			"Failed reading file")
-	}
-	spec = ns
 }

gangplank/cmd/pod.go
@@ -30,9 +30,6 @@ var (
 	// and reading of the secrets.
 	serviceAccount string
 
-	// cosaCmds is used to define the commands to run
-	cosaCmds []string
-
 	// Run CI pod via podman (out of cluster)
 	cosaViaPodman bool
 
@@ -62,17 +59,13 @@ func init() {
 	spec.AddCliFlags(cmdPod.Flags())
 	cmdPod.Flags().BoolVar(&cosaWorkDirContext, "setWorkDirCtx", false, "set workDir's selinux content")
 	cmdPod.Flags().BoolVarP(&cosaViaPodman, "podman", "", false, "use podman to execute task")
-	cmdPod.Flags().StringSliceVarP(&cosaCmds, "cmd", "c", []string{}, "commands to run")
 	cmdPod.Flags().StringVarP(&cosaImage, "image", "i", "", "use an alternative image")
-	cmdPod.Flags().StringVarP(&cosaSrvDir, "srvDir", "S", "", "podman mode - directory to mount as /srv")
 	cmdPod.Flags().StringVarP(&cosaWorkDir, "workDir", "w", "", "podman mode - workdir to use")
 	cmdPod.Flags().StringVarP(&serviceAccount, "serviceaccount", "a", "", "service account to use")
 
 	cmdPod.Flags().StringVarP(&remoteKubeConfig, "remoteKubeConfig", "R", "", "launch COSA in a remote cluster")
 	cmdPod.Flags().StringVarP(&cosaNamespace, "namespace", "N", "", "use a different namespace")
-	cmdPod.Flags().StringSliceVar(&generateCommands, "singleCmd", []string{}, "commands to run in stage")
-	cmdPod.Flags().StringSliceVar(&generateSingleRequires, "singleReq", []string{}, "artifacts to require")
-
+	cmdPod.Flags().AddFlagSet(specCommonFlags)
 }
 
 // runPod is the Jenkins/CI interface into Gangplank. It "mocks"
@@ -108,24 +101,7 @@ func runPod(c *cobra.Command, args []string) {
 	}
 
 	clusterCtx := ocp.NewClusterContext(ctx, cluster)
-
-	if specFile != "" {
-		log.WithFields(log.Fields{
-			"jobspec":          specFile,
-			"ingored cli args": "-A|--artifact|--singleReq|--singleCmd",
-		}).Info("Using jobspec from file, some cli arguments will be ignored")
-		if spec.Recipe.Repos == nil {
-			spec.AddRepos()
-		}
-	} else {
-		log.Info("Generating jobspec from CLI arguments")
-		if generateCommands != nil || generateSingleRequires != nil {
-			log.Info("--cmd and --req forces single stage mode, only one stage will be run")
-			generateSingleStage = true
-		}
-		generateJobSpec()
-	}
-
+	setCliSpec()
 	spec.Job.MinioCfgFile = minioCfgFile
 
 	if cosaWorkDirContext {

commit bf95d79f0dc834988d893e19887d845ec41c7c53

gangplank: allow specs passed in from cli
Fix regression introduced in #2133 where the spec file passed in from
the command line was not being used.

gangplank/cmd/generate.go
@@ -66,6 +66,12 @@ func init() {
 // setCliSpec reads or generates a jobspec based on CLI arguments.
 func setCliSpec() {
 	if specFile != "" {
+		js, err := jobspec.JobSpecFromFile(specFile)
+		if err != nil {
+			log.WithError(err).Fatal("failed to read jobspec")
+		}
+		spec = js
+
 		log.WithFields(log.Fields{
 			"jobspec":          specFile,
 			"ingored cli args": "-A|--artifact|--singleReq|--singleCmd",
@@ -81,13 +87,7 @@ func setCliSpec() {
 		log.Info("--cmd and --req forces single stage mode, only one stage will be run")
 		generateSingleStage = true
 	}
-	if specFile != "" {
-		js, err := jobspec.JobSpecFromFile(specFile)
-		if err != nil {
-			log.WithError(err).Fatal("failed to read jobspec")
-		}
-		spec = js
-	}
+
 	log.Info("Generating stages")
 	spec.GenerateStages(automaticBuildStages, generateSingleStage)
 	log.Infof("Adding commands %v to first stage", generateCommands)