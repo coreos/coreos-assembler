d2af67d954928cbc9a81b4e724290d4628c4c500
Ben Howard <ben.howard@redhat.com>
2021-04-29 13:45:56 -0600

    Gangplank: add Kola Test definitions
    This adds both the a CLI and jobspec short hand for testing.
    
    Signed-off-by: Ben Howard <ben.howard@redhat.com>

--- gangplank/cmd/commonflags.go
+++ gangplank/cmd/commonflags.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	jobspec "github.com/coreos/gangplank/spec"
 	flag "github.com/spf13/pflag"
 )
 
 // Flags has the configuration flags.
 var specCommonFlags = flag.NewFlagSet("", flag.ContinueOnError)
 
+// cosaKolaTests are used to generate automatic Kola stages.
+var cosaKolaTests []string
+
 func init() {
 	specCommonFlags.StringSliceVar(&generateCommands, "singleCmd", []string{}, "commands to run in stage")
 	specCommonFlags.StringSliceVar(&generateSingleRequires, "singleReq", []string{}, "artifacts to require")
 	specCommonFlags.StringVarP(&cosaSrvDir, "srvDir", "S", "", "directory for /srv; in pod mount this will be bind mounted")
+	jobspec.AddKolaTestFlags(&cosaKolaTests, specCommonFlags)
 }
