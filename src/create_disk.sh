2e1eac46a32f9d1dad3665f5fbc863f114e7a058
Dusty Mabe <dusty@dustymabe.com>
2021-05-05 15:39:48 -0400

    Updates for master -> main branch renamings

--- src/create_disk.sh
+++ src/create_disk.sh
@@ -346,7 +346,7 @@ s390x)
     # this is only a temporary solution until we are able to do firstboot check at bootloader
     # stage on s390x, either through zipl->grub2-emu or zipl standalone.
     # See https://github.com/coreos/ignition-dracut/issues/84
-    # A similar hack is present in https://github.com/coreos/coreos-assembler/blob/master/src/gf-platformid#L55
+    # A similar hack is present in https://github.com/coreos/coreos-assembler/blob/main/src/gf-platformid#L55
     echo "$(grep options $blsfile) ignition.firstboot" > $tmpfile
 
     # ideally we want to invoke zipl with bls and zipl.conf but we might need
