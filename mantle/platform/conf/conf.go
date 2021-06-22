coreos/coreos-assembler

commit 22f725a39e182a30253abdc4143dbad21c505ee3

mantle/platform/conf: Panic on additions to an empty config
Adding anything to an empty config does not make sense and can lead to
subtle bugs.

mantle/platform/conf/conf.go
@@ -92,12 +92,16 @@ type Conf struct {
 	ignitionV32exp *v32exptypes.Config
 }
 
+// Empty creates a completely empty configuration. Any configuration addition
+// applied to an empty config triggers a panic.
 func Empty() *UserData {
 	return &UserData{
 		kind: kindEmpty,
 	}
 }
 
+// ContainerLinuxConfig creates a Container Linux Config Userdata struct from
+// the provided string.
 func ContainerLinuxConfig(data string) *UserData {
 	return &UserData{
 		kind: kindContainerLinuxConfig,
@@ -596,6 +600,8 @@ func (c *Conf) AddFile(path, filesystem, contents string, mode int) {
 		c.addFileV23(path, filesystem, contents, mode)
 	} else if c.ignitionV24 != nil {
 		c.addFileV24(path, filesystem, contents, mode)
+	} else {
+		panic("Could not find a supported Ignition config version")
 	}
 }
 
@@ -725,6 +731,8 @@ func (c *Conf) AddSystemdUnit(name, contents string, state systemdUnitState) {
 		c.addSystemdUnitV31(name, contents, enable, mask)
 	} else if c.ignitionV32exp != nil {
 		c.addSystemdUnitV32exp(name, contents, enable, mask)
+	} else {
+		panic("Could not find a supported Ignition config version")
 	}
 }
 
@@ -921,6 +929,8 @@ func (c *Conf) AddSystemdUnitDropin(service, name, contents string) {
 		c.addSystemdDropinV31(service, name, contents)
 	} else if c.ignitionV32exp != nil {
 		c.addSystemdDropinV32exp(service, name, contents)
+	} else {
+		panic("Could not find a supported Ignition config version")
 	}
 }
 
@@ -1092,6 +1102,8 @@ func (c *Conf) AddAuthorizedKeys(user string, keys []string) {
 		c.addAuthorizedKeysV31(user, keys)
 	} else if c.ignitionV32exp != nil {
 		c.addAuthorizedKeysV32exp(user, keys)
+	} else {
+		panic("Could not find a supported Ignition config version")
 	}
 }
 
@@ -1235,6 +1247,8 @@ func (c *Conf) AddConfigSource(source string) {
 		c.addConfigSourceV31(source)
 	} else if c.ignitionV32exp != nil {
 		c.addConfigSourceV32exp(source)
+	} else {
+		panic("Could not find a supported Ignition config version")
 	}
 }
 

commit 7332eb1c481a15646c5049f25240b6312e0e03e2

mantle/platform/conf: don't panic on additions to an empty config
If the userdata is specified as Empty(), the test wants to ensure that
there is no Ignition config at all, and all of the additions made to the
config by BaseCluster.RenderUserData() should be ignored.  The panics
added in 22f725a39e18 interfere with that, so remove them again.

Currently, the only test that uses Empty() is fcos.ignition.misc.empty,
which relies on Afterburn SSH key injection.  When that test was ported
from CL in 6e9e49bb12ad, the UserData field wasn't renamed to UserDataV3,
and the nil UserDataV3 caused the test to start running with a default
non-empty Ignition config.  The SSH key was thus injected twice (in the
Ignition config and via Afterburn), but the test doesn't check for that,
so it continued to pass.  Thus, when 22f725a39e18 broke Empty(), the test
continued to work until 1952a49f90df dropped the separate UserDataV3 field
and the test started invoking Empty() again.  Even then, the test passed
cosa CI because it's skipped on qemu, which doesn't have a separate
mechanism for injecting SSH keys.

Fixes panic in fcos.ignition.misc.empty:

    panic: Could not find a supported Ignition config version
    goroutine 5056 [running]:
    github.com/coreos/mantle/platform/conf.(*Conf).AddAuthorizedKeys(0xc000125200, 0x20e06cb, 0x4, 0xc00086ab50, 0x1, 0x1)
    	/root/containerbuild/mantle/platform/conf/conf.go:690 +0x16b
    github.com/coreos/mantle/platform/conf.(*Conf).CopyKeys(0xc000125200, 0xc000010a10, 0x1, 0x1)
    	/root/containerbuild/mantle/platform/conf/conf.go:701 +0x165
    github.com/coreos/mantle/platform.(*BaseCluster).RenderUserData(0xc000735900, 0xc000110f30, 0xc000923d58, 0xd, 0xc001435e20, 0x40)
    	/root/containerbuild/mantle/platform/cluster.go:207 +0x6c5
    github.com/coreos/mantle/platform/machine/aws.(*cluster).NewMachine(0xc00086ab10, 0xc000110f30, 0x0, 0x0, 0x0, 0xc001435ef8)
    	/root/containerbuild/mantle/platform/machine/aws/cluster.go:32 +0x16d

mantle/platform/conf/conf.go
@@ -73,7 +73,7 @@ type Conf struct {
 }
 
 // Empty creates a completely empty configuration. Any configuration addition
-// applied to an empty config triggers a panic.
+// applied to an empty config is ignored.
 func Empty() *UserData {
 	return &UserData{
 		kind: kindEmpty,
@@ -387,8 +387,6 @@ func (c *Conf) AddFile(path, filesystem, contents string, mode int) {
 		c.addFileV32(path, filesystem, contents, mode)
 	} else if c.ignitionV33exp != nil {
 		c.addFileV33exp(path, filesystem, contents, mode)
-	} else {
-		panic("Could not find a supported Ignition config version")
 	}
 }
 
@@ -484,8 +482,6 @@ func (c *Conf) AddSystemdUnit(name, contents string, state systemdUnitState) {
 		c.addSystemdUnitV32(name, contents, enable, mask)
 	} else if c.ignitionV33exp != nil {
 		c.addSystemdUnitV33exp(name, contents, enable, mask)
-	} else {
-		panic("Could not find a supported Ignition config version")
 	}
 }
 
@@ -586,8 +582,6 @@ func (c *Conf) AddSystemdUnitDropin(service, name, contents string) {
 		c.addSystemdDropinV32(service, name, contents)
 	} else if c.ignitionV33exp != nil {
 		c.addSystemdDropinV33exp(service, name, contents)
-	} else {
-		panic("Could not find a supported Ignition config version")
 	}
 }
 
@@ -686,8 +680,6 @@ func (c *Conf) AddAuthorizedKeys(user string, keys []string) {
 		c.addAuthorizedKeysV32(user, keys)
 	} else if c.ignitionV33exp != nil {
 		c.addAuthorizedKeysV33exp(user, keys)
-	} else {
-		panic("Could not find a supported Ignition config version")
 	}
 }
 
@@ -794,8 +786,6 @@ func (c *Conf) AddConfigSource(source string) {
 		c.addConfigSourceV32(source)
 	} else if c.ignitionV33exp != nil {
 		c.addConfigSourceV33exp(source)
-	} else {
-		panic("Could not find a supported Ignition config version")
 	}
 }
 