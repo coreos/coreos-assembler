coreos/coreos-assembler

commit a542f26614fa6b03e36c743eda9374fe52b41911

mantle/platform: Ignore some non-fatal unit failures in RHCOS
Special case to ignore some non-fatal unit failures frequently seen in
flakes. This is for RHEL 8 only as this has been fixed upstream in
systemd but not fixed in RHEL 8.

Tracked in https://bugzilla.redhat.com/show_bug.cgi?id=1914362

mantle/platform/platform.go
@@ -21,6 +21,7 @@ import (
 	"io"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 
@@ -391,6 +392,39 @@ func NewMachines(c Cluster, userdata *conf.UserData, n int, options MachineOptio
 	return machs, nil
 }
 
+// checkSystemdUnitFailures make sure no system unit is in failed state.
+func checkSystemdUnitFailures(output string, distribution string) error {
+	var ignoredUnits []string
+
+	// Temporarily ignore some non-fatal flakes on RHCOS. See:
+	// https://bugzilla.redhat.com/show_bug.cgi?id=1914362
+	if distribution == "rhcos" {
+		ignoredUnits = append(ignoredUnits, "user@1000.service")
+		ignoredUnits = append(ignoredUnits, "user-runtime-dir@1000.service")
+	}
+
+	var failedUnits []string
+	for _, unit := range strings.Split(output, "\n") {
+		u := strings.Fields(unit)[0]
+		ignore := false
+		for _, i := range ignoredUnits {
+			if u == i {
+				// Ignoring this unit
+				ignore = true
+				break
+			}
+		}
+		if !ignore {
+			failedUnits = append(failedUnits, u)
+		}
+	}
+	if len(failedUnits) > 0 {
+		return fmt.Errorf("some systemd units failed:\n%s", output)
+	}
+
+	return nil
+}
+
 // CheckMachine tests a machine for various error conditions such as ssh
 // being available and no systemd units failing at the time ssh is reachable.
 // It also ensures the remote system is running Container Linux by CoreOS or
@@ -423,8 +457,13 @@ func CheckMachine(ctx context.Context, m Machine) error {
 	}
 
 	// ensure we're talking to a supported system
+	var distribution string
 	switch string(out) {
-	case `coreos-`, `rhcos-`, `fedora-coreos`:
+	case `fedora-coreos`:
+		distribution = "fcos"
+		break
+	case `rhcos-`:
+		distribution = "rhcos"
 		break
 	default:
 		return fmt.Errorf("not a supported instance: %v", string(out))
@@ -437,7 +476,7 @@ func CheckMachine(ctx context.Context, m Machine) error {
 			return fmt.Errorf("systemctl: %s: %v: %s", out, err, stderr)
 		}
 		if len(out) > 0 {
-			return fmt.Errorf("some systemd units failed:\n%s", out)
+			return checkSystemdUnitFailures(string(out), distribution)
 		}
 	}
 

mantle/platform/platform_test.go
@@ -1,0 +1,57 @@
+// Copyright 2021 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package platform
+
+import "testing"
+
+func TestSystemUnitFiltering(t *testing.T) {
+	// Output from: `systemctl --no-legend --state failed list-units`
+	var output string
+
+	output = `● abrt-oops.service         loaded failed failed ABRT kernel log watcher
+● systemd-timesyncd.service loaded failed failed Network Time Synchronization`
+	if checkSystemdUnitFailures(output, "fcos") == nil {
+		t.Errorf("Should have failed")
+	}
+	output = `abrt-oops.service         loaded failed failed ABRT kernel log watcher
+systemd-timesyncd.service loaded failed failed Network Time Synchronization`
+	if checkSystemdUnitFailures(output, "rhcos") == nil {
+		t.Errorf("Should have failed")
+	}
+
+	output = `● user@1000.service             loaded failed failed Foo
+● user-runtime-dir@1000.service loaded failed failed Bar`
+	if checkSystemdUnitFailures(output, "fcos") == nil {
+		t.Errorf("Should have failed")
+	}
+	output = `user@1000.service             loaded failed failed Foo
+user-runtime-dir@1000.service loaded failed failed Bar`
+	if checkSystemdUnitFailures(output, "rhcos") != nil {
+		t.Errorf("Should have passed")
+	}
+
+	output = `● abrt-oops.service         loaded failed failed ABRT kernel log watcher
+● user@1000.service             loaded failed failed Foo
+● user-runtime-dir@1000.service loaded failed failed Bar`
+	if checkSystemdUnitFailures(output, "fcos") == nil {
+		t.Errorf("Should have failed")
+	}
+	output = `abrt-oops.service         loaded failed failed ABRT kernel log watcher
+user@1000.service             loaded failed failed Foo
+user-runtime-dir@1000.service loaded failed failed Bar`
+	if checkSystemdUnitFailures(output, "rhcos") == nil {
+		t.Errorf("Should have failed")
+	}
+}

commit b2f4beb3ff8c391d118f294d71c3f30d91510f5e

mantle/platform: Fix unit failures ignore path
Use the stable DBus interface instead of parsing systemctl output.

Fixes: a542f266 mantle/platform: Ignore some non-fatal unit failures in RHCOS

mantle/platform/platform.go
@@ -392,12 +392,15 @@ func NewMachines(c Cluster, userdata *conf.UserData, n int, options MachineOptio
 	return machs, nil
 }
 
-// checkSystemdUnitFailures make sure no system unit is in failed state.
+// checkSystemdUnitFailures ensures that no system unit is in a failed state,
+// temporarily ignoring some non-fatal flakes on RHCOS.
+// See: https://bugzilla.redhat.com/show_bug.cgi?id=1914362
 func checkSystemdUnitFailures(output string, distribution string) error {
-	var ignoredUnits []string
+	if len(output) == 0 {
+		return nil
+	}
 
-	// Temporarily ignore some non-fatal flakes on RHCOS. See:
-	// https://bugzilla.redhat.com/show_bug.cgi?id=1914362
+	var ignoredUnits []string
 	if distribution == "rhcos" {
 		ignoredUnits = append(ignoredUnits, "user@1000.service")
 		ignoredUnits = append(ignoredUnits, "user-runtime-dir@1000.service")
@@ -405,17 +408,16 @@ func checkSystemdUnitFailures(output string, distribution string) error {
 
 	var failedUnits []string
 	for _, unit := range strings.Split(output, "\n") {
-		u := strings.Fields(unit)[0]
-		ignore := false
+		// Filter ignored units
+		ignored := false
 		for _, i := range ignoredUnits {
-			if u == i {
-				// Ignoring this unit
-				ignore = true
+			if unit == i {
+				ignored = true
 				break
 			}
 		}
-		if !ignore {
-			failedUnits = append(failedUnits, u)
+		if !ignored {
+			failedUnits = append(failedUnits, unit)
 		}
 	}
 	if len(failedUnits) > 0 {
@@ -429,8 +431,6 @@ func checkSystemdUnitFailures(output string, distribution string) error {
 // being available and no systemd units failing at the time ssh is reachable.
 // It also ensures the remote system is running Container Linux by CoreOS or
 // Red Hat CoreOS.
-//
-// TODO(mischief): better error messages.
 func CheckMachine(ctx context.Context, m Machine) error {
 	// ensure ssh works and the system is ready
 	sshChecker := func() error {
@@ -468,13 +468,14 @@ func CheckMachine(ctx context.Context, m Machine) error {
 	}
 
 	if !m.RuntimeConf().AllowFailedUnits {
-		// ensure no systemd units failed during boot
-		out, stderr, err = m.SSH("systemctl --no-legend --state failed list-units")
+		// Ensure no systemd units failed during boot
+		out, stderr, err = m.SSH("busctl --json=short call org.freedesktop.systemd1 /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager ListUnitsFiltered as 2 state failed | jq -r '.data[][][0]'")
 		if err != nil {
-			return fmt.Errorf("systemctl: %s: %v: %s", out, err, stderr)
+			return fmt.Errorf("failed to query systemd for failed units: %s: %v: %s", out, err, stderr)
 		}
-		if len(out) > 0 {
-			return checkSystemdUnitFailures(string(out), distribution)
+		err = checkSystemdUnitFailures(string(out), distribution)
+		if err != nil {
+			return err
 		}
 	}
 

mantle/platform/platform_test.go
@@ -17,40 +17,39 @@ package platform
 import "testing"
 
 func TestSystemUnitFiltering(t *testing.T) {
-	// Output from: `systemctl --no-legend --state failed list-units`
+	// Output from:
+	// $ busctl --json=short call \
+	//       org.freedesktop.systemd1 \
+	//       /org/freedesktop/systemd1 \
+	//       org.freedesktop.systemd1.Manager \
+	//       ListUnitsFiltered as 2 state failed \
+	//       | jq -r '.data[][][0]'
 	var output string
 
-	output = `● abrt-oops.service         loaded failed failed ABRT kernel log watcher
-● systemd-timesyncd.service loaded failed failed Network Time Synchronization`
+	output = `abrt-oops.service
+systemd-timesyncd.service`
 	if checkSystemdUnitFailures(output, "fcos") == nil {
 		t.Errorf("Should have failed")
 	}
-	output = `abrt-oops.service         loaded failed failed ABRT kernel log watcher
-systemd-timesyncd.service loaded failed failed Network Time Synchronization`
 	if checkSystemdUnitFailures(output, "rhcos") == nil {
 		t.Errorf("Should have failed")
 	}
 
-	output = `● user@1000.service             loaded failed failed Foo
-● user-runtime-dir@1000.service loaded failed failed Bar`
+	output = `user@1000.service
+user-runtime-dir@1000.service`
 	if checkSystemdUnitFailures(output, "fcos") == nil {
 		t.Errorf("Should have failed")
 	}
-	output = `user@1000.service             loaded failed failed Foo
-user-runtime-dir@1000.service loaded failed failed Bar`
 	if checkSystemdUnitFailures(output, "rhcos") != nil {
 		t.Errorf("Should have passed")
 	}
 
-	output = `● abrt-oops.service         loaded failed failed ABRT kernel log watcher
-● user@1000.service             loaded failed failed Foo
-● user-runtime-dir@1000.service loaded failed failed Bar`
+	output = `abrt-oops.service
+user@1000.service
+user-runtime-dir@1000.service`
 	if checkSystemdUnitFailures(output, "fcos") == nil {
 		t.Errorf("Should have failed")
 	}
-	output = `abrt-oops.service         loaded failed failed ABRT kernel log watcher
-user@1000.service             loaded failed failed Foo
-user-runtime-dir@1000.service loaded failed failed Bar`
 	if checkSystemdUnitFailures(output, "rhcos") == nil {
 		t.Errorf("Should have failed")
 	}