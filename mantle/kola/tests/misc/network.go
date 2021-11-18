cb32aea9df646ef7d2524a37309aa4fc4a5d666a
Ram Lavi <ralavi@redhat.com>
2021-11-18 11:04:24 -0500

    update setup-ovs script
    To resolve the issue of connections not up after reboot, the setup-ovs
    is changed to use NetworkManager profile files
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- mantle/kola/tests/misc/network.go
+++ mantle/kola/tests/misc/network.go
@@ -283,6 +283,20 @@ var (
           primary_ip=%s
           secondary_ip=%s
 
+          # Get the location of the NetworkManager config files
+          NMPATH=$(NetworkManager --print-config | sed -nr "/^\[keyfile\]/ { :l /^path[ ]*/ { s/.*=[ ]*//; p; q;}; n; b l;}")
+
+          # Store the location of the NM config files
+          if [[ ! -d $NMPATH ]]; then
+           NMPATH=/etc/NetworkManager/system-connections
+          fi
+          rm -rf ${NMPATH}/*
+
+          if [[ ! -f /boot/mac_addresses ]] ; then
+            echo "no mac address configuration file found .. exiting"
+            exit 0
+          fi
+
           network_namespace=test-netns
           # Create a network namespace
           ip netns add ${network_namespace}
@@ -310,74 +324,88 @@ var (
 
 	setupOvsScript = `#!/usr/bin/env bash
           set -ex
+
+          rm -rf /etc/NetworkManager/system-connections/*
+
+          # Get the location of the NetworkManager config files
+          NMPATH=$(NetworkManager --print-config | sed -nr "/^\[keyfile\]/ { :l /^path[ ]*/ { s/.*=[ ]*//; p; q;}; n; b l;}")
+
           if [[ ! -f /boot/mac_addresses ]] ; then
-            echo "no mac address configuration file found .. skipping setup-ovs"
-            exit 0
+            echo "no mac address configuration file found .. exiting"
+            exit 1
+          fi
+
+          # Store the location of the NM config files
+          if [[ ! -d $NMPATH ]]; then
+           NMPATH=/etc/NetworkManager/system-connections
           fi
           
+          # Copy the config files from stored location to NM settings folder
+          if [[ -d /var/ovsbond ]]; then
+            echo "Loading OVS old profile"
+            cp -r /var/ovsbond/*  $NMPATH
+            systemctl restart NetworkManager
+          fi
+
           if [[ $(nmcli conn | grep -c ovs) -eq 0 ]]; then
             echo "configure ovs bonding"
+            ovs-vsctl --if-exists del-br brcnv
             primary_mac=$(cat /boot/mac_addresses | awk -F= '/PRIMARY_MAC/ {print $2}')
             secondary_mac=$(cat /boot/mac_addresses | awk -F= '/SECONDARY_MAC/ {print $2}')
-            
+
             default_device=""
             secondary_device=""
             profile_name=""
             secondary_profile_name=""
-            
-            
+
             for dev in $(nmcli device status | awk '/ethernet/ {print $1}'); do
-              dev_mac=$(nmcli -g GENERAL.HWADDR dev show $dev | sed -e 's/\\//g' | tr '[A-Z]' '[a-z]')
-              case $dev_mac in
-                $primary_mac)
-                  default_device=$dev
-                  profile_name=$(nmcli -g GENERAL.CONNECTION dev show $dev)
-                  ;;
-                $secondary_mac)
-                  secondary_device=$dev
-                  secondary_profile_name=$(nmcli -g GENERAL.CONNECTION dev show $dev)
-                  ;;
-                *)
-                  ;;
-               esac
+          	dev_mac=$(nmcli -g GENERAL.HWADDR dev show $dev | sed -e 's/\\//g' | tr '[A-Z]' '[a-z]')
+          	case $dev_mac in
+          	  $primary_mac)
+                    default_device=$dev
+                    profile_name=$(nmcli -g GENERAL.CONNECTION dev show $dev)
+                    ;;
+          	  $secondary_mac)
+                    secondary_device=$dev
+                    secondary_profile_name=$(nmcli -g GENERAL.CONNECTION dev show $dev)
+                    ;;
+          	  *)
+                    ;;
+          	 esac
             done
             echo -e "default dev: $default_device ($profile_name)\nsecondary dev: $secondary_device ($secondary_profile_name)"
-            
+
             mac=$(sudo nmcli -g GENERAL.HWADDR dev show $default_device | sed -e 's/\\//g')
-          
-            # delete old bridge if it exists
-            ovs-vsctl --if-exists del-br brcnv
-            
+
             # make bridge
-            nmcli conn add type ovs-bridge conn.interface brcnv
+            nmcli conn add type ovs-bridge conn.interface brcnv con-name ovs-bridge-brcnv
             nmcli conn add type ovs-port conn.interface brcnv-port master brcnv
-            nmcli conn add type ovs-interface \
-                           conn.id brcnv-iface \
-                           conn.interface brcnv master brcnv-port \
-                           ipv4.method auto \
-                           ipv4.dhcp-client-id "mac" \
-                           connection.autoconnect no \
-                           802-3-ethernet.cloned-mac-address $mac
-  
+            nmcli conn add type ovs-interface con-name ovs-brcnv-iface conn.interface brcnv master brcnv-port ipv4.method auto ipv4.dhcp-client-id "mac" connection.autoconnect no 802-3-ethernet.cloned-mac-address $mac
+
             # make bond
-            nmcli conn add type ovs-port conn.interface bond0 master brcnv ovs-port.bond-mode balance-slb
-            nmcli conn add type ethernet conn.interface $default_device master bond0
-            nmcli conn add type ethernet conn.interface $secondary_device master bond0
+            nmcli conn add type ovs-port conn.interface bond0 master brcnv ovs-port.bond-mode balance-slb con-name ovs-slave-bond0
+            nmcli conn add type ethernet conn.interface $default_device master bond0 con-name ovs-slave-1
+            nmcli conn add type ethernet conn.interface $secondary_device master bond0 con-name ovs-slave-2
             nmcli conn down "$profile_name" || true
             nmcli conn mod "$profile_name" connection.autoconnect no || true
             nmcli conn down "$secondary_profile_name" || true
             nmcli conn mod "$secondary_profile_name" connection.autoconnect no || true
-            if ! nmcli conn up brcnv-iface; then
-                nmcli conn up "$profile_name" || true
-                nmcli conn mod "$profile_name" connection.autoconnect yes
-                nmcli conn up "$secondary_profile_name" || true
-                nmcli conn mod "$secondary_profile_name" connection.autoconnect yes
-                nmcli c delete $(nmcli c show |grep ovs-cnv |awk '{print $1}') || true
+            if ! nmcli conn up ovs-brcnv-iface; then
+          	  nmcli conn up "$profile_name" || true
+          	  nmcli conn mod "$profile_name" connection.autoconnect yes
+          	  nmcli conn up "$secondary_profile_name" || true
+          	  nmcli conn mod "$secondary_profile_name" connection.autoconnect yes
+          	  nmcli conn delete $(nmcli c show |grep ovs-cnv |awk '{print $1}') || true
             else
-                nmcli conn mod brcnv-iface connection.autoconnect yes
+          	  nmcli conn mod ovs-brcnv-iface connection.autoconnect yes
+          	  nmcli conn up ovs-slave-2
+          	  # Remove Old NM config files and copy the new-ones
+          	  rm -rf /var/ovsbond
+          	  cp -r $NMPATH /var/ovsbond || true
             fi
           else
-              echo "ovs bridge already present"
+          	echo "ovs bridge already present"
+          	for c in ovs-bridge-brcnv ovs-slave-bond0 ovs-slave-brcnv-port ovs-slave-1 ovs-slave-2 ovs-brcnv-iface; do nmcli c up $c; done
           fi
 `
 )
@@ -436,7 +464,7 @@ func NetworkBondWithDhcp(c cluster.TestCluster) {
 	secondaryMac := "52:55:00:d1:56:01"
 	primaryIp := "192.168.0.55"
 	secondaryIp := "192.168.0.56"
-	ovsBridgeInterface := "brcnv-iface"
+	ovsBridgeInterface := "ovs-brcnv-iface"
 
 	setupBondWithDhcpTest(c, primaryMac, secondaryMac, primaryIp, secondaryIp)
 
@@ -491,12 +519,12 @@ func setupBondWithDhcpTest(c cluster.TestCluster, primaryMac, secondaryMac, prim
 					"name": "openvswitch.service"
 				},
 				{
-					"contents": "[Unit]\nDescription=Capture MAC address from kargs\nAfter=network-online.target\nAfter=openvswitch.service\nConditionKernelCommandLine=macAddressList\nRequiresMountsFor=/boot\n\n[Service]\nType=oneshot\nMountFlags=slave\nExecStart=/usr/local/bin/capture-macs\n\n[Install]\nRequiredBy=multi-user.target\n",
+					"contents": "[Unit]\nDescription=Capture MAC address from kargs\nBefore=coreos-installer.target\nAfter=coreos-installer.service\n\nConditionKernelCommandLine=macAddressList\nRequiresMountsFor=/boot\n\n[Service]\nType=oneshot\nMountFlags=slave\nExecStart=/usr/local/bin/capture-macs\n\n[Install]\nRequiredBy=multi-user.target\n",
 					"enabled": true,
 					"name": "capture-macs.service"
 				},
 				{
-					"contents": "[Unit]\nDescription=Create VETH Pairs and Configue OVS interface over bond\nAfter=network-online.target\nAfter=capture-macs.service\nAfter=openvswitch.service\n\n[Service]\nType=oneshot\nExecStart=/usr/local/bin/create-veth-pairs\n\n[Install]\nRequiredBy=multi-user.target\n",
+					"contents": "[Unit]\nDescription=Create VETH Pairs and Configue OVS interface over bond\nBefore=ovs-configuration.service\nAfter=NetworkManager.service\nAfter=openvswitch.service\nAfter=capture-macs.service\n\n[Service]\nType=oneshot\nExecStart=/usr/local/bin/create-veth-pairs\n\n[Install]\nRequiredBy=multi-user.target\n",
 					"enabled": true,
 					"name": "create-veth-pairs.service"
 				}
@@ -530,7 +558,7 @@ func setupBondWithDhcpTest(c cluster.TestCluster, primaryMac, secondaryMac, prim
 func NetworkSecondaryNics(c cluster.TestCluster) {
 	primaryMac := "52:55:00:d1:56:00"
 	secondaryMac := "52:55:00:d1:56:01"
-	ovsBridgeInterface := "brcnv-iface"
+	ovsBridgeInterface := "ovs-brcnv-iface"
 
 	setupMultipleNetworkTest(c, primaryMac, secondaryMac)
 
@@ -660,12 +688,12 @@ func setupMultipleNetworkTest(c cluster.TestCluster, primaryMac, secondaryMac st
 					"name": "openvswitch.service"
 				},
 				{
-					"contents": "[Unit]\nDescription=Capture MAC address from kargs\nAfter=network-online.target\nAfter=openvswitch.service\nConditionKernelCommandLine=macAddressList\nRequiresMountsFor=/boot\n\n[Service]\nType=oneshot\nMountFlags=slave\nExecStart=/usr/local/bin/capture-macs\n\n[Install]\nRequiredBy=multi-user.target\n",
+					"contents": "[Unit]\nDescription=Capture MAC address from kargs\nBefore=coreos-installer.target\nAfter=coreos-installer.service\n\nConditionKernelCommandLine=macAddressList\nRequiresMountsFor=/boot\n\n[Service]\nType=oneshot\nMountFlags=slave\nExecStart=/usr/local/bin/capture-macs\n\n[Install]\nRequiredBy=multi-user.target\n",
 					"enabled": true,
 					"name": "capture-macs.service"
 				},
 				{
-					"contents": "[Unit]\nDescription=Setup OVS bonding\nAfter=capture-macs.service\n\n[Service]\nType=oneshot\nExecStart=/usr/local/bin/setup-ovs\n\n[Install]\nRequiredBy=multi-user.target\n",
+					"contents": "[Unit]\nDescription=Setup OVS bonding\nBefore=ovs-configuration.service\nAfter=NetworkManager.service\nAfter=openvswitch.service\nAfter=capture-macs.service\n\n[Service]\nType=oneshot\nExecStart=/usr/local/bin/setup-ovs\n\n[Install]\nRequiredBy=multi-user.target\n",
 					"enabled": true,
 					"name": "setup-ovs.service"
 				}
