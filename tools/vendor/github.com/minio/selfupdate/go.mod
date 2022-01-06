74f7888fea6110b047f39d4c4f7b42d9f963791b
Micah Abbott <miabbott@redhat.com>
2022-01-06 16:44:22 -0500

    tools: remove all the go module cruft
    The content in the `tools` directory is used to build a number of
    binaries from source, in order for use with the `gangplank` build
    process.
    
    Rather than have to manage golang modules to just get binaries, let's
    start installing the binaries directly rather than building directly
    from source/modules.
    
    The removal of all the go module scaffolding is the first step towards
    installing the tools as binaries.

--- tools/vendor/github.com/minio/selfupdate/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/minio/selfupdate
-
-go 1.14
-
-require golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
--- tools/vendor/github.com/princjef/gomarkdoc/lang/godoc.go
+++
@@ -1,3 +0,0 @@
-// Package lang provides constructs for defining golang language constructs and
-// extracting information from them for documentation purposes.
-package lang
