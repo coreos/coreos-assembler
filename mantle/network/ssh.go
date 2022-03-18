coreos/coreos-assembler

commit dcd432d2a546d8a8a2284f17f90cd354a14a2945

mantle: switch back to RSA SSH keys
golang.org/x/crypto/ssh finally supports RSA SHA-2 signatures, so we can
drop our workarounds for Fedora crypto policy forbidding RSA SHA-1.

Fixes #1772.  Reverts ac54a8a085d0, db56cfeca5d6, and a2066dbedf8f.

mantle/network/ssh.go
@@ -15,9 +15,8 @@
 package network
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -31,6 +30,7 @@ import (
 const (
 	defaultPort = 22
 	defaultUser = "core"
+	rsaKeySize  = 2048
 )
 
 // DefaultSSHDir is a process-global path that can be set, and
@@ -57,7 +57,7 @@ type SSHAgent struct {
 // NewSSHAgent constructs a new SSHAgent using dialer to create ssh
 // connections.
 func NewSSHAgent(dialer Dialer) (*SSHAgent, error) {
-	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	key, err := rsa.GenerateKey(rand.Reader, rsaKeySize)
 	if err != nil {
 		return nil, err
 	}

mantle/kola/tests/ignition/empty.go
@@ -28,7 +28,7 @@ func init() {
 		Name:             "fcos.ignition.misc.empty",
 		Run:              noIgnitionSSHKey,
 		ClusterSize:      1,
-		ExcludePlatforms: []string{"qemu", "esx", "aws", "azure"},
+		ExcludePlatforms: []string{"qemu", "esx"},
 		Distros:          []string{"fcos"},
 		UserData:         conf.Empty(),
 		Tags:             []string{"ignition"},
@@ -37,7 +37,7 @@ func init() {
 		Name:             "fcos.ignition.v3.noop",
 		Run:              noIgnitionSSHKey,
 		ClusterSize:      1,
-		ExcludePlatforms: []string{"qemu", "esx", "aws", "azure"},
+		ExcludePlatforms: []string{"qemu", "esx"},
 		Distros:          []string{"fcos"},
 		Flags:            []register.Flag{register.NoSSHKeyInUserData},
 		UserData:         conf.Ignition(`{"ignition":{"version":"3.0.0"}}`),

mantle/platform/conf/conf_test.go
@@ -74,7 +74,7 @@ func TestConfCopyKey(t *testing.T) {
 
 		str := conf.String()
 
-		if !strings.Contains(str, "ecdsa-sha2-nistp256 ") || !strings.Contains(str, " core@default") {
+		if !strings.Contains(str, "ssh-rsa ") || !strings.Contains(str, " core@default") {
 			t.Errorf("ssh public key not found in config %d: %s", i, str)
 			continue
 		}

mantle/platform/machine/aws/flight.go
@@ -61,19 +61,12 @@ func NewFlight(opts *aws.Options) (platform.Flight, error) {
 		api:        api,
 	}
 
-	// We have worked around the golang library limitation for
-	// keyexchange algorithm by switching to an ecdsa key in
-	// network/ssh.go. However, AWS requires an rsa key. For now
-	// (until we get an updated golang library) we'll just satisfy
-	// the requirement by using a fake key and disabling the
-	// fcos.ignition.misc.empty and fcos.ignition.v3.noop tests on AWS.
-	// https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html#how-to-generate-your-own-key-and-import-it-to-aws
-	key, err := platform.GenerateFakeKey()
+	keys, err := af.Keys()
 	if err != nil {
 		af.Destroy()
 		return nil, err
 	}
-	if err := api.AddKey(af.Name(), key); err != nil {
+	if err := api.AddKey(af.Name(), keys[0].String()); err != nil {
 		af.Destroy()
 		return nil, err
 	}

mantle/platform/machine/azure/flight.go
@@ -33,9 +33,8 @@ var (
 
 type flight struct {
 	*platform.BaseFlight
-	api        *azure.API
-	SSHKey     string
-	FakeSSHKey string
+	api    *azure.API
+	SSHKey string
 }
 
 // NewFlight creates an instance of a Flight suitable for spawning
@@ -66,10 +65,6 @@ func NewFlight(opts *azure.Options) (platform.Flight, error) {
 		return nil, err
 	}
 	af.SSHKey = keys[0].String()
-	af.FakeSSHKey, err = platform.GenerateFakeKey()
-	if err != nil {
-		return nil, err
-	}
 
 	return af, nil
 }
@@ -87,18 +82,8 @@ func (af *flight) NewCluster(rconf *platform.RuntimeConfig) (platform.Cluster, e
 		flight:      af,
 	}
 
-	// We have worked around the golang library limitation for
-	// keyexchange algorithm by switching to an ecdsa key in
-	// network/ssh.go.  However, Azure requires an RSA key.  For
-	// now (until we get an updated golang library) we'll just
-	// satisfy the requirement by using a fake key and disabling
-	// the fcos.ignition.misc.empty and fcos.ignition.v3.noop
-	// tests on Azure.
-	// https://github.com/coreos/coreos-assembler/issues/1772
-	// https://docs.microsoft.com/en-us/azure/virtual-machines/linux/mac-create-ssh-keys#supported-ssh-key-formats
 	if !rconf.NoSSHKeyInMetadata {
-		ac.sshKey = af.FakeSSHKey
-		//ac.sshKey = af.SSHKey
+		ac.sshKey = af.SSHKey
 	}
 
 	ac.ResourceGroup, err = af.api.CreateResourceGroup("kola-cluster")

commit 4fc1c81a5b2c590e3cea8794cbc5b6f09721addb

mantle/network/ssh: use ExtendedAgent for SSHAgent
The `Agent` interface doesn't support `SignWithFlags`. So when trying to
use `ssh` with the mantle agent to SSH into a VM, the signing request
during handshake has its flags stripped:

https://cs.opensource.google/go/x/crypto/+/3147a52a:ssh/agent/server.go;l=133-137

This causes the wrong algorithm to be used, which leads to:

    agent key RSA SHA256:<hash> returned incorrect signature type

The reason kola itself doesn't hit this is because it doesn't go through
the socket.

The underlying `keyring` object does implement `ExtendedAgent` though,
which does support `SignWithFlags`, so just cast it.

Fixes: dcd432d2a ("mantle: switch back to RSA SSH keys")

mantle/network/ssh.go
@@ -45,7 +45,7 @@ type Dialer interface {
 // SSHAgent can manage keys, updates cloud config, and loves ponies.
 // The embedded dialer is used for establishing new SSH connections.
 type SSHAgent struct {
-	agent.Agent
+	agent.ExtendedAgent
 	Dialer
 	User         string
 	Socket       string
@@ -99,13 +99,13 @@ func NewSSHAgent(dialer Dialer) (*SSHAgent, error) {
 	}
 
 	a := &SSHAgent{
-		Agent:        keyring,
-		Dialer:       dialer,
-		User:         defaultUser,
-		Socket:       sockPath,
-		sockDir:      sockDir,
-		sockdirOwned: sockdirOwned,
-		listener:     listener,
+		ExtendedAgent: keyring.(agent.ExtendedAgent),
+		Dialer:        dialer,
+		User:          defaultUser,
+		Socket:        sockPath,
+		sockDir:       sockDir,
+		sockdirOwned:  sockdirOwned,
+		listener:      listener,
 	}
 
 	go func() {