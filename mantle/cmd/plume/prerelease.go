4daabadc3529ff5b7bf6f9b8e83effe37a3dd61b
Benjamin Gilbert <bgilbert@redhat.com>
2022-03-30 18:05:28 -0400

    mantle/plume: drop Azure support
    It uses ASM APIs, and any event we no longer use plume to publish to
    Azure.
    
    (cherry picked from commit 580314c73638c90f68b90ce826f154396ec32868)

--- mantle/cmd/plume/prerelease.go
+++ mantle/cmd/plume/prerelease.go
@@ -25,16 +25,12 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
-	"time"
 
-	"github.com/Azure/azure-sdk-for-go/management/storageservice"
-	"github.com/Microsoft/azure-vhd-utils/vhdcore/validator"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"golang.org/x/net/context"
 
 	"github.com/coreos/mantle/platform/api/aws"
-	"github.com/coreos/mantle/platform/api/azure"
 	"github.com/coreos/mantle/sdk"
 	"github.com/coreos/mantle/storage"
 	"github.com/coreos/mantle/system"
@@ -56,16 +52,11 @@ var (
 			displayName: "AWS",
 			handler:     awsPreRelease,
 		},
-		"azure": platform{
-			displayName: "Azure",
-			handler:     azurePreRelease,
-		},
 	}
 	platformList []string
 
 	selectedPlatforms  []string
 	selectedDistro     string
-	azureProfile       string
 	awsCredentialsFile string
 	verifyKeyFile      string
 	imageInfoFile      string
@@ -86,8 +77,7 @@ type platform struct {
 }
 
 type imageInfo struct {
-	AWS   *amiList        `json:"aws,omitempty"`
-	Azure *azureImageInfo `json:"azure,omitempty"`
+	AWS *amiList `json:"aws,omitempty"`
 }
 
 // Common switches between Fedora Cloud and Fedora CoreOS
@@ -109,7 +99,6 @@ func init() {
 
 	cmdPreRelease.Flags().StringSliceVar(&selectedPlatforms, "platform", platformList, "platform to pre-release")
 	cmdPreRelease.Flags().StringVar(&selectedDistro, "distro", "fedora", "system to pre-release")
-	cmdPreRelease.Flags().StringVar(&azureProfile, "azure-profile", "", "Azure Profile json file")
 	cmdPreRelease.Flags().StringVar(&awsCredentialsFile, "aws-credentials", "", "AWS credentials file")
 	cmdPreRelease.Flags().StringVar(&verifyKeyFile,
 		"verify-key", "", "path to ASCII-armored PGP public key to be used in verifying download signatures.  Defaults to CoreOS Buildbot (0412 7D0B FABE C887 1FFB  2CCE 50E0 8855 93D2 DCB4)")
@@ -207,157 +196,6 @@ func getFedoraImageFile(client *http.Client, spec *channelSpec, src *storage.Buc
 		return "", err
 	}
 	return imagePath, nil
-}
-
-func uploadAzureBlob(spec *channelSpec, api *azure.API, storageKey storageservice.GetStorageServiceKeysResponse, vhdfile, container, blobName string) error {
-	blobExists, err := api.BlobExists(spec.Azure.StorageAccount, storageKey.PrimaryKey, container, blobName)
-	if err != nil {
-		return fmt.Errorf("failed to check if file %q in account %q container %q exists: %v", vhdfile, spec.Azure.StorageAccount, container, err)
-	}
-
-	if blobExists {
-		return nil
-	}
-
-	if err := api.UploadBlob(spec.Azure.StorageAccount, storageKey.PrimaryKey, vhdfile, container, blobName, false); err != nil {
-		if _, ok := err.(azure.BlobExistsError); !ok {
-			return fmt.Errorf("uploading file %q to account %q container %q failed: %v", vhdfile, spec.Azure.StorageAccount, container, err)
-		}
-	}
-	return nil
-}
-
-func createAzureImage(spec *channelSpec, api *azure.API, blobName, imageName string) error {
-	imageexists, err := api.OSImageExists(imageName)
-	if err != nil {
-		return fmt.Errorf("failed to check if image %q exists: %T %v", imageName, err, err)
-	}
-
-	if imageexists {
-		plog.Printf("OS Image %q exists, using it", imageName)
-		return nil
-	}
-
-	plog.Printf("Creating OS image with name %q", imageName)
-
-	bloburl := api.UrlOfBlob(spec.Azure.StorageAccount, spec.Azure.Container, blobName).String()
-
-	// a la https://github.com/coreos/scripts/blob/998c7e093922298637e7c7e82e25cee7d336144d/oem/azure/set-image-metadata.sh
-	md := &azure.OSImage{
-		Label:             spec.Azure.Label,
-		Name:              imageName,
-		OS:                "Linux",
-		Description:       spec.Azure.Description,
-		MediaLink:         bloburl,
-		ImageFamily:       spec.Azure.Label,
-		PublishedDate:     time.Now().UTC().Format("2006-01-02"),
-		RecommendedVMSize: spec.Azure.RecommendedVMSize,
-		IconURI:           spec.Azure.IconURI,
-		SmallIconURI:      spec.Azure.SmallIconURI,
-	}
-
-	return api.AddOSImage(md)
-}
-
-func replicateAzureImage(spec *channelSpec, api *azure.API, imageName string) error {
-	plog.Printf("Fetching Azure Locations...")
-	locations, err := api.Locations()
-	if err != nil {
-		return err
-	}
-
-	plog.Printf("Replicating image to locations: %s", strings.Join(locations, ", "))
-
-	channelTitle := strings.Title(specChannel)
-
-	if err := api.ReplicateImage(imageName, spec.Azure.Offer, channelTitle, specVersion, locations...); err != nil {
-		return fmt.Errorf("image replication failed: %v", err)
-	}
-
-	return nil
-}
-
-type azureImageInfo struct {
-	ImageName string `json:"image"`
-}
-
-// azurePreRelease runs everything necessary to prepare a CoreOS release for Azure.
-//
-// This includes uploading the vhd image to Azure storage, creating an OS image from it,
-// and replicating that OS image.
-func azurePreRelease(ctx context.Context, client *http.Client, src *storage.Bucket, spec *channelSpec, imageInfo *imageInfo) error {
-	if spec.Azure.StorageAccount == "" {
-		plog.Notice("Azure image creation disabled.")
-		return nil
-	}
-
-	// download azure vhd image and unzip it
-	vhdfile, err := getImageFile(client, spec, src, spec.Azure.Image)
-	if err != nil {
-		return err
-	}
-
-	// sanity check - validate VHD file
-	plog.Printf("Validating VHD file %q", vhdfile)
-	if err := validator.ValidateVhd(vhdfile); err != nil {
-		return err
-	}
-	if err := validator.ValidateVhdSize(vhdfile); err != nil {
-		return err
-	}
-
-	blobName := fmt.Sprintf("container-linux-%s-%s.vhd", specVersion, specChannel)
-	// channel name should be caps for azure image
-	imageName := fmt.Sprintf("%s-%s-%s", spec.Azure.Offer, strings.Title(specChannel), specVersion)
-
-	for _, environment := range spec.Azure.Environments {
-		// construct azure api client
-		api, err := azure.New(&azure.Options{
-			AzureProfile:      azureProfile,
-			AzureSubscription: environment.SubscriptionName,
-		})
-		if err != nil {
-			return fmt.Errorf("failed to create Azure API: %v", err)
-		}
-
-		plog.Printf("Fetching Azure storage credentials")
-
-		storageKey, err := api.GetStorageServiceKeys(spec.Azure.StorageAccount)
-		if err != nil {
-			return err
-		}
-
-		// upload blob, do not overwrite
-		plog.Printf("Uploading %q to Azure Storage...", vhdfile)
-
-		containers := append([]string{spec.Azure.Container}, environment.AdditionalContainers...)
-		for _, container := range containers {
-			err := uploadAzureBlob(spec, api, storageKey, vhdfile, container, blobName)
-			if err != nil {
-				return err
-			}
-		}
-
-		// create image
-		if err := createAzureImage(spec, api, blobName, imageName); err != nil {
-			// if it is a conflict, it already exists!
-			if !azure.IsConflictError(err) {
-				return err
-			}
-
-			plog.Printf("Azure image %q already exists", imageName)
-		}
-
-		// replicate it
-		if err := replicateAzureImage(spec, api, imageName); err != nil {
-			return err
-		}
-	}
-
-	imageInfo.Azure = &azureImageInfo{
-		ImageName: imageName,
-	}
-	return nil
 }
 
 func getSpecAWSImageMetadata(spec *channelSpec) (map[string]string, error) {
