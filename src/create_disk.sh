coreos/coreos-assembler

commit c9036faecbf9659809382c449a39c9e024fc7c3e

Serialize `grub-script` literally into image.json
Prep for injecting `image.json` into the ostree commit.  To make
that meaningful, it has to be entirely independent of coreos-assembler.

This bit is crucial for being able to do image builds in a manner
independent of coreos-assembler's content, using solely the ostree
commit as input.

src/create_disk.sh
@@ -364,7 +364,7 @@ install_grub_cfg() {
     # 0700 to match the RPM permissions which I think are mainly in case someone has
     # manually set a grub password
     mkdir -p -m 0700 $rootfs/boot/grub2
-    cp -v $grub_script $rootfs/boot/grub2/grub.cfg
+    printf "%s" "$grub_script" > $rootfs/boot/grub2/grub.cfg
 }
 
 # Other arch-specific bootloader changes

src/cosalib/cmdlib.py
@@ -354,6 +354,9 @@ def generate_image_json(srcfile):
     r = yaml.safe_load(open("/usr/lib/coreos-assembler/image-default.yaml"))
     for k, v in flatten_image_yaml(srcfile).items():
         r[k] = v
+    # Serialize our default GRUB config
+    with open("/usr/lib/coreos-assembler/grub.cfg") as f:
+        r['grub-script'] = f.read()
     return r
 
 

src/image-default.yaml
@@ -4,7 +4,6 @@ bootfs: "ext4"
 rootfs: "xfs"
 # Add arguments here that will be passed to e.g. mkfs.xfs
 rootfs-args: ""
-grub-script: "/usr/lib/coreos-assembler/grub.cfg"
 
 # Additional default kernel arguments injected into disk images
 extra-kargs: []

commit 91a168ce9f3ac8062e11862e864a902ac8b6d00e

create_disk.sh: add missing trailing newline in grub.cfg
Fixes: c9036faecbf ("Serialize `grub-script` literally into image.json")

src/create_disk.sh
@@ -364,7 +364,7 @@ install_grub_cfg() {
     # 0700 to match the RPM permissions which I think are mainly in case someone has
     # manually set a grub password
     mkdir -p -m 0700 $rootfs/boot/grub2
-    printf "%s" "$grub_script" > $rootfs/boot/grub2/grub.cfg
+    printf "%s\n" "$grub_script" > $rootfs/boot/grub2/grub.cfg
 }
 
 # Other arch-specific bootloader changes