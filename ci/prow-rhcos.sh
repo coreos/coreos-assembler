bdba31a059752f2d273eb0227797fd0716a6f7f0
Colin Walters <walters@verbum.org>
2022-06-14 13:54:21 -0400

    ci/prow-rhcos: Adjust for recent changes in openshift/os
    Hopefully will fix the `ci/prow/rhcos` context here.

--- ci/prow-rhcos.sh
+++ ci/prow-rhcos.sh
@@ -18,4 +18,4 @@ export COSA_SKIP_OVERLAY=1
 # Create a temporary cosa workdir
 cd "$(mktemp -d)"
 cosa init --transient -b "${RHCOS_BRANCH}" https://github.com/openshift/os
-exec src/config/ci/prow-build-test-qemu.sh
+exec src/config/ci/prow-entrypoint.sh rhcos-86-build-test-qemu
