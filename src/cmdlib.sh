ffa37cb9e0f907fc15910cb0bb1bc7dd29c6dfd8
Jonathan Lebon <jonathan@jlebon.com>
2022-06-27 12:45:13 -0400

    cmdlib: consistently use `overlay/` prefix for injected overlays
    Like in e6b07ffb2 ("cmdlib: rename `cosa-image-json` ostree overlay"),
    we need the content set overlay and the `overrides/rootfs` overlay to
    use that prefix. That's how `compose.sh` knows to pull it over 9p.
    
    Fixes: #2949

--- src/cmdlib.sh
+++ src/cmdlib.sh
@@ -454,8 +454,8 @@ EOF
         chmod 0644 "${tmp_overridesdir}/contentsetrootfs/usr/share/buildinfo/content_manifest.json"
 
         echo -n "Committing ${tmp_overridesdir}/contentsetrootfs... "
-        commit_ostree_layer "${tmp_overridesdir}/contentsetrootfs" contentset
-        layers="${layers} contentset"
+        commit_ostree_layer "${tmp_overridesdir}/contentsetrootfs" overlay/contentset
+        layers="${layers} overlay/contentset"
     fi
 
     if [ -n "${layers}" ]; then
@@ -469,10 +469,10 @@ EOF
     if [[ -d "${rootfs_overrides}" && -n $(ls -A "${rootfs_overrides}") ]]; then
         echo -n "Committing ${rootfs_overrides}... "
         touch "${overrides_active_stamp}"
-        commit_ostree_layer "${rootfs_overrides}" "cosa-overrides-rootfs"
+        commit_ostree_layer "${rootfs_overrides}" "overlay/cosa-overrides-rootfs"
           cat >> "${override_manifest}" << EOF
 ostree-override-layers:
-  - cosa-overrides-rootfs
+  - overlay/cosa-overrides-rootfs
 EOF
     fi
 }
