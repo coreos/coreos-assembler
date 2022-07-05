755dfc15924c49c76733d663ae349b893c585e2b
Dusty Mabe <dusty@dustymabe.com>
2022-07-05 18:56:10 -0400

    cmd-buildfetch: add --aws-config-file option
    This will allow us to specify the AWS_CONFIG_FILE from the command
    line without having to set it in the environment.
    
    This required reworking the cosalib/s3 library into an S3() class
    so that we could call boto3.client('s3') after setting the
    AWS_CONFIG_FILE environment variable in cmd-buildfetch. Otherwise
    it would get called as soon as the library was imported and setting
    the environment variable would have no effect (too late).
    
    (cherry picked from commit 9d55a20a12f54f284ece838e2ab57c703f18a20c)

--- src/cosalib/s3.py
+++ src/cosalib/s3.py
@@ -9,51 +9,46 @@ from cosalib.cmdlib import (
 from tenacity import retry
 
 
-S3 = boto3.client('s3')
-S3CONFIG = boto3.s3.transfer.TransferConfig(
-    num_download_attempts=5
-)
+class S3(object):
+    def __init__(self):
+        self.client = boto3.client('s3')
 
+    def download_file(self, bucket, key, dest):
+        self.client.download_file(bucket, key, dest,
+            Config=boto3.s3.transfer.TransferConfig(num_download_attempts=5))
 
-def download_file(bucket, key, dest):
-    S3.download_file(bucket, key, dest, Config=S3CONFIG)
+    @retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
+    def head_bucket(self, bucket):
+        self.client.head_bucket(Bucket=bucket)
 
+    @retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
+    def head_object(self, bucket, key):
+        try:
+            self.client.head_object(Bucket=bucket, Key=key)
+        except ClientError as e:
+            if e.response['Error']['Code'] == '404':
+                return False
+            raise e
+        return True
 
-@retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
-def head_bucket(bucket):
-    S3.head_bucket(Bucket=bucket)
+    @retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
+    def list_objects(self, bucket, prefix, delimiter="/", result_key='Contents'):
+        kwargs = {
+            'Bucket': bucket,
+            'Delimiter': delimiter,
+            'Prefix': prefix,
+        }
+        isTruncated = True
+        while isTruncated:
+            batch = self.client.list_objects_v2(**kwargs)
+            yield from batch.get(result_key) or []
+            kwargs['ContinuationToken'] = batch.get('NextContinuationToken')
+            isTruncated = batch['IsTruncated']
 
-
-@retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
-def head_object(bucket, key):
-    try:
-        S3.head_object(Bucket=bucket, Key=key)
-    except ClientError as e:
-        if e.response['Error']['Code'] == '404':
-            return False
-        raise e
-    return True
-
-
-@retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
-def list_objects(bucket, prefix, delimiter="/", result_key='Contents'):
-    kwargs = {
-        'Bucket': bucket,
-        'Delimiter': delimiter,
-        'Prefix': prefix,
-    }
-    isTruncated = True
-    while isTruncated:
-        batch = S3.list_objects_v2(**kwargs)
-        yield from batch.get(result_key) or []
-        kwargs['ContinuationToken'] = batch.get('NextContinuationToken')
-        isTruncated = batch['IsTruncated']
-
-
-@retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
-def delete_object(bucket, key):
-    sub_objects = list(list_objects(bucket, key))
-    if sub_objects != []:
-        print("S3: deleting {sub_objects}")
-        S3.delete_objects(Bucket=bucket, Delete=sub_objects)
-    S3.delete_object(Bucket=bucket, Key=key)
+    @retry(stop=retry_stop, retry=retry_boto_exception, before_sleep=retry_callback)
+    def delete_object(self, bucket, key):
+        sub_objects = list(self.list_objects(bucket, key))
+        if sub_objects != []:
+            print("S3: deleting {sub_objects}")
+            self.client.delete_objects(Bucket=bucket, Delete=sub_objects)
+        self.client.delete_object(Bucket=bucket, Key=key)
