f120746fe7512a830195ec16d290fdaf8afe262d
Dusty Mabe <dusty@dustymabe.com>
2022-07-10 16:23:30 -0400

    add `coreos-assembler remote-session` command
    This adds a `remote-session` subcommand to coreos-assembler. The
    intent is for us to be able to create remote sessions on remote
    servers via `podman --remote` and execute builds there. This is
    especially attractive when considering that it elegantly enables
    us to do builds for a different architecture by targeting a remote
    host.
    
    Convenience subcommands are provided to enable easily execute arbitary
    remote commands (via `cosa remote-session exec shell`) and also copying
    files back and forth via an rsync-like interface with
    `cosa remote-session sync`.
    
    Here's what a typical session may look like:
    
    ```
    // initialize local dir
    mkdir fcos && cd fcos
    cosa init https://github.com/coreos/fedora-coreos-config.git
    cosa buildfetch
    
    // initialize remote session and sync files
    REMOTEHOST=myhost.example.com; REMOTEUSER=builder; REMOTEUID=1001
    export CONTAINER_HOST="ssh://${REMOTEUSER}@${REMOTEHOST}/run/user/${REMOTEUID}/podman/podman.sock"
    export COREOS_ASSEMBLER_REMOTE_SESSION=$(cosa remote-session create)
    cosa remote-session sync ./ :/srv/
    
    // execute build/tests
    cosa fetch && cosa build && cosa kola run
    
    // sync back results
    cosa remote-session sync :/srv/ ./
    
    // destroy remote session
    cosa remote-session destroy
    unset COREOS_ASSEMBLER_REMOTE_SESSION
    ```

---
+++ cmd/remote-session.go
@@ -0,0 +1,255 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type RemoteSessionOptions struct {
+	Image      string
+	Expiration string
+}
+
+var (
+	remoteSessionOpts RemoteSessionOptions
+
+	cmdRemoteSession = &cobra.Command{
+		Use:   "remote-session",
+		Short: "cosa remote-session [command]",
+		Long:  "Initiate and use remote sessions for COSA execution.",
+	}
+
+	cmdRemoteSessionCreate = &cobra.Command{
+		Use:   "create",
+		Short: "Create a remote session",
+		Long: "Create a remote session. This command will print an ID to " +
+			"STDOUT that should be set in COREOS_ASSEMBLER_REMOTE_SESSION " +
+			"environment variable for later commands to use.",
+		Args:    cobra.ExactArgs(0),
+		PreRunE: preRunCheckEnv,
+		RunE:    runCreate,
+	}
+
+	cmdRemoteSessionDestroy = &cobra.Command{
+		Use:   "destroy",
+		Short: "Destroy a remote session",
+		Long: "Destroy a remote session. After running this command the " +
+			"COREOS_ASSEMBLER_REMOTE_SESSION should be unset.",
+		Args:    cobra.ExactArgs(0),
+		PreRunE: preRunCheckEnv,
+		RunE:    runDestroy,
+	}
+
+	cmdRemoteSessionExec = &cobra.Command{
+		Use:     "exec",
+		Short:   "Execute a cosa command in the remote session",
+		Long:    "Execute a cosa command in the remote session.",
+		Args:    cobra.MinimumNArgs(1),
+		PreRunE: preRunCheckEnv,
+		RunE:    runExec,
+	}
+
+	cmdRemoteSessionPS = &cobra.Command{
+		Use:     "ps",
+		Short:   "Check if the remote session is running",
+		Long:    "Check if the remote session is running.",
+		Args:    cobra.ExactArgs(0),
+		PreRunE: preRunCheckEnv,
+		RunE:    runPS,
+	}
+
+	cmdRemoteSessionSync = &cobra.Command{
+		Use:   "sync",
+		Short: "sync files/directories to/from the remote",
+		Long: "sync files/directories to/from the remote. The symantics here " +
+			"are similar to rsync or scp. Provide `:from to` or `from :to`. " +
+			"The argument with the leading ':' will represent the remote.",
+		Args:    cobra.MinimumNArgs(2),
+		PreRunE: preRunCheckEnv,
+		RunE:    runSync,
+	}
+)
+
+// Function to determine if stdin is a terminal or not.
+func isatty() bool {
+	cmd := exec.Command("tty")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = ioutil.Discard
+	cmd.Stderr = ioutil.Discard
+	err := cmd.Run()
+	return err == nil
+}
+
+// Function to check if a given environment variable exists
+// and is non-empty.
+func envVarIsSet(v string) bool {
+	val, ok := os.LookupEnv(v)
+	if !ok || val == "" {
+		return false
+	} else {
+		return true
+	}
+}
+
+// Function to return an error object with appropriate text for an
+// environment variable error based on the given inputs.
+func envVarError(v string, required bool) error {
+	if required {
+		return fmt.Errorf("The env var %s must be defined and non-empty", v)
+	} else {
+		return fmt.Errorf("The env var %s must not be defined", v)
+	}
+}
+
+// Function to check requisite environment variables. This is run
+// before each subcommand to perform the checks.
+func preRunCheckEnv(c *cobra.Command, args []string) error {
+	// We need to make sure that the CONTAINER_HOST env var
+	// is set for all commands. This is used for `podman --remote`.
+	// We could also check `CONTAINER_SSHKEY` key here but it's not
+	// strictly required (user could be using ssh-agent).
+	if !envVarIsSet("CONTAINER_HOST") {
+		return envVarError("CONTAINER_HOST", true)
+	}
+	// We need to check COREOS_ASSEMBLER_REMOTE_SESSION. For create
+	// we need to make sure it's not set. For all other commands we
+	// need to make sure it is set.
+	remoteSessionVarIsSet := envVarIsSet("COREOS_ASSEMBLER_REMOTE_SESSION")
+	if c.Use == "create" && remoteSessionVarIsSet {
+		return envVarError("COREOS_ASSEMBLER_REMOTE_SESSION", false)
+	} else if c.Use != "create" && !remoteSessionVarIsSet {
+		return envVarError("COREOS_ASSEMBLER_REMOTE_SESSION", true)
+	}
+	return nil
+}
+
+// Creates a "remote session" on the remote. This just creates a
+// container on the remote and prints to STDOUT the container ID.
+// The user is then expected to store this ID in the
+// COREOS_ASSEMBLER_REMOTE_SESSION environment variable.
+func runCreate(c *cobra.Command, args []string) error {
+	podmanargs := []string{"--remote", "run", "--rm", "-d", "--privileged",
+		"--security-opt=label=disable", "--volume=/srv/",
+		"--uidmap=1000:0:1", "--uidmap=0:1:1000", "--uidmap=1001:1001:64536",
+		"--device=/dev/kvm", "--device=/dev/fuse", "--tmpfs=/tmp",
+		"--entrypoint=/usr/bin/dumb-init", remoteSessionOpts.Image,
+		"sleep", remoteSessionOpts.Expiration}
+	cmd := exec.Command("podman", podmanargs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// Destroys the "remote session". In reality it just deletes
+// the container referenced by $COREOS_ASSEMBLER_REMOTE_SESSION.
+func runDestroy(c *cobra.Command, args []string) error {
+	session := os.Getenv("COREOS_ASSEMBLER_REMOTE_SESSION")
+	podmanargs := []string{"--remote", "rm", "-f", session}
+	cmd := exec.Command("podman", podmanargs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// Executes a command in the "remote session". Mostly just a
+// `podman --remote exec`.
+func runExec(c *cobra.Command, args []string) error {
+	podmanargs := []string{"--remote", "exec", "-i"}
+	if isatty() {
+		podmanargs = append(podmanargs, "-t")
+	}
+	session := os.Getenv("COREOS_ASSEMBLER_REMOTE_SESSION")
+	podmanargs = append(podmanargs, session, "cosa")
+	podmanargs = append(podmanargs, args...)
+	cmd := exec.Command("podman", podmanargs...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			// If the command failed let's exit with the same exitcode
+			// as the remotely executed process.
+			os.Exit(exitError.ExitCode())
+		} else {
+			return err
+		}
+	}
+	return nil
+}
+
+// Executes a `podman --remote ps -a --filter id=<container>`
+// to show the status of the remote running cosa container.
+func runPS(c *cobra.Command, args []string) error {
+	session := os.Getenv("COREOS_ASSEMBLER_REMOTE_SESSION")
+	podmanargs := []string{"--remote", "ps", "-a",
+		fmt.Sprintf("--filter=id=%s", session)}
+	cmd := exec.Command("podman", podmanargs...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// runSync provides an rsync-like interface that allows
+// files to be copied to/from the remote. It uses
+// `podman --remote exec` as the transport for rsync (see [1])
+//
+// One of the arguments here must be prepended with a `:`. This
+// argument will represent the path on the remote. This function
+// will substitute `:` with `$COREOS_ASSEMBLER_REMOTE_SESSION:`.
+// That environment var just contains the container ID on the remote.
+//
+// [1] https://github.com/moby/moby/issues/13660
+func runSync(c *cobra.Command, args []string) error {
+	// check environment
+	if err := checkEnv(); err != nil {
+		return err
+	}
+	// check arguments. Need one with pre-pended ':'
+	found := 0
+	for index, arg := range args {
+		if strings.HasPrefix(arg, ":") {
+			args[index] = fmt.Sprintf("%s%s",
+				os.Getenv("COREOS_ASSEMBLER_REMOTE_SESSION"), arg)
+			found++
+		}
+	}
+	if found != 1 {
+		return fmt.Errorf("Must pass in a single arg to rsync with `:` prepended")
+	}
+	// build command and execute
+	rsyncargs := []string{"-avh", "--mkpath", "--blocking-io",
+		"--rsh", "podman --remote exec --user root -i"}
+	rsyncargs = append(rsyncargs, args...)
+	cmd := exec.Command("rsync", rsyncargs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func init() {
+	cmdRemoteSession.AddCommand(cmdRemoteSessionCreate)
+	cmdRemoteSession.AddCommand(cmdRemoteSessionDestroy)
+	cmdRemoteSession.AddCommand(cmdRemoteSessionExec)
+	cmdRemoteSession.AddCommand(cmdRemoteSessionPS)
+	cmdRemoteSession.AddCommand(cmdRemoteSessionSync)
+
+	// cmdRemoteSessionCreate options
+	cmdRemoteSessionCreate.Flags().StringVarP(
+		&remoteSessionOpts.Image, "image", "",
+		"quay.io/coreos-assembler/coreos-assembler:latest",
+		"The COSA container image to use on the remote")
+	cmdRemoteSessionCreate.Flags().StringVarP(
+		&remoteSessionOpts.Expiration, "expiration", "", "infinity",
+		"The amount of time before the remote-session auto-exits")
+}
+
+// execute the cmdRemoteSession cobra command
+func runRemoteSession(argv []string) error {
+	cmdRemoteSession.SetArgs(argv)
+	return cmdRemoteSession.Execute()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/coreos/coreos-assembler
 
 go 1.15
+
+require github.com/spf13/cobra v1.5.0
