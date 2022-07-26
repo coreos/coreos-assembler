0e1cc90196a4d41a491644d9eae20c4f41d9b3d0
Dusty Mabe <dusty@dustymabe.com>
2022-07-26 09:53:27 -0400

    cosalib: enhance run_cmd; rename to runcmd
    Replace run_cmd with a more featureful version taken from our
    coreos-ostree-importer [1]. Also rename it to runcmd and convert
    all uses to pass in a list versus a string for the command to
    decrease the chances of shell injection.
    
    [1] https://github.com/coreos/fedora-coreos-releng-automation/blob/e85d22672ef8acb79ba22a4b11d0f529585d74b7/coreos-ostree-importer/coreos_ostree_importer.py#L232-L245
    
    (cherry picked from commit 2a1121d281f71d2e620a9d0b1a5a56950170826f)
    (cherry picked from commit ed206de947e3fe43a1695f90576654d0e475117f)

--- src/cosalib/utils.py
+++ src/cosalib/utils.py
@@ -1,13 +1,27 @@
-from subprocess import check_output, CalledProcessError
+import logging
+import subprocess
+
+# Set local logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
-def run_cmd(command):
+def runcmd(cmd: list, **kwargs: int) -> subprocess.CompletedProcess:
     '''
-    Run the given command using check_call/check_output and verify its return code.
-    @param command  str command to be executed
+    Run the given command using subprocess.run and perform verification.
+    @param cmd: list that represents the command to be executed
+    @param kwargs: key value pairs that represent options to run()
     '''
     try:
-        output = check_output(command, shell=True)
-    except CalledProcessError as e:
-        raise SystemExit(f"Failed to invoke command: {e}")
-    return output
+        # default args to pass to subprocess.run
+        pargs = {"check": True, "capture_output": True}
+        logger.debug(f"Running command: {cmd}")
+        pargs.update(kwargs)
+        cp = subprocess.run(cmd, **pargs)
+    except subprocess.CalledProcessError as e:
+        logger.error("Command returned bad exitcode")
+        logger.error(f"COMMAND: {cmd}")
+        logger.error(f" STDOUT: {e.stdout.decode()}")
+        logger.error(f" STDERR: {e.stderr.decode()}")
+        raise e
+    return cp  # subprocess.CompletedProcess
