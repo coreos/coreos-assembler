7063fde6ce062be8679552b8a7d474776c767852
Joseph Marrero <jmarrero@redhat.com>
2022-09-08 09:59:57 -0400

    build-extensions-container: add version label to container

--- cmd/build-extensions-container.go
+++ cmd/build-extensions-container.go
@@ -32,7 +32,7 @@ func buildExtensionContainer() error {
 		return err
 	}
 	targetname := "extensions-container-" + buildID + "." + arch + ".ociarchive"
-	process := "runvm -- /usr/lib/coreos-assembler/build-extensions-oscontainer.sh " + arch + " $tmp_builddir/" + targetname
+	process := "runvm -- /usr/lib/coreos-assembler/build-extensions-oscontainer.sh " + arch + " $tmp_builddir/" + targetname + " " + buildID
 	if err := sh.Process(process); err != nil {
 		return err
 	}
--- src/build-extensions-oscontainer.sh
+++ src/build-extensions-oscontainer.sh
@@ -2,16 +2,18 @@
 #Used by cmd/build-extensions-container.go
 #Find the RHCOS ociarchive.
 set -euo pipefail
-buildid=$1
+arch=$1
 shift
 filename=$1
 shift
-builddir="$PWD/builds/latest/${buildid}"
+buildid=$1
+shift
+builddir="$PWD/builds/latest/${arch}"
 ostree_ociarchive=$(ls "${builddir}"/*-ostree*.ociarchive)
 # Build the image, replacing the FROM directive with the local image we have
 (cd src/config
  set -x
- podman build --from oci-archive:"$ostree_ociarchive" --network=host --build-arg COSA=true -t localhost/extensions-container -f extensions/Dockerfile .
+ podman build --from oci-archive:"$ostree_ociarchive" --network=host --build-arg COSA=true --label version="$buildid" -t localhost/extensions-container -f extensions/Dockerfile .
 )
 # Call skopeo to export it from the container storage to an oci-archive.
 (set -x
