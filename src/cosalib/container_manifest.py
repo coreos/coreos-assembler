ec09c87464e5d2616380db6e8e50a25777259b38
Jonathan Lebon <jonathan@jlebon.com>
2022-10-21 09:24:11 -0400

    cmd-push-container-manifest: store arch-specific digest in `meta.json`
    As discussed in https://github.com/coreos/coreos-assembler/issues/3122,
    ART needs the arch-specific digest in the `meta.json` rather than the
    manifest list digest. FCOS isn't planning to use the digest so doesn't
    care about which one gets chosen.
    
    Update `cosa push-container-manifest` to use the arch-specific digest.
    
    (cherry picked from commit c7b575761146d06174318552626b561038bd43e6)
    (cherry picked from commit 2efa5498d809356b5a156f3b3b8400f5663e6c15)
    (cherry picked from commit 368538212414b51f56ce6a416cd8d906a1b198b9)

--- src/cosalib/container_manifest.py
+++ src/cosalib/container_manifest.py
@@ -1,9 +1,11 @@
+import json
+
 from cosalib.utils import runcmd
 
 
-def create_local_container_manifest(repo, tag, images):
+def create_local_container_manifest(repo, tag, images) -> dict:
     '''
-    Create local manifest list
+    Create local manifest list and return the final manifest JSON
     @param images list of image specifications (including transport)
     @param repo str registry repository
     @param tag str manifest tag
@@ -13,6 +15,9 @@ def create_local_container_manifest(repo, tag, images):
     for image in images:
         cmd = ["podman", "manifest", "add", f"{repo}:{tag}", image]
         runcmd(cmd)
+    manifest_info = runcmd(["podman", "manifest", "inspect", f"{repo}:{tag}"],
+                           capture_output=True).stdout
+    return json.loads(manifest_info)
 
 
 def delete_local_container_manifest(repo, tag):
@@ -25,12 +30,11 @@ def delete_local_container_manifest(repo, tag):
     runcmd(cmd)
 
 
-def push_container_manifest(repo, tags, digestfile, v2s2=False):
+def push_container_manifest(repo, tags, v2s2=False):
     '''
     Push manifest to registry
     @param repo str registry repository
     @param tags list of tags to push
-    @param digestfile str write container digest to file
     @param v2s2 boolean use to force v2s2 format
     '''
     base_cmd = ["podman", "manifest", "push", "--all", f"{repo}:{tags[0]}"]
@@ -39,22 +43,20 @@ def push_container_manifest(repo, tags, digestfile, v2s2=False):
         # to create a manifest with 2 different mediaType. It seems to be
         # a Quay issue.
         base_cmd.extend(["--remove-signatures", "-f", "v2s2"])
-    if digestfile:
-        base_cmd.extend([f"--digestfile={digestfile}"])
     runcmd(base_cmd + [f"{repo}:{tags[0]}"])
     for tag in tags[1:]:
         runcmd(base_cmd + [f"{repo}:{tag}"])
 
 
-def create_and_push_container_manifest(repo, tags, images, v2s2, digestfile):
+def create_and_push_container_manifest(repo, tags, images, v2s2) -> dict:
     '''
-    Do it all! Create, Push, Cleanup
+    Do it all! Create, push, cleanup, and return the final manifest JSON.
     @param repo str registry repository
     @param tags list of tags
     @param images list of image specifications (including transport)
     @param v2s2 boolean use to force v2s2 format
-    @param digestfile str write container digest to file
     '''
-    create_local_container_manifest(repo, tags[0], images)
-    push_container_manifest(repo, tags, digestfile, v2s2)
+    manifest_info = create_local_container_manifest(repo, tags[0], images)
+    push_container_manifest(repo, tags, v2s2)
     delete_local_container_manifest(repo, tags[0])
+    return manifest_info
