05ffa2fb1f901824bb9b38a73179ae73288bd232
Jonathan Lebon <jonathan@jlebon.com>
2022-10-24 15:10:34 -0400

    create-legacy-oscontainer: rename command to `buildextend-legacy-oscontainer`
    This way, it's consistent with all the other artifacts we build in the
    pipeline which makes it easier to add support for. Don't retain the old
    name since it's not used anywhere else yet.
    
    (cherry picked from commit 03dd8da933722902b9775c823af68afa5187f774)
    
    jlebon: Had to add command to bash entrypoint instead of go.

--- src/buildextend-legacy-oscontainer.sh
+++ src/buildextend-legacy-oscontainer.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # Start VM and call buildah
 . /usr/lib/coreos-assembler/cmdlib.sh
 prepare_build
-runvm -- /usr/lib/coreos-assembler/create-legacy-oscontainer.py "$@"
+runvm -- /usr/lib/coreos-assembler/buildextend-legacy-oscontainer.py "$@"
