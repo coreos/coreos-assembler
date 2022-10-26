517e84d8aba9b832b6b770d787558bcd0a44fbc9
Jonathan Lebon <jonathan@jlebon.com>
2022-10-25 21:12:59 -0400

    cmd-buildextend-legacy-oscontainer: hack around broken 9p
    We're seeing the classic ENOMEM error from `cosa
    buildextend-legacy-oscontainer` when trying to write back the legacy
    oscontainer over 9p. Instead, have the supermin VM output the final OCI
    archive over virtio-serial.
    
    (cherry picked from commit 13a6ee49a7850700f393d76ae90b8555e5b850be)

--- src/buildextend-legacy-oscontainer.sh
+++ src/buildextend-legacy-oscontainer.sh
@@ -3,5 +3,12 @@
 set -euo pipefail
 # Start VM and call buildah
 . /usr/lib/coreos-assembler/cmdlib.sh
+final_outfile=$(realpath "$1"); shift
 prepare_build
-runvm -- /usr/lib/coreos-assembler/buildextend-legacy-oscontainer.py "$@"
+# shellcheck disable=SC2154
+tmp_outfile=${tmp_builddir}/legacy-oscontainer.ociarchive
+runvm -chardev "file,id=ociarchiveout,path=${tmp_outfile}" \
+    -device "virtserialport,chardev=ociarchiveout,name=ociarchiveout" -- \
+    /usr/lib/coreos-assembler/buildextend-legacy-oscontainer.py \
+        --ociarchive "/dev/virtio-ports/ociarchiveout" "$@"
+/usr/lib/coreos-assembler/finalize-artifact "${tmp_outfile}" "${final_outfile}"
