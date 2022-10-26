e81bf75c4c898c16ec02d36a4b6a2d70b7fa804f
Jonathan Lebon <jonathan@jlebon.com>
2022-10-26 10:29:33 -0400

    buildextend-legacy-oscontainer.py: rename `--ociarchive` to `--output`
    (cherry picked from commit 4b883d570d60abd3b61527c458ad8e439ada870c)

--- src/buildextend-legacy-oscontainer.sh
+++ src/buildextend-legacy-oscontainer.sh
@@ -10,5 +10,5 @@ tmp_outfile=${tmp_builddir}/legacy-oscontainer.ociarchive
 runvm -chardev "file,id=ociarchiveout,path=${tmp_outfile}" \
     -device "virtserialport,chardev=ociarchiveout,name=ociarchiveout" -- \
     /usr/lib/coreos-assembler/buildextend-legacy-oscontainer.py \
-        --ociarchive "/dev/virtio-ports/ociarchiveout" "$@"
+        --output "/dev/virtio-ports/ociarchiveout" "$@"
 /usr/lib/coreos-assembler/finalize-artifact "${tmp_outfile}" "${final_outfile}"
