coreos/coreos-assembler

commit b4cb8578850aba272fd7fb31700ddbeaaf0f9989

create_disk: Fix s390x regression from previous change
On every architecture except s390x we rely on BLS i.e.
`sysroot.bootloader=none`.

However apparently there's a bug here if we try to deploy
with the `bootloader=zipl` from the start, instead of doing
it later.  I think that's an ostree bug, but for now rework
the code so that it works the same as before on s390x.

Closes: https://github.com/coreos/coreos-assembler/issues/3067

src/create_disk.sh
@@ -162,10 +162,6 @@ if [ "${rootfs_size}" != "0" ]; then
     rootfs_size="+${rootfs_size}"
 fi
 
-# we use pure BLS on most architectures; this may
-# be overridden below
-bootloader_backend=none
-
 # shellcheck disable=SC2031
 case "$arch" in
     x86_64)
@@ -188,7 +184,6 @@ case "$arch" in
         -n ${ROOTPN}:0:"${rootfs_size}" -c ${ROOTPN}:root -t ${ROOTPN}:0FC63DAF-8483-4772-8E79-3D69D8477DE4
         ;;
     s390x)
-        bootloader_backend=zipl
         sgdisk_args=()
         if [[ ${secure_execution} -eq 1 ]]; then
             # shellcheck disable=SC2206
@@ -317,7 +312,8 @@ fi
 # Now that we have the basic disk layout, initialize the basic
 # OSTree layout, load in the ostree commit and deploy it.
 ostree admin init-fs --modern $rootfs
-ostree config --repo $rootfs/ostree/repo set sysroot.bootloader "${bootloader_backend}"
+# May be overridden below (e.g. s390x)
+ostree config --repo $rootfs/ostree/repo set sysroot.bootloader none
 # Opt-in to https://github.com/ostreedev/ostree/pull/1767 AKA
 # https://github.com/ostreedev/ostree/issues/1265
 ostree config --repo $rootfs/ostree/repo set sysroot.readonly true
@@ -491,6 +487,7 @@ ppc64le)
     install_grub_cfg
     ;;
 s390x)
+    ostree config --repo $rootfs/ostree/repo set sysroot.bootloader zipl
     # XXX: switch to using --append-karg once we have new enough rdcore
     # https://github.com/coreos/coreos-installer/pull/950
     rdcore_zipl_args=("--boot-mount=$rootfs/boot" "--kargs=ignition.firstboot")
@@ -505,7 +502,8 @@ s390x)
 esac
 
 # enable support for GRUB password
-if [ "${bootloader_backend}" = "none" ]; then
+# shellcheck disable=SC2031
+if "$arch" != "s390x"; then
     ostree config --repo $rootfs/ostree/repo set sysroot.bls-append-except-default 'grub_users=""'
 fi
 

commit ce440e82a3c3c433ecd1b69f31f808d94f431ece

create_disk: correctly configure BLS grub_users setting (CVE-2022-3675)
The conditional always failed since it's not a valid command without
the brackets. So the `sysroot.bls-append-except-default` option was
not set in builds created by affected cosa versions. This means that
on systems provisioned from those releases with a GRUB password set,
unprivileged GRUB users can boot non-default GRUB menu entries without
entering a password.

Fixes: b4cb857885 ("create_disk: Fix s390x regression from previous change")
For: https://github.com/coreos/fedora-coreos-tracker/issues/1333

src/create_disk.sh
@@ -501,7 +501,7 @@ esac
 
 # enable support for GRUB password
 # shellcheck disable=SC2031
-if "$arch" != "s390x"; then
+if [ "$arch" != s390x ]; then
     ostree config --repo $rootfs/ostree/repo set sysroot.bls-append-except-default 'grub_users=""'
 fi
 