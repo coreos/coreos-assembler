750927b16a25adf9d04b404c74d2909bec7ff065
Jonathan Lebon <jonathan@jlebon.com>
2022-11-29 15:22:08 -0500

    qemu.go: drop `vsmt=8` and `cap-fwnmi=off` qemu args for ppc64le
    We're troubleshooting performance issues on a ppc64le machine and one of
    the things that came up comparing what we do to e.g. libguestfs is that
    the latter just does `-machine pseries,accel=kvm:tcg` whereas we have
    these extra args.
    
    - `vsmt=8` was added to work around issues with RHEL7 kernels, which is
      no longer relevant to us
    - `cap-fwnmi=off` was added in #1975; it's not clear if we still need it
      or not. Things seem to work fine without it in manual tests on a
      ppc64le machine.
    
    Keep `kvm-type=HV` which dates from the introduction of ppc64le support
    in kola. Looking at the QEMU docs at
    https://qemu.readthedocs.io/en/latest/system/ppc/pseries.html#switching-between-the-kvm-pr-and-kvm-hv-kernel-module
    it ensures that we directly use virtualization support in the CPU.
    
    (cherry picked from commit 54914bf2c9ba045fe2b7775fc2b198b939eb1ca0)

--- mantle/platform/qemu.go
+++ mantle/platform/qemu.go
@@ -1088,7 +1088,9 @@ func baseQemuArgs() []string {
 	case "ppc64le":
 		ret = []string{
 			"qemu-system-ppc64",
-			"-machine", "pseries,kvm-type=HV,vsmt=8,cap-fwnmi=off," + accel,
+			// kvm-type=HV ensures we use bare metal KVM and not "user mode"
+			// https://qemu.readthedocs.io/en/latest/system/ppc/pseries.html#switching-between-the-kvm-pr-and-kvm-hv-kernel-module
+			"-machine", "pseries,kvm-type=HV," + accel,
 		}
 	default:
 		panic(fmt.Sprintf("RpmArch %s combo not supported for qemu ", system.RpmArch()))
