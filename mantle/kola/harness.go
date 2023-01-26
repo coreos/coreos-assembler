56abda41e9bba2e89bbeb27e6bc0a77a3369da07
Renata Ravanelli <rravanel@redhat.com>
2023-01-26 19:30:46 +0000

    Make functions public accessible to all other packages
    This patch is a backport of #3298
    
    Backport:
    * Patch refresh - no functional change.
    
    Signed-off-by: Renata Ravanelli <rravanel@redhat.com>

--- mantle/kola/harness.go
+++ mantle/kola/harness.go
@@ -265,8 +265,8 @@ func NewFlight(pltfrm string) (flight platform.Flight, err error) {
 	return
 }
 
-// matchesPatterns returns true if `s` matches one of the patterns in `patterns`.
-func matchesPatterns(s string, patterns []string) (bool, error) {
+// MatchesPatterns returns true if `s` matches one of the patterns in `patterns`.
+func MatchesPatterns(s string, patterns []string) (bool, error) {
 	for _, pattern := range patterns {
 		match, err := filepath.Match(pattern, s)
 		if err != nil {
@@ -279,8 +279,8 @@ func matchesPatterns(s string, patterns []string) (bool, error) {
 	return false, nil
 }
 
-// hasString returns true if `s` equals one of the strings in `slice`.
-func hasString(s string, slice []string) bool {
+// HasString returns true if `s` equals one of the strings in `slice`.
+func HasString(s string, slice []string) bool {
 	for _, e := range slice {
 		if e == s {
 			return true
@@ -319,7 +319,7 @@ type ManifestData struct {
 	} `yaml:"add-commit-metadata"`
 }
 
-func parseDenyListYaml(pltfrm string) error {
+func ParseDenyListYaml(pltfrm string) error {
 	var objs []DenyListObj
 
 	// Parse kola-denylist into structs
@@ -362,15 +362,15 @@ func parseDenyListYaml(pltfrm string) error {
 	// Accumulate patterns filtering by set policies
 	plog.Debug("Processing denial patterns from yaml...")
 	for _, obj := range objs {
-		if len(obj.Arches) > 0 && !hasString(arch, obj.Arches) {
+		if len(obj.Arches) > 0 && !HasString(arch, obj.Arches) {
 			continue
 		}
 
-		if len(obj.Platforms) > 0 && !hasString(pltfrm, obj.Platforms) {
+		if len(obj.Platforms) > 0 && !HasString(pltfrm, obj.Platforms) {
 			continue
 		}
 
-		if len(stream) > 0 && len(obj.Streams) > 0 && !hasString(stream, obj.Streams) {
+		if len(stream) > 0 && len(obj.Streams) > 0 && !HasString(stream, obj.Streams) {
 			continue
 		}
 
@@ -418,7 +418,7 @@ func filterTests(tests map[string]*register.Test, patterns []string, pltfrm stri
 	// Higher-level functions default to '*' if the user didn't pass anything.
 	// Notice this. (This totally ignores the corner case where the user
 	// actually typed '*').
-	userTypedPattern := !hasString("*", patterns)
+	userTypedPattern := !HasString("*", patterns)
 	for name, t := range tests {
 		if NoNet && testRequiresInternet(t) {
 			plog.Debugf("Skipping test that requires network: %s", t.Name)
@@ -463,14 +463,14 @@ func filterTests(tests map[string]*register.Test, patterns []string, pltfrm stri
 			continue
 		}
 
-		nameMatch, err := matchesPatterns(t.Name, patterns)
+		nameMatch, err := MatchesPatterns(t.Name, patterns)
 		if err != nil {
 			return nil, err
 		}
 
 		tagMatch := false
 		for _, tag := range positiveTags {
-			tagMatch = hasString(tag, t.Tags) || tag == t.RequiredTag
+			tagMatch = HasString(tag, t.Tags) || tag == t.RequiredTag
 			if tagMatch {
 				break
 			}
@@ -478,7 +478,7 @@ func filterTests(tests map[string]*register.Test, patterns []string, pltfrm stri
 
 		negativeTagMatch := false
 		for _, tag := range negativeTags {
-			negativeTagMatch = hasString(tag, t.Tags)
+			negativeTagMatch = HasString(tag, t.Tags)
 			if negativeTagMatch {
 				break
 			}
@@ -586,7 +586,7 @@ func runProvidedTests(testsBank map[string]*register.Test, patterns []string, mu
 	//    either way
 
 	// Add denylisted tests in kola-denylist.yaml to DenylistedTests
-	err := parseDenyListYaml(pltfrm)
+	err := ParseDenyListYaml(pltfrm)
 	if err != nil {
 		plog.Fatal(err)
 	}
