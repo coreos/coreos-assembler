788f1c99771d64e12a90df73f8651b3aa5bcc5e5
Nikita Dubrovskii <nikita@linux.ibm.com>
2023-02-14 16:52:03 +0100

    s390x: support Ignition private key for official builds
    Signed-off-by: Nikita Dubrovskii <nikita@linux.ibm.com>

--- src/secex-genprotimgvm-scripts/genprotimg-script.sh
+++ src/secex-genprotimgvm-scripts/genprotimg-script.sh
@@ -6,10 +6,18 @@ echo "Preparing for genprotimg-daemon"
 
 source="/build/genprotimg"
 destination="/genprotimg"
+pkey="usr/lib/coreos/ignition.asc"
+
+trap "rm -f ${source}/${pkey}" EXIT
 
 # Files need to be named correctly
 # genprotimg daemon can only see /genprotimg folder
 cp "${source}/vmlinuz" "${source}/initrd.img" "${source}/parmfile" "${destination}/"
+
+# Append Ignition GPG private key to initramfs
+cd "${source}"
+echo "${pkey}" | cpio --quiet -H newc -o | gzip -9 -n >> "${destination}/initrd.img"
+rm "${pkey}"
 
 # Signal daemon that it can run genprotimg
 touch "${destination}/signal.file"
