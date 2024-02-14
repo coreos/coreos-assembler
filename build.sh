5e37923bfc8f5709ee818bd882958c39077e0355
Dusty Mabe <dusty@dustymabe.com>
2024-02-14 15:41:21 -0500

    osbuild: add sanity check in qemu OSbuild stage
    This will compare the image that was just created to see if it
    has any problems.

--- build.sh
+++ build.sh
@@ -178,6 +178,7 @@ patch_osbuild() {
     # Now all the software is under the /usr/lib/osbuild dir and we can patch
     cat /usr/lib/coreos-assembler/0004-fscache-add-eviction-log-statement.patch                   \
         /usr/lib/coreos-assembler/0001-stages-zipl.inst-improve-kernel-initrd-path-resoluti.patch \
+        /usr/lib/coreos-assembler/0001-stages-qemu-sanity-check-created-image.patch               \
             | patch -d /usr/lib/osbuild -p1
 
     # And then move the files back; supermin appliance creation will need it back
