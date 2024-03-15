coreos/coreos-assembler

commit b8f70e813b53af34148d98600daeee0be75a9b53

kola: handle `--oscontainer` imperatively
Right now, `--oscontainer` is handled by a systemd unit which runs
`rpm-ostree rebase` and then reboots the system. The main disadvantage
with this is that it means we don't actually get any logging of the
pivot step since it happens before SSH comes up.

But also, I want to make that switch support local OCI archives, but the
way it's currently implemented won't work with that since there's no
way to `DropFile()` it before this unit kicks in.

Move handling that arg to `runTest()` time.

mantle/kola/harness.go
@@ -1839,6 +1839,16 @@ func runTest(h *harness.H, t *register.Test, pltfrm string, flight platform.Flig
 		}
 	}
 
+	if Options.OSContainer != "" {
+		if CosaBuild == nil {
+			h.Fatalf("Requested oscontainer pivot, but no cosa build found")
+		}
+		for _, m := range tcluster.Machines() {
+			tcluster.RunCmdSyncf(m, "sudo rpm-ostree rebase --experimental %s", Options.OSContainer)
+			m.Reboot()
+		}
+	}
+
 	if t.ExternalTest != "" {
 		setupExternalTest(h, t, tcluster)
 		// Collect the journal logs after execution is finished

mantle/platform/cluster.go
@@ -272,27 +272,6 @@ func (bc *BaseCluster) RenderUserData(userdata *platformConf.UserData, ignitionV
 		conf.DisableAutomaticUpdates()
 	}
 
-	if bc.bf.baseopts.OSContainer != "" {
-		conf.AddSystemdUnit("kola-container-rebase.service", fmt.Sprintf(`[Unit]
-Description=Rebase to target container
-ConditionPathExists=!/etc/kola-rebase-done
-Before=sshd.service
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-Type=oneshot
-# Newer zincati more explicitly fails on containers; fully disable it
-ExecStart=systemctl mask --now zincati
-ExecStart=rpm-ostree rebase --experimental %s
-ExecStart=touch /etc/kola-rebase-done
-ExecStart=systemctl reboot
-
-[Install]
-WantedBy=multi-user.target
-`, bc.bf.baseopts.OSContainer), platformConf.Enable)
-	}
-
 	if conf.IsIgnition() {
 		if !conf.ValidConfig() {
 			return nil, fmt.Errorf("invalid ignition config")

commit 836f8ac6a230a1a5906b0bf97051715e9c9e842a

harness: don't require cosa build for `--oscontainer`
The `--oscontainer` switch takes either a filename to an OCI archive or
a pullspec. In neither case is a cosa directory required.

Fixes b8f70e813 ("kola: handle `--oscontainer` imperatively") which
mistakenly cargo culted that bit from surrounding code.

mantle/kola/harness.go
@@ -1840,9 +1840,6 @@ func runTest(h *harness.H, t *register.Test, pltfrm string, flight platform.Flig
 	}
 
 	if Options.OSContainer != "" {
-		if CosaBuild == nil {
-			h.Fatalf("Requested oscontainer pivot, but no cosa build found")
-		}
 		rebase_arg := Options.OSContainer
 		// if it looks like a path to an OCI archive, then copy it into the system
 		if strings.HasSuffix(Options.OSContainer, ".ociarchive") {