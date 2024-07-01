57d158fa2e3b6b477f235a1211bf1d54bda36413
Nikita Dubrovskii <nikita@linux.ibm.com>
2024-07-01 14:21:58 +0200

    s390x: enable osbuild based secex build

--- src/secex-genprotimgvm-scripts/post-script.sh
+++ src/secex-genprotimgvm-scripts/post-script.sh
@@ -4,21 +4,26 @@ set -exuo pipefail
 
 echo "Moving sdboot and executing zipl"
 
-workdir="/build"
-sdboot="/genprotimg/se.img"
-genprotimg_dir="${workdir}/genprotimg"
-se_boot=$(mktemp -d /tmp/se-XXXXXX)
+# CoreOS based secure-vm uses '/var' as prefix
+PREFIX=/
+if [ -e /var/genprotimg ]; then
+    PREFIX=/var
+fi
 
+# This is our coreos.qemu-secex.qcow2
 disk=$(realpath /dev/disk/by-id/virtio-target)
-disk_se="${disk}1"
 
-mount "${disk_se}" "${se_boot}"
-cp "${sdboot}" "${se_boot}/sdboot"
-zipl -V -i ${se_boot}/sdboot -t ${se_boot}
+
+# 'se' labeled partition on target image, holds 'sdboot' image
+disk_se="${disk}1"
+se_mnt=$(mktemp -d /tmp/se-XXXXXX)
+mount "${disk_se}" "${se_mnt}"
+cp "${PREFIX}/genprotimg/se.img" "${se_mnt}/sdboot"
+zipl -V -i "${se_mnt}/sdboot" -t "${se_mnt}"
+umount "${se_mnt}"
+rmdir "${se_mnt}"
+
 
 # Disable debug output, the last message should be success
 set +x
 echo "Success, added sdboot to image and executed zipl"
-
-umount "${se_boot}"
-rm -rf "${se_boot}"
