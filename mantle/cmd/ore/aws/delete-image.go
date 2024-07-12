dbcbbdd49f735dfda19f625d30577b3506297d37
gursewak1997 <gursmangat@gmail.com>
2024-07-12 10:12:25 -0700

    mantle/ore: add ore aws delete-image
    This enables us to deregister aws AMIs and/or snapshots.
    It also handles the cases of resource IDs unavailable and
    notFound by not returning an error.

---
+++ mantle/cmd/ore/aws/delete-image.go
@@ -0,0 +1,68 @@
+// Copyright 2017 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aws
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	cmdDeleteImage = &cobra.Command{
+		Use:   "delete-image --ami <ami_id> --snapshot <snapshot_id> ...",
+		Short: "Delete AMI and/or snapshot",
+		Run:   runDeleteImage,
+	}
+	amiID        string
+	snapshotID   string
+	allowMissing bool
+)
+
+func init() {
+	// Initialize the command and its flags
+	AWS.AddCommand(cmdDeleteImage)
+	cmdDeleteImage.Flags().StringVar(&amiID, "ami", "", "AWS ami tag")
+	cmdDeleteImage.Flags().StringVar(&snapshotID, "snapshot", "", "AWS snapshot tag")
+	cmdDeleteImage.Flags().BoolVar(&allowMissing, "allow-missing", false, "Do not error out on the resource not existing")
+}
+
+func runDeleteImage(cmd *cobra.Command, args []string) {
+	// Check if either amiID or snapshotID is provided
+	if amiID == "" && snapshotID == "" {
+		fmt.Fprintf(os.Stderr, "Provide --ami or --snapshot to delete\n")
+		os.Exit(1)
+	}
+
+	// Remove resources based on provided flags
+	if amiID != "" {
+		err := API.RemoveByAmiTag(amiID, allowMissing)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not delete %v: %v\n", amiID, err)
+			os.Exit(1)
+		}
+	}
+
+	if snapshotID != "" {
+		err := API.RemoveBySnapshotTag(snapshotID, allowMissing)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not delete %v: %v\n", snapshotID, err)
+			os.Exit(1)
+		}
+	}
+
+	os.Exit(0)
+}
