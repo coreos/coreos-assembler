19314f65174deea7cd8fc65e94e4bedb8a38af5e
Dusty Mabe <dusty@dustymabe.com>
2024-09-17 12:42:35 -0400

    workaround selinux issues with osbuild
    We have a few issues right now where files in our images
    don't have any selinux context (i.e. end up unlabeled_t).
    Here we workaround the hidden mountpoints issue [1] with
    a patch to OSBuild to hardcode some chcon calls. We
    workaround the "bunch of files under /sysroot are unlabeled"
    issue [2] by backported a proposed upstream change to
    the org.osbuild.selinux stage [3] and then using it to
    explicitly set the context on the root of the tree to
    `root_t`. We also add a fix [4] for another issue where
    '/boot/coreos/platforms.json' would end up with the
    wrong label.
    
    [1] https://github.com/coreos/fedora-coreos-tracker/issues/1771
    [2] https://github.com/coreos/fedora-coreos-tracker/issues/1772
    [3] https://github.com/osbuild/osbuild/pull/1889
    [4] https://github.com/osbuild/osbuild/pull/1888
    
    (cherry picked from commit d3302e0fc9bedec2d4e935e3528eb5abd44e7ae8)

--- build.sh
+++ build.sh
@@ -178,6 +178,9 @@ patch_osbuild() {
     # Now all the software is under the /usr/lib/osbuild dir and we can patch
     cat /usr/lib/coreos-assembler/0004-fscache-add-eviction-log-statement.patch                   \
         /usr/lib/coreos-assembler/0001-stages-qemu-sanity-check-created-image.patch               \
+        /usr/lib/coreos-assembler/0001-stages-coreos.platform-use-shutil.copy.patch               \
+        /usr/lib/coreos-assembler/0001-stages-selinux-don-t-require-file_contexts-if-labels.patch \
+        /usr/lib/coreos-assembler/0001-hacks-for-coreos-selinux-issues.patch                      \
             | patch -d /usr/lib/osbuild -p1
 
     # And then move the files back; supermin appliance creation will need it back
