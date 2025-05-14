fbfbe27d4ab17008709d8e3ad24c9dac03c42f3d
Michael Armijo <marmijo@redhat.com>
2025-05-14 17:58:40 -0600

    ore/azure: add options to create and delete Shared Image Gallery Images
    Add a new `create-gallery-image` ore command to Support creating images
    within Azure Shared Image Galleries (Gallery Images). The command
    creates image definitions and versions in Azure Shared Image Galleries.
    Gallery images can be created from either a blob URL or an existing
    managed image. A `--azure-publisher` flag is added to assign a publisher
    to the gallery image.
    
    `delete-gallery-image` is also added to delete individual gallery images
    or an entire Shared Image Gallery.

---
+++ mantle/cmd/ore/azure/delete-gallery-image.go
@@ -0,0 +1,69 @@
+// Copyright 2025 Red Hat
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package azure
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	cmdDeleteGalleryImage = &cobra.Command{
+		Use:     "delete-gallery-image",
+		Short:   "Delete Azure Gallery image",
+		Long:    "Remove a Shared Image Gallery image from Azure.",
+		RunE:    runDeleteGalleryImage,
+		Aliases: []string{"delete-image-arm"},
+
+		SilenceUsage: true,
+	}
+
+	deleteGallery bool
+)
+
+func init() {
+	sv := cmdDeleteGalleryImage.Flags().StringVar
+	bv := cmdDeleteGalleryImage.Flags().BoolVar
+
+	sv(&imageName, "image-name", "", "image name")
+	sv(&resourceGroup, "resource-group", "kola", "resource group name")
+	sv(&galleryName, "gallery-name", "kola", "gallery name")
+	bv(&deleteGallery, "delete-gallery", false, "delete entire gallery")
+
+	Azure.AddCommand(cmdDeleteGalleryImage)
+}
+
+func runDeleteGalleryImage(cmd *cobra.Command, args []string) error {
+	if err := api.SetupClients(); err != nil {
+		return fmt.Errorf("setting up clients: %v\n", err)
+	}
+
+	if deleteGallery {
+		err := api.DeleteGallery(galleryName, resourceGroup)
+		if err != nil {
+			return fmt.Errorf("Couldn't delete gallery: %v\n", err)
+		}
+		return nil
+	}
+
+	err := api.DeleteGalleryImage(imageName, resourceGroup, galleryName)
+	if err != nil {
+		return fmt.Errorf("Couldn't delete gallery image: %v\n", err)
+	}
+
+	plog.Printf("Image %q in gallery %q in resource group %q removed", imageName, galleryName, resourceGroup)
+	return nil
+}
