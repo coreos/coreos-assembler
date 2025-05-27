d7074e28549f12c714bb51f14bb17098aa9b4248
Dusty Mabe <dusty@dustymabe.com>
2025-05-27 14:56:22 -0400

    osbuild: drop patch fixing s390x ISO paths
    The upstream fix has landed in a release so we no longer need this
    patch. This reverts commit d31cd4cb3eec267806338e0231bcc64949971cd2.

--- build.sh
+++ build.sh
@@ -170,25 +170,25 @@ write_archive_info() {
 }
 
 patch_osbuild() {
-   # return # we have no patches right now
+   return # we have no patches right now
    ## Add a few patches that either haven't made it into a release or
    ## that will be obsoleted with other work that will be done soon.
 
    ## To make it easier to apply patches we'll move around the osbuild
    ## code on the system first:
-   rmdir /usr/lib/osbuild/osbuild
-   mv /usr/lib/python3.13/site-packages/osbuild /usr/lib/osbuild/
-   mkdir /usr/lib/osbuild/tools
-   mv /usr/bin/osbuild-mpp /usr/lib/osbuild/tools/
+   #rmdir /usr/lib/osbuild/osbuild
+   #mv /usr/lib/python3.13/site-packages/osbuild /usr/lib/osbuild/
+   #mkdir /usr/lib/osbuild/tools
+   #mv /usr/bin/osbuild-mpp /usr/lib/osbuild/tools/
 
    ## Now all the software is under the /usr/lib/osbuild dir and we can patch
-   patch -d /usr/lib/osbuild -p1 < src/0001-stages-coreos.live-artifacts-fix-kernel-and-initrd-p.patch
+   #cat foo.patch | patch -d /usr/lib/osbuild -p1
    #
    ## And then move the files back; supermin appliance creation will need it back
    ## in the places delivered by the RPM.
-   mv /usr/lib/osbuild/tools/osbuild-mpp /usr/bin/osbuild-mpp
-   mv /usr/lib/osbuild/osbuild /usr/lib/python3.13/site-packages/osbuild
-   mkdir /usr/lib/osbuild/osbuild
+   #mv /usr/lib/osbuild/tools/osbuild-mpp /usr/bin/osbuild-mpp
+   #mv /usr/lib/osbuild/osbuild /usr/lib/python3.13/site-packages/osbuild
+   #mkdir /usr/lib/osbuild/osbuild
 }
 
 if [ $# -ne 0 ]; then
