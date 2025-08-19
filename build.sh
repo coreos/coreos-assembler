10945dccbded84641532ba7cd6c207e594fee133
Dusty Mabe <dusty@dustymabe.com>
2025-08-19 10:42:42 -0400

    osbuild: drop patch for pulling os name from /usr/lib/os-release
    The upstream fix [1] has landed in a release [2] so we no longer need
    this patch.
    
    [1] https://github.com/osbuild/osbuild/commit/7593b65dc77bf5dbb26fe9c54386759ab7f740a7
    [2] https://github.com/osbuild/osbuild/releases/tag/v157

--- build.sh
+++ build.sh
@@ -193,25 +193,25 @@ write_archive_info() {
 }
 
 patch_osbuild() {
+   return # we have no patches right now
    ## Add a few patches that either haven't made it into a release or
    ## that will be obsoleted with other work that will be done soon.
 
    ## To make it easier to apply patches we'll move around the osbuild
    ## code on the system first:
-   rmdir /usr/lib/osbuild/osbuild
-   mv /usr/lib/python3.13/site-packages/osbuild /usr/lib/osbuild/
-   mkdir /usr/lib/osbuild/tools
-   mv /usr/bin/osbuild-mpp /usr/lib/osbuild/tools/
+   #rmdir /usr/lib/osbuild/osbuild
+   #mv /usr/lib/python3.13/site-packages/osbuild /usr/lib/osbuild/
+   #mkdir /usr/lib/osbuild/tools
+   #mv /usr/bin/osbuild-mpp /usr/lib/osbuild/tools/
 
    ## Now all the software is under the /usr/lib/osbuild dir and we can patch
    #cat foo.patch | patch -d /usr/lib/osbuild -p1
-   patch -d /usr/lib/osbuild -p1 < /usr/lib/coreos-assembler/0001-live-artifacts-read-os-name-from-usr-lib-os-release.patch
-
+   #
    ## And then move the files back; supermin appliance creation will need it back
    ## in the places delivered by the RPM.
-   mv /usr/lib/osbuild/tools/osbuild-mpp /usr/bin/osbuild-mpp
-   mv /usr/lib/osbuild/osbuild /usr/lib/python3.13/site-packages/osbuild
-   mkdir -p /usr/lib/osbuild/osbuild
+   #mv /usr/lib/osbuild/tools/osbuild-mpp /usr/bin/osbuild-mpp
+   #mv /usr/lib/osbuild/osbuild /usr/lib/python3.13/site-packages/osbuild
+   #mkdir -p /usr/lib/osbuild/osbuild
 }
 
 if [ $# -ne 0 ]; then
