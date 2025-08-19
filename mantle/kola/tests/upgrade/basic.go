6b92f3476bf65a89b51029f5779b72720669466c
Jonathan Lebon <jonathan@jlebon.com>
2025-08-19 16:16:54 -0400

    kola/tests/upgrade: tweak for OCI payloads
    First, I'm not sure why `:latest` was appended to the filename of OCI
    archives. I think that's just a historical artifact. But nowadays, at
    least with the new non-encapsulated OCI archives, this causes an error
    with skopeo during rebasing:
    
        error: Preparing import: Fetching manifest: failed to invoke method OpenImage: no descriptor found for reference "latest"
    
    So drop that.
    
    Also, don't rely on `ostree-commit` from `meta.json` yet when
    synthesizing another commit to upgrade to, since it's not reproducible
    yet. This is fixed in latest bootc, but it still needs to propagate.
    
    (There's a larger picture issue here which is that this test is becoming
    less and less realistic as we keep going. We shouldn't be liberally
    doing rpm-ostree rebases like this, but instead try to leverage Zincati
    more. Also the synthetic update should be fetched as another container
    image in a registry, possibly hosted locally, rather than an OSTree
    commit.)

--- mantle/kola/tests/upgrade/basic.go
+++ mantle/kola/tests/upgrade/basic.go
@@ -148,7 +148,7 @@ func fcosUpgradeBasic(c cluster.TestCluster) {
 		c.Fatal(err)
 	}
 	usingContainer := booted.ContainerImageReference != ""
-	sourceContainerRef := fmt.Sprintf("ostree-unverified-image:oci-archive:%s:latest", containerImageFilename)
+	sourceContainerRef := fmt.Sprintf("ostree-unverified-image:oci-archive:%s", containerImageFilename)
 
 	c.Run("setup", func(c cluster.TestCluster) {
 		ostreeref := kola.CosaBuild.Meta.BuildRef
@@ -162,13 +162,12 @@ func fcosUpgradeBasic(c cluster.TestCluster) {
 		}
 
 		// Keep any changes around here in sync with tests/rhcos/upgrade.go too!
-
 		// See https://github.com/coreos/fedora-coreos-tracker/issues/812
 		if usingContainer {
 			// In the container path we'll pass this file directly, so put it outside
 			// of the user's home directory so the systemd service can find it.
 			c.RunCmdSyncf(m, "sudo mv %s /var/tmp/%s", containerImageFilename, containerImageFilename)
-			sourceContainerRef = fmt.Sprintf("ostree-unverified-image:oci-archive:/var/tmp/%s:latest", containerImageFilename)
+			sourceContainerRef = fmt.Sprintf("ostree-unverified-image:oci-archive:/var/tmp/%s", containerImageFilename)
 		} else {
 			tmprepo := workdir + "/repo-bare"
 			// TODO: https://github.com/ostreedev/ostree-rs-ext/issues/34
@@ -210,11 +209,13 @@ func fcosUpgradeBasic(c cluster.TestCluster) {
 
 	c.Run("upgrade-from-current", func(c cluster.TestCluster) {
 		newVersion := kola.CosaBuild.Meta.OstreeVersion + ".kola"
-		ostreeCommit := kola.CosaBuild.Meta.OstreeCommit
 		if usingContainer {
+			// until https://github.com/bootc-dev/bootc/pull/1421 propagates, we can't rely on kola.CosaBuild.Meta.OstreeCommit being the same
+			ostreeCommit := c.MustSSHf(m, "sudo rpm-ostree status --json | jq -r '.deployments[0].checksum'")
 			newCommit := c.MustSSHf(m, "sudo ostree commit -b testupdate --tree=ref=%s --add-metadata-string version=%s", ostreeCommit, newVersion)
 			rpmostreeRebase(c, m, string(newCommit), newVersion)
 		} else {
+			ostreeCommit := kola.CosaBuild.Meta.OstreeCommit
 			ostree_command := "ostree commit --repo %s -b %s --tree ref=%s --add-metadata-string version=%s " +
 				"--keep-metadata='fedora-coreos.stream' --keep-metadata='coreos-assembler.basearch' --parent=%s"
 			newCommit := c.MustSSHf(m,
