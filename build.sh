0dbe4f43f3d757ec48115a49f68a52e89335ad5d
jbtrystram <jbtrystram@redhat.com>
2025-09-08 11:37:13 +0200

    osbuild: drop patches
    Osbuild 159 was release so we can drop patches.

--- build.sh
+++ build.sh
@@ -195,22 +195,23 @@ write_archive_info() {
 patch_osbuild() {
    # Add a few patches that either haven't made it into a release or
    # that will be obsoleted with other work that will be done soon.
+   return 0
 
-   # To make it easier to apply patches we'll move around the osbuild
-   # code on the system first:
-   rmdir /usr/lib/osbuild/osbuild
-   mv /usr/lib/python3.13/site-packages/osbuild /usr/lib/osbuild/
-   mkdir /usr/lib/osbuild/tools
-   mv /usr/bin/osbuild-mpp /usr/lib/osbuild/tools/
+   # # To make it easier to apply patches we'll move around the osbuild
+   # # code on the system first:
+   # rmdir /usr/lib/osbuild/osbuild
+   # mv /usr/lib/python3.13/site-packages/osbuild /usr/lib/osbuild/
+   # mkdir /usr/lib/osbuild/tools
+   # mv /usr/bin/osbuild-mpp /usr/lib/osbuild/tools/
 
-   # Now all the software is under the /usr/lib/osbuild dir and we can patch
-   patch -d /usr/lib/osbuild -p1 < /usr/lib/coreos-assembler/0001-stages-coreos.live-artifacts-rework-volume-ID-handli.patch
+   # # Now all the software is under the /usr/lib/osbuild dir and we can patch
+   # patch -d /usr/lib/osbuild -p1 < /usr/lib/coreos-assembler/src/my-git-commit.patch
 
-   # And then move the files back; supermin appliance creation will need it back
-   # in the places delivered by the RPM.
-   mv /usr/lib/osbuild/tools/osbuild-mpp /usr/bin/osbuild-mpp
-   mv /usr/lib/osbuild/osbuild /usr/lib/python3.13/site-packages/osbuild
-   mkdir -p /usr/lib/osbuild/osbuild
+   # # And then move the files back; supermin appliance creation will need it back
+   # # in the places delivered by the RPM.
+   # mv /usr/lib/osbuild/tools/osbuild-mpp /usr/bin/osbuild-mpp
+   # mv /usr/lib/osbuild/osbuild /usr/lib/python3.13/site-packages/osbuild
+   # mkdir -p /usr/lib/osbuild/osbuild
 }
 
 if [ $# -ne 0 ]; then
