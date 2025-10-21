625fe2f38f9dd4c7e2c8e696d683592e4bc942c0
jbtrystram <jbtrystram@redhat.com>
2025-10-21 16:46:05 +0200

    osbuild/ bootc-install: symlink aleph files
    Create symlinks to the aleph file created by bootc so our tests and
    tooling find the aleph at the expected path.

--- src/cmdlib.sh
+++ src/cmdlib.sh
@@ -770,10 +770,11 @@ runvm() {
 
     # include COSA in the image
     find /usr/lib/coreos-assembler/ -type f > "${vmpreparedir}/hostfiles"
-    cat <<EOF >> "${vmpreparedir}/hostfiles"
-/usr/lib/osbuild/stages/org.osbuild.coreos.live-artifacts.mono
-/usr/lib/osbuild/stages/org.osbuild.coreos.live-artifacts.mono.meta.json
-EOF
+    # Include arbitrary files from the host  
+#     cat <<EOF >> "${vmpreparedir}/hostfiles"
+# /usr/lib/osbuild/stages/org.osbuild.ln
+# /usr/lib/osbuild/stages/org.osbuild.ln.meta.json
+# EOF
 
     # and include all GPG keys
     echo '/etc/pki/rpm-gpg/*' >> "${vmpreparedir}/hostfiles"
