2c056e2bf14786366264639c7d1998b1117f6983
Dusty Mabe <dusty@dustymabe.com>
2025-11-13 15:57:34 -0500

    osbuild: backport patches for setting xfs agcount
    This will allow us to set the agcount when creating XFS filesystems.
    
    https://github.com/osbuild/osbuild/pull/2241

--- build.sh
+++ build.sh
@@ -192,26 +192,25 @@ write_archive_info() {
 }
 
 patch_osbuild() {
-    return # No patches at this time
-  # # Add a few patches that either haven't made it into a release or
-  # # that will be obsoleted with other work that will be done soon.
-  # #
-  # # To make it easier to apply patches we'll move around the osbuild
-  # # code on the system first:
-  # rmdir /usr/lib/osbuild/osbuild
-  # python_lib_dir=$(ls -d /usr/lib/python*)
-  # mv ${python_lib_dir}/site-packages/osbuild /usr/lib/osbuild/
-  # mkdir -p /usr/lib/osbuild/tools
-  # mv /usr/bin/osbuild-mpp /usr/lib/osbuild/tools/
-  # # Now all the software is under the /usr/lib/osbuild dir and we can patch
-  # cat \
-  #     /usr/lib/coreos-assembler/0001.patch \
-  #         | patch -d /usr/lib/osbuild -p1
-  # # And then move the files back; supermin appliance creation will need it back
-  # # in the places delivered by the RPM.
-  # mv /usr/lib/osbuild/tools/osbuild-mpp /usr/bin/osbuild-mpp
-  # mv /usr/lib/osbuild/osbuild ${python_lib_dir}/site-packages/osbuild
-  # mkdir -p /usr/lib/osbuild/osbuild
+    # Add a few patches that either haven't made it into a release or
+    # that will be obsoleted with other work that will be done soon.
+    #
+    # To make it easier to apply patches we'll move around the osbuild
+    # code on the system first:
+    rmdir /usr/lib/osbuild/osbuild
+    python_lib_dir=$(ls -d /usr/lib/python*)
+    mv ${python_lib_dir}/site-packages/osbuild /usr/lib/osbuild/
+    mkdir -p /usr/lib/osbuild/tools
+    mv /usr/bin/osbuild-mpp /usr/lib/osbuild/tools/
+    # Now all the software is under the /usr/lib/osbuild dir and we can patch
+    cat \
+        /usr/lib/coreos-assembler/0001-stages-mkfs.xfs-support-setting-agcount.patch \
+            | patch -d /usr/lib/osbuild -p1
+    # And then move the files back; supermin appliance creation will need it back
+    # in the places delivered by the RPM.
+    mv /usr/lib/osbuild/tools/osbuild-mpp /usr/bin/osbuild-mpp
+    mv /usr/lib/osbuild/osbuild ${python_lib_dir}/site-packages/osbuild
+    mkdir -p /usr/lib/osbuild/osbuild
 }
 
 if [ $# -ne 0 ]; then
