47450dbbab27aca68ea05111705bf4cdcf917bb8
Nikita Dubrovskii <nikita@linux.ibm.com>
2025-11-13 16:20:57 +0100

    kola: drop InstalledMachine struct

--- mantle/cmd/kola/testiso.go
+++ mantle/cmd/kola/testiso.go
@@ -855,12 +855,14 @@ func testPXE(ctx context.Context, inst qemu.Install, outdir string) (time.Durati
 		return 0, errors.Wrapf(err, "running PXE")
 	}
 	defer func() {
-		if err := mach.Destroy(); err != nil {
+		err := mach.DeleteTempdir()
+		mach.Destroy()
+		if err != nil {
 			plog.Errorf("Failed to destroy PXE: %v", err)
 		}
 	}()
 
-	return awaitCompletion(ctx, mach.QemuInst, outdir, completionChannel, mach.BootStartedErrorChannel, []string{liveOKSignal, signalCompleteString})
+	return awaitCompletion(ctx, mach.Instance(), outdir, completionChannel, mach.BootStartedErrorChannel(), []string{liveOKSignal, signalCompleteString})
 }
 
 func testLiveIso(ctx context.Context, inst qemu.Install, outdir string, minimal bool) (time.Duration, error) {
@@ -925,12 +927,14 @@ func testLiveIso(ctx context.Context, inst qemu.Install, outdir string, minimal 
 		return 0, errors.Wrapf(err, "running iso install")
 	}
 	defer func() {
-		if err := mach.Destroy(); err != nil {
+		err := mach.DeleteTempdir()
+		mach.Destroy()
+		if err != nil {
 			plog.Errorf("Failed to destroy iso: %v", err)
 		}
 	}()
 
-	return awaitCompletion(ctx, mach.QemuInst, outdir, completionChannel, mach.BootStartedErrorChannel, []string{liveOKSignal, signalCompleteString})
+	return awaitCompletion(ctx, mach.Instance(), outdir, completionChannel, mach.BootStartedErrorChannel(), []string{liveOKSignal, signalCompleteString})
 }
 
 // testLiveFIPS verifies that adding fips=1 to the ISO results in a FIPS mode system
