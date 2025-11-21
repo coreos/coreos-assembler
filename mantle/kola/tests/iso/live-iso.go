0e2e7dce5e033fc5f82829a4324208d2acbe0008
Nikita Dubrovskii <nikita@linux.ibm.com>
2025-11-21 11:05:56 +0100

    kola: fold iso.as-disk* tests

--- mantle/kola/tests/iso/live-iso.go
+++ mantle/kola/tests/iso/live-iso.go
@@ -42,6 +42,13 @@ func isoTest(name string, run func(c cluster.TestCluster), arch []string) *regis
 }
 
 func init() {
+	// The iso-as-disk tests are only supported in x86_64 because other
+	// architectures don't have the required hybrid partition table.
+	register.RegisterTest(isoTest("as-disk", isoAsDisk, []string{"x86_64"}))
+	register.RegisterTest(isoTest("as-disk.uefi", isoAsDiskUefi, []string{"x86_64"}))
+	register.RegisterTest(isoTest("as-disk.uefi-secure", isoAsDiskUefiSecure, []string{"x86_64"}))
+	register.RegisterTest(isoTest("as-disk.4k.uefi", isoAsDisk4kUefi, []string{"x86_64"}))
+
 	register.RegisterTest(isoTest("install", isoInstall, []string{"x86_64"}))
 
 	register.RegisterTest(isoTest("offline-install", isoOfflineInstall, []string{"x86_64", "s390x", "ppc64le"}))
@@ -394,6 +401,36 @@ func newQemuBuilderWithDisk(opts IsoTestOpts, outdir string) (*platform.QemuBuil
 	}
 
 	return builder, config, nil
+}
+
+func isoAsDisk(c cluster.TestCluster) {
+	opts := IsoTestOpts{}
+	opts.SetInsecureOnDevBuild()
+	isoTestAsDisk(c, opts)
+}
+
+func isoAsDiskUefi(c cluster.TestCluster) {
+	opts := IsoTestOpts{
+		enableUefi: true,
+	}
+	opts.SetInsecureOnDevBuild()
+	isoTestAsDisk(c, opts)
+}
+func isoAsDiskUefiSecure(c cluster.TestCluster) {
+	opts := IsoTestOpts{
+		enableUefiSecure: true,
+	}
+	opts.SetInsecureOnDevBuild()
+	isoTestAsDisk(c, opts)
+}
+
+func isoAsDisk4kUefi(c cluster.TestCluster) {
+	opts := IsoTestOpts{
+		enable4k:   true,
+		enableUefi: true,
+	}
+	opts.SetInsecureOnDevBuild()
+	isoTestAsDisk(c, opts)
 }
 
 func isoInstall(c cluster.TestCluster) {
@@ -1101,4 +1138,47 @@ func awaitCompletion(c cluster.TestCluster, inst *platform.QemuInstance, console
 		}
 	}
 	return err
+}
+
+func isoTestAsDisk(c cluster.TestCluster, opts IsoTestOpts) {
+	var outdir string
+	//var qc *qemu.Cluster
+	switch pc := c.Cluster.(type) {
+	case *qemu.Cluster:
+		outdir = pc.RuntimeConf().OutputDir
+		//qc = pc
+	default:
+		c.Fatalf("Unsupported cluster type")
+	}
+
+	isopath := filepath.Join(kola.CosaBuild.Dir, kola.CosaBuild.Meta.BuildArtifacts.LiveIso.Path)
+	builder, config, err := newQemuBuilder(opts, outdir)
+	if err != nil {
+		c.Fatal(err)
+	}
+	defer builder.Close()
+	// Drop the bootindex bit (applicable to all arches except s390x and ppc64le); we want it to be the default
+	if err := builder.AddIso(isopath, "", true); err != nil {
+		c.Fatal(err)
+	}
+
+	completionChannel, err := builder.VirtioChannelRead("testisocompletion")
+	if err != nil {
+		c.Fatal(err)
+	}
+
+	config.AddSystemdUnit("live-signal-ok.service", liveSignalOKUnit, conf.Enable)
+	config.AddSystemdUnit("verify-no-efi-boot-entry.service", verifyNoEFIBootEntry, conf.Enable)
+	builder.SetConfig(config)
+
+	mach, err := builder.Exec()
+	if err != nil {
+		c.Fatal(err)
+	}
+	defer mach.Destroy()
+
+	err = awaitCompletion(c, mach, opts.console, outdir, completionChannel, nil, []string{liveOKSignal})
+	if err != nil {
+		c.Fatal(err)
+	}
 }
