d80dad220b53c5936956dc81eafe7998b9c6de38
Nikita Dubrovskii <nikita@linux.ibm.com>
2025-11-21 14:27:33 +0100

    kola/tests/iso: refactor *fips* tests
    Update the test to use the existing MachineBuilder API after the
    testiso migration.

--- mantle/kola/tests/iso/live-fips.go
+++ mantle/kola/tests/iso/live-fips.go
@@ -7,15 +7,18 @@ import (
 	"github.com/coreos/coreos-assembler/mantle/kola"
 	"github.com/coreos/coreos-assembler/mantle/kola/cluster"
 	"github.com/coreos/coreos-assembler/mantle/kola/register"
+	"github.com/coreos/coreos-assembler/mantle/platform"
 	"github.com/coreos/coreos-assembler/mantle/platform/conf"
 	"github.com/coreos/coreos-assembler/mantle/platform/machine/qemu"
+	"github.com/pkg/errors"
 )
 
+// These tests only run on RHCOS
+var tests_RHCOS_uefi = []string{
+	"iso-fips.uefi",
+}
+
 func init() {
-	// These tests only run on RHCOS
-	var tests_RHCOS_uefi = []string{
-		"iso-fips.uefi",
-	}
 	for _, testName := range tests_RHCOS_uefi {
 		register.RegisterTest(&register.Test{
 			Run: func(c cluster.TestCluster) {
@@ -24,6 +27,7 @@ func init() {
 			},
 			ClusterSize:   0,
 			Name:          "iso." + testName,
+			Description:   "verifies that adding fips=1 to the ISO results in a FIPS mode system",
 			Timeout:       installTimeoutMins * time.Minute,
 			Distros:       []string{"rhcos"},
 			Platforms:     []string{"qemu"},
@@ -32,40 +36,7 @@ func init() {
 	}
 }
 
-// testLiveFIPS verifies that adding fips=1 to the ISO results in a FIPS mode system
-func testLiveFIPS(c cluster.TestCluster, opts IsoTestOpts) {
-	var outdir string
-	//var qc *qemu.Cluster
-	switch pc := c.Cluster.(type) {
-	case *qemu.Cluster:
-		outdir = pc.RuntimeConf().OutputDir
-		//qc = pc
-	default:
-		c.Fatalf("Unsupported cluster type")
-	}
-
-	isopath := filepath.Join(kola.CosaBuild.Dir, kola.CosaBuild.Meta.BuildArtifacts.LiveIso.Path)
-	builder, config, err := newQemuBuilder(opts, outdir)
-	if err != nil {
-		c.Fatal(err)
-	}
-	defer builder.Close()
-	if err := builder.AddIso(isopath, "", false); err != nil {
-		c.Fatal(err)
-	}
-
-	// This is the core change under test - adding the `fips=1` kernel argument via
-	// coreos-installer iso kargs modify should enter fips mode.
-	// Removing this line should cause this test to fail.
-	builder.AppendKernelArgs = "fips=1"
-
-	completionChannel, err := builder.VirtioChannelRead("testisocompletion")
-	if err != nil {
-		c.Fatal(err)
-	}
-
-	config.AddSystemdUnit("fips-verify.service", `
-[Unit]
+var fipsVerify = `[Unit]
 OnFailure=emergency.target
 OnFailureJobMode=isolate
 Before=fips-signal-ok.service
@@ -77,23 +48,57 @@ ExecStart=grep 1 /proc/sys/crypto/fips_enabled
 ExecStart=grep FIPS etc/crypto-policies/config
 
 [Install]
-RequiredBy=fips-signal-ok.service
-`, conf.Enable)
-	config.AddSystemdUnit("fips-signal-ok.service", liveSignalOKUnit, conf.Enable)
-	config.AddSystemdUnit("fips-emergency-target.service", signalFailureUnit, conf.Enable)
+RequiredBy=fips-signal-ok.service`
 
-	// Just for reliability, we'll run fully offline
-	builder.Append("-net", "none")
+func testLiveFIPS(c cluster.TestCluster, opts IsoTestOpts) {
+	qc, ok := c.Cluster.(*qemu.Cluster)
+	if !ok {
+		c.Fatalf("Unsupported cluster type")
+	}
 
-	builder.SetConfig(config)
-	mach, err := builder.Exec()
+	config, err := conf.EmptyIgnition().Render(conf.FailWarnings)
 	if err != nil {
 		c.Fatal(err)
 	}
-	defer mach.Destroy()
-
-	err = awaitCompletion(c, mach, opts.console, outdir, completionChannel, nil, []string{liveOKSignal})
+	config.AddSystemdUnit("fips-verify.service", fipsVerify, conf.Enable)
+	config.AddSystemdUnit("fips-signal-ok.service", liveSignalOKUnit, conf.Enable)
+	config.AddSystemdUnit("fips-emergency-target.service", signalFailureUnit, conf.Enable)
+	keys, err := qc.Keys()
 	if err != nil {
+		c.Fatal(err)
+	}
+	config.CopyKeys(keys)
+
+	errchan := make(chan error)
+	setupDisks := func(_ platform.MachineOptions, builder *platform.QemuBuilder) error {
+		isopath := filepath.Join(kola.CosaBuild.Dir, kola.CosaBuild.Meta.BuildArtifacts.LiveIso.Path)
+		if err := builder.AddIso(isopath, "", false); err != nil {
+			return err
+		}
+		completionChannel, err := builder.VirtioChannelRead("testisocompletion")
+		if err != nil {
+			return errors.Wrap(err, "setting up virtio-serial channel")
+		}
+		go func() {
+			errchan <- CheckTestOutput(completionChannel, []string{liveOKSignal})
+		}()
+		return nil
+	}
+
+	options := platform.MachineOptions{AppendKernelArgs: "fips=1"}
+	if opts.enableUefi {
+		options.Firmware = "uefi"
+	}
+
+	machineBuilder := &qemu.MachineBuilder{
+		SetupDisks: setupDisks,
+	}
+	_, err = qc.NewMachineWithBuilder(config, options, machineBuilder)
+	if err != nil {
+		c.Fatalf("Unable to create test machine: %v", err)
+	}
+
+	if err := <-errchan; err != nil {
 		c.Fatal(err)
 	}
 }
