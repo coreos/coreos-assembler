d2ffe6f59770e1b7021d5cf48e840ef2245e3065
Nikita Dubrovskii <nikita@linux.ibm.com>
2025-11-24 12:13:20 +0100

    kola/tests/iso: refactor iso-as-disk* tests
    Also remove the 4k sector size test variant as it's technically impossible
    to configure. The AddIso() function and iso-as-disk implementation do
    not support sector size configuration - the ISO is attached as a raw,
    readonly device without any sector size options.
    
    The Disk.SectorSize field only applies to regular disk images added
    via AddDisk() or AddDisksFromSpecs(), not to ISOs attached via AddIso().
    Since iso-as-disk tests boot directly from the ISO file (not a disk
    image), sector size configuration is not applicable.

--- mantle/kola/tests/iso/live-as-disk.go
+++ mantle/kola/tests/iso/live-as-disk.go
@@ -7,20 +7,21 @@ import (
 	"github.com/coreos/coreos-assembler/mantle/kola"
 	"github.com/coreos/coreos-assembler/mantle/kola/cluster"
 	"github.com/coreos/coreos-assembler/mantle/kola/register"
+	"github.com/coreos/coreos-assembler/mantle/platform"
 	"github.com/coreos/coreos-assembler/mantle/platform/conf"
 	"github.com/coreos/coreos-assembler/mantle/platform/machine/qemu"
+	"github.com/pkg/errors"
 )
 
-func init() {
-	// The iso-as-disk tests are only supported in x86_64 because other
-	// architectures don't have the required hybrid partition table.
-	var tests_as_disk_x86_64 = []string{
-		"iso-as-disk.bios",
-		"iso-as-disk.uefi",
-		"iso-as-disk.uefi-secure",
-		"iso-as-disk.4k.uefi",
-	}
+// The iso-as-disk tests are only supported in x86_64 because other
+// architectures don't have the required hybrid partition table.
+var tests_as_disk_x86_64 = []string{
+	"iso-as-disk.bios",
+	"iso-as-disk.uefi",
+	"iso-as-disk.uefi-secure",
+}
 
+func init() {
 	for _, testName := range tests_as_disk_x86_64 {
 		register.RegisterTest(&register.Test{
 			Run: func(c cluster.TestCluster) {
@@ -38,44 +39,56 @@ func init() {
 }
 
 func isoTestAsDisk(c cluster.TestCluster, opts IsoTestOpts) {
-	var outdir string
-	//var qc *qemu.Cluster
-	switch pc := c.Cluster.(type) {
-	case *qemu.Cluster:
-		outdir = pc.RuntimeConf().OutputDir
-		//qc = pc
-	default:
+	qc, ok := c.Cluster.(*qemu.Cluster)
+	if !ok {
 		c.Fatalf("Unsupported cluster type")
 	}
 
-	isopath := filepath.Join(kola.CosaBuild.Dir, kola.CosaBuild.Meta.BuildArtifacts.LiveIso.Path)
-	builder, config, err := newQemuBuilder(opts, outdir)
+	config, err := conf.EmptyIgnition().Render(conf.FailWarnings)
 	if err != nil {
 		c.Fatal(err)
 	}
-	defer builder.Close()
-	// Drop the bootindex bit (applicable to all arches except s390x and ppc64le); we want it to be the default
-	if err := builder.AddIso(isopath, "", true); err != nil {
-		c.Fatal(err)
-	}
-
-	completionChannel, err := builder.VirtioChannelRead("testisocompletion")
-	if err != nil {
-		c.Fatal(err)
-	}
-
 	config.AddSystemdUnit("live-signal-ok.service", liveSignalOKUnit, conf.Enable)
 	config.AddSystemdUnit("verify-no-efi-boot-entry.service", verifyNoEFIBootEntry, conf.Enable)
-	builder.SetConfig(config)
-
-	mach, err := builder.Exec()
+	keys, err := qc.Keys()
 	if err != nil {
 		c.Fatal(err)
 	}
-	defer mach.Destroy()
+	config.CopyKeys(keys)
 
-	err = awaitCompletion(c, mach, opts.console, outdir, completionChannel, nil, []string{liveOKSignal})
+	errchan := make(chan error)
+	setupDisks := func(_ platform.MachineOptions, builder *platform.QemuBuilder) error {
+		isopath := filepath.Join(kola.CosaBuild.Dir, kola.CosaBuild.Meta.BuildArtifacts.LiveIso.Path)
+		if err := builder.AddIso(isopath, "", true); err != nil {
+			return err
+		}
+		completionChannel, err := builder.VirtioChannelRead("testisocompletion")
+		if err != nil {
+			return errors.Wrap(err, "setting up virtio-serial channel")
+		}
+		go func() {
+			errchan <- CheckTestOutput(completionChannel, []string{liveOKSignal})
+		}()
+		return nil
+	}
+
+	options := platform.MachineOptions{}
+	switch {
+	case opts.enableUefiSecure:
+		options.Firmware = "uefi-secure"
+	case opts.enableUefi:
+		options.Firmware = "uefi"
+	}
+
+	machineBuilder := &qemu.MachineBuilder{
+		SetupDisks: setupDisks,
+	}
+	_, err = qc.NewMachineWithBuilder(config, options, machineBuilder)
 	if err != nil {
+		c.Fatalf("Unable to create test machine: %v", err)
+	}
+
+	if err := <-errchan; err != nil {
 		c.Fatal(err)
 	}
 }
