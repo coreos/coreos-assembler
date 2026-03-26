4beebc1e3199eb1458fdaa53b9a9e9175f950751
Michael Armijo <marmijo@redhat.com>
2026-03-26 14:50:49 -0600

    mantle/azure: delete gallery image versions individually
    Change Azure gallery deletion to operate on a single gallery image
    version instead of removing all versions and deleting the image
    definition unconditionally.
    
    Require `--version` for `delete-gallery-image`, apply gallery-profile
    version handling when resolving the version to delete, and treat a
    missing version as a successful no-op.
    
    Also add `--delete-definition` to optionally remove the gallery image
    definition after the version delete, letting Azure reject the request if
    other versions still exist in the definition.

--- mantle/cmd/ore/azure/delete-gallery-image.go
+++ mantle/cmd/ore/azure/delete-gallery-image.go
@@ -30,35 +30,41 @@ var (
 
 		SilenceUsage: true,
 	}
+
+	deleteDefinition bool
 )
 
 func init() {
 	sv := cmdDeleteGalleryImage.Flags().StringVar
+	bv := cmdDeleteGalleryImage.Flags().BoolVar
 
 	sv(&imageName, "gallery-image-name", "", "gallery image name")
 	sv(&resourceGroup, "resource-group", "kola", "resource group name")
 	sv(&galleryName, "gallery-name", "kola", "gallery name")
+	sv(&version, "version", "", "The azure gallery image version")
+	sv(&galleryProfile, "coreos-gallery-profile", "", "The CoreOS specific gallery profile to apply to the image on upload")
+	bv(&deleteDefinition, "delete-definition", false, "delete the gallery image definition after deleting the specified version")
 
 	Azure.AddCommand(cmdDeleteGalleryImage)
 }
 
 func runDeleteGalleryImage(cmd *cobra.Command, args []string) error {
+	if imageName == "" {
+		return fmt.Errorf("must supply --gallery-image-name")
+	}
+
+	if version == "" {
+		return fmt.Errorf("must supply --version")
+	}
+
 	if err := api.SetupClients(); err != nil {
 		return fmt.Errorf("setting up clients: %v\n", err)
 	}
 
-	err := api.DeleteGalleryImage(imageName, resourceGroup, galleryName)
+	err := api.DeleteGalleryImageVersion(imageName, version, resourceGroup, galleryName, galleryProfile, deleteDefinition)
 	if err != nil {
-		return fmt.Errorf("Couldn't delete gallery image: %v\n", err)
+		return fmt.Errorf("Couldn't delete gallery image version: %v\n", err)
 	}
 
-	// Gallery image versions are backed by managed images with the same name,
-	// so we can easily identify and delete them together.
-	err = api.DeleteImage(imageName, resourceGroup)
-	if err != nil {
-		return fmt.Errorf("Couldn't delete image: %v\n", err)
-	}
-
-	plog.Printf("Image %q in gallery %q in resource group %q removed", imageName, galleryName, resourceGroup)
 	return nil
 }
