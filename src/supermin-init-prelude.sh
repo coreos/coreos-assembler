7926676f448bdb58bb72c781a079b7114635a575
Dusty Mabe <dusty@dustymabe.com>
2026-04-14 09:45:12 -0400

    supermin: drop hacks for OS configuration
    Because we weren't using Fedora CoreOS as the build root (because
    no python) for OSBuild stages we accumulated some hacks here in
    the supermin-init-prelude (basically script that runs on initial
    boot of the supermin VM).
    
    Now that we are using Fedora CoreOS as the build root, with a
    thin python layer from COSA on top, we can drop these hacks.
    
    The ostree composefs setting is already in FCOS today and the
    bootc-install settings will be with [1].
    
    [1] https://github.com/coreos/fedora-coreos-config/pull/4093

--- src/supermin-init-prelude.sh
+++ src/supermin-init-prelude.sh
@@ -90,34 +90,3 @@ touch /etc/cosa-supermin
 # the missing link.
 update-alternatives --install /etc/alternatives/iptables iptables /usr/sbin/iptables-nft 1
 update-alternatives --install /etc/alternatives/ip6tables ip6tables /usr/sbin/ip6tables-nft 1
-
-# To build the disk image using osbuild and bootc install to-filesystem we need to
-# have a prepare-root config in the build environnement for bootc to read.
-# This workaround can be removed when https://github.com/bootc-dev/bootc/issues/1410
-# is fixed or we have python in all streams which allows us to use the OCI image as the buildroot.
-# Note that RHCOS and SCOS use the OCI as buildroot so they should not be affected by this.
-cat > /usr/lib/ostree/prepare-root.conf <<EOF
-[composefs]
-enabled = true
-EOF
-
-# Tell bootc to enforce that `/etc/containers/policy.json` include a default
-# policy that verify our images signature.
-# When moving to image-builder, this config can be moved into the container itself
-# but as long as we are using osbuild manually we have to carry this in the buildroot.
-# TODO: uncomment this when https://github.com/bootc-dev/bootc/pull/2116
-# is merged and released
-# cat > usr/lib/bootc/install/10-sigpolicy.toml <<EOF
-# [install]
-# enforce-container-sigpolicy = true
-# EOF
-
-# TODO move this to an overlay in fedora-coreos-config
-# so it get baked into the container at build time. We
-# want the container to be the source of truth as much as possible.
-# Same as the entries above, we need to have this in cosa until
-# we move to image builder
-cat <<EOF > /usr/lib/bootc/install/10-grub-users.toml
-[install.ostree]
-bls-append-except-default = 'grub_users=""'
-EOF
