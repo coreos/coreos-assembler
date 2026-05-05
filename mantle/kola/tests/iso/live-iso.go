ac9288d078e2fd7763b5661a0011d3a52303f61e
Nikita Dubrovskii <nikita@linux.ibm.com>
2026-05-05 08:13:18 +0200

    kola/tests/iso: add reprovision tag to live tests
    Mark the live ISO tests with the reprovision tag because those
    require a lot of memory and should run serially.

--- mantle/kola/tests/iso/live-iso.go
+++ mantle/kola/tests/iso/live-iso.go
@@ -81,7 +81,7 @@ func init() {
 	for _, testName := range getAllLiveIsoTests() {
 		// Skip base checks (looks at journal for failures) until bootupd fix lands
 		// https://github.com/coreos/fedora-coreos-tracker/issues/2136
-		tags := []string{kola.SkipBaseChecksTag}
+		tags := []string{kola.SkipBaseChecksTag, "reprovision"}
 		if !strings.Contains(testName, "offline") {
 			tags = append(tags, kola.NeedsInternetTag)
 		}
