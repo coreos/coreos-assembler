07cc8cd22cdae29576bbe61b37d00aeb1aa36c6e
Dusty Mabe <dusty@dustymabe.com>
2026-06-11 17:01:43 -0400

    kola/tests/iso: simplify iso fips test
    Now that we support BootFrom in the machineOptions we can greatly
    simplify the iso fips test and also allow it to be folded back into
    the test harness (ClusterSize: 1) so the harness is what's taking care
    of the lifecycle of the machine.

--- mantle/kola/tests/iso/live-fips.go
+++ mantle/kola/tests/iso/live-fips.go
@@ -1,16 +1,12 @@
 package iso
 
 import (
-	"path/filepath"
 	"time"
 
 	"github.com/coreos/coreos-assembler/mantle/kola"
 	"github.com/coreos/coreos-assembler/mantle/kola/cluster"
 	"github.com/coreos/coreos-assembler/mantle/kola/register"
 	"github.com/coreos/coreos-assembler/mantle/platform"
-	"github.com/coreos/coreos-assembler/mantle/platform/conf"
-	"github.com/coreos/coreos-assembler/mantle/platform/machine/qemu"
-	"github.com/pkg/errors"
 )
 
 // These tests only run on RHCOS
@@ -20,90 +16,34 @@ var tests_RHCOS_uefi = []string{
 
 func init() {
 	for _, testName := range tests_RHCOS_uefi {
+		opts := getIsoTestOpts(testName)
+
 		register.RegisterTest(&register.Test{
-			Run: func(c cluster.TestCluster) {
-				opts := getIsoTestOpts(testName)
-				testLiveFIPS(c, opts)
-			},
-			ClusterSize:   0,
+			Run:           testLiveFIPS,
+			ClusterSize:   1,
 			Name:          "iso." + testName,
 			Description:   "verifies that adding fips=1 to the ISO results in a FIPS mode system",
-			Timeout:       installTimeoutMins * time.Minute,
+			Timeout:       3 * time.Minute, // Just boots the ISO -> quick
 			Distros:       []string{"rhcos"},
 			Platforms:     []string{"qemu"},
 			Architectures: []string{"x86_64", "aarch64"},
 			// Skip base checks (looks at journal for failures) until bootupd fix lands
 			// https://github.com/coreos/fedora-coreos-tracker/issues/2136
 			Tags: []string{kola.SkipBaseChecksTag, "reprovision"},
+			MachineOptions: platform.MachineOptions{
+				AppendKernelArgs: "fips=1",
+				Firmware:         opts.firmware,
+				BootFrom:         platform.BootFromISO,
+			},
 		})
 	}
 }
 
-var fipsVerify = `[Unit]
-OnFailure=emergency.target
-OnFailureJobMode=isolate
-Before=fips-signal-ok.service
-
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=grep 1 /proc/sys/crypto/fips_enabled
-ExecStart=grep FIPS etc/crypto-policies/config
-
-[Install]
-RequiredBy=fips-signal-ok.service`
-
-func testLiveFIPS(c cluster.TestCluster, opts IsoTestOpts) {
-	EnsureLiveArtifactsExist(c)
-
-	qc, ok := c.Cluster.(*qemu.Cluster)
-	if !ok {
-		c.Fatalf("Unsupported cluster type")
-	}
-
-	config, err := conf.EmptyIgnition().Render(conf.FailWarnings)
-	if err != nil {
-		c.Fatal(err)
-	}
-	config.AddSystemdUnit("fips-verify.service", fipsVerify, conf.Enable)
-	config.AddSystemdUnit("fips-signal-ok.service", liveSignalOKUnit, conf.Enable)
-	config.AddSystemdUnit("fips-emergency-target.service", signalFailureUnit, conf.Enable)
-	keys, err := qc.Keys()
-	if err != nil {
-		c.Fatal(err)
-	}
-	config.CopyKeys(keys)
-
-	errchan := make(chan error)
-	setupDisks := func(_ platform.MachineOptions, builder *platform.QemuBuilder) error {
-		isopath := filepath.Join(kola.CosaBuild.Dir, kola.CosaBuild.Meta.BuildArtifacts.LiveIso.Path)
-		if err := builder.AddIso(isopath, "", false); err != nil {
-			return err
-		}
-		completionChannel, err := builder.VirtioChannelRead("testisocompletion")
-		if err != nil {
-			return errors.Wrap(err, "setting up virtio-serial channel")
-		}
-		go func() {
-			errchan <- CheckTestOutput(completionChannel, []string{liveOKSignal})
-		}()
-		return nil
-	}
-
-	options := platform.MachineOptions{
-		AppendKernelArgs: "fips=1",
-		Firmware:         opts.firmware,
-	}
-
-	machineBuilder := &qemu.MachineBuilder{
-		SetupDisks: setupDisks,
-	}
-	_, err = qc.NewMachineWithBuilder(config, options, machineBuilder)
-	if err != nil {
-		c.Fatalf("Unable to create test machine: %v", err)
-	}
-
-	if err := <-errchan; err != nil {
-		c.Fatal(err)
-	}
+func testLiveFIPS(c cluster.TestCluster) {
+	m := c.Machines()[0]
+	// Verify we are on Live ISO
+	c.RunCmdSync(m, "test -f /run/ostree-live")
+	// And that FIPS is enabled
+	c.RunCmdSync(m, "grep 1 /proc/sys/crypto/fips_enabled")
+	c.RunCmdSync(m, "grep FIPS /etc/crypto-policies/config")
 }
